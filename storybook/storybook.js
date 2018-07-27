ae758a12b3852f4ce7832b2d9148fdccabce3379
Drew Miller <drew@yulife.com>
2018-07-27 15:47:58 +0100

    Task: Install Storybook

---
+++ storybook/storybook.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { Navigation } from "react-native-navigation";
+import { getStorybookUI, configure } from "@storybook/react-native";
+import { loadStories } from "./storyLoader";
+
+// import stories
+configure(() => {
+    loadStories()
+}, module);
+
+// This assumes that storybook is running on the same host as your RN packager,
+// to set manually use, e.g. host: "localhost" option
+const StorybookUIRoot = getStorybookUI({
+    port: 7007,
+    onDeviceUI: true
+});
+
+// react-native hot module loader must take in a Class - https://github.com/facebook/react-native/issues/10991
+// https://github.com/storybooks/storybook/issues/2081
+// eslint-disable-next-line react/prefer-stateless-function
+class StorybookUIHMRRoot extends Component {
+    render() {
+        return <StorybookUIRoot /> ;
+    }
+}
+
+Navigation.registerComponent("yulife.Storybook", () => StorybookUIHMRRoot);
+
+Navigation.events().registerAppLaunchedListener(() => {
+
+    Navigation.setDefaultOptions({
+        topBar: {
+            visible: false
+        }
+    });
+
+    Navigation.setRoot({
+        root: {
+            stack: {
+                children: [{
+                    component: {
+                        name: "yulife.Storybook"
+                    },
+                    id: "TEST",
+                }]
+            }
+        }
+    });
+
+});
