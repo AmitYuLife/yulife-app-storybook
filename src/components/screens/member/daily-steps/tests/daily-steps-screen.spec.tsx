317cdb824060876e25837e4525813acf6e6d8b8c
Drew Miller <poppiestar@users.noreply.github.com>
2018-08-12 16:38:28 +0100

    Chore: Add tests for daily steps screen

--- src/components/screens/member/daily-steps/tests/daily-steps-screen.spec.tsx
+++ src/components/screens/member/daily-steps/tests/daily-steps-screen.spec.tsx
@@ -3,39 +3,56 @@ import * as React from "react";
 import "react-native";
 import DailyStepsScreen from "../daily-steps-screen";
 
+const defaultProps = {
+    coinsToday: 0,
+    coinsTotal: 0,
+    currentStreak: 0,
+    hasNotification: false,
+    isDoneToday: false,
+    labels: [
+        {
+            name: "yucoin",
+            onPress: jest.fn(),
+        },
+        {
+            name: "quest",
+            onPress: jest.fn(),
+        },
+        {
+            name: "rewards",
+            onPress: jest.fn(),
+        },
+    ],
+    maxStreak: 4,
+    onCtaPress: jest.fn(),
+    onMenuPress: jest.fn(),
+    onStreakPress: jest.fn(),
+    steps: 0,
+};
+
 describe("DailyStepsScreen", () => {
 
-    it("should render", () => {
+    it("should render default values", () => {
 
         const actual = shallow(
             <DailyStepsScreen
-                coinsToday={0}
-                coinsTotal={0}
-                currentStreak={0}
-                hasNotification={false}
-                isDoneToday={false}
-                labels={[
-                    {
-                        name: "yucoin",
-                        onPress: jest.fn(),
-                    },
-                    {
-                        name: "quest",
-                        onPress: jest.fn(),
-                    },
-                    {
-                        name: "rewards",
-                        onPress: jest.fn(),
-                    },
-                ]}
-                maxStreak={4}
-                onCtaPress={jest.fn()}
-                onMenuPress={jest.fn()}
-                onStreakPress={jest.fn()}
-                steps={0}
+                {...defaultProps}
             />
         );
 
         expect(actual).toMatchSnapshot();
     });
+
+    it("should render daily streak", () => {
+
+        const actual = shallow(
+            <DailyStepsScreen
+                {...defaultProps}
+                displayStreak={true}
+            />
+        );
+
+        expect(actual).toMatchSnapshot();
+    });
+
 });
