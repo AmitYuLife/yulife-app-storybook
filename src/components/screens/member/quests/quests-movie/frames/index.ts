7be4f29c2852a1ea27209f2380456a136c80d9d6
John Salcedo <john@aurity.co>
2018-09-13 05:57:33 +0000

    Task: add map animation

---
+++ src/components/screens/member/quests/quests-movie/frames/index.ts
@@ -0,0 +1,70 @@
+import frame34 from "../../quests-screen/subcomponents/backgrounds/assets/bg-8";
+import frame1 from "./1";
+import frame10 from "./10";
+import frame11 from "./11";
+import frame12 from "./12";
+import frame13 from "./13";
+import frame14 from "./14";
+import frame15 from "./15";
+import frame16 from "./16";
+import frame17 from "./17";
+import frame18 from "./18";
+import frame19 from "./19";
+import frame2 from "./2";
+import frame20 from "./20";
+import frame21 from "./21";
+import frame22 from "./22";
+import frame23 from "./23";
+import frame24 from "./24";
+import frame25 from "./25";
+import frame26 from "./26";
+import frame27 from "./27";
+import frame28 from "./28";
+import frame29 from "./29";
+import frame3 from "./3";
+import frame30 from "./30";
+import frame31 from "./31";
+import frame32 from "./32";
+import frame33 from "./33";
+import frame4 from "./4";
+import frame5 from "./5";
+import frame6 from "./6";
+import frame7 from "./7";
+import frame8 from "./8";
+import frame9 from "./9";
+export default [
+    frame1,
+    frame2,
+    frame3,
+    frame4,
+    frame5,
+    frame6,
+    frame7,
+    frame8,
+    frame9,
+    frame10,
+    frame11,
+    frame12,
+    frame13,
+    frame14,
+    frame15,
+    frame16,
+    frame17,
+    frame18,
+    frame19,
+    frame20,
+    frame21,
+    frame22,
+    frame23,
+    frame24,
+    frame25,
+    frame26,
+    frame27,
+    frame28,
+    frame29,
+    frame30,
+    frame31,
+    frame32,
+    frame33,
+    frame34
+];
