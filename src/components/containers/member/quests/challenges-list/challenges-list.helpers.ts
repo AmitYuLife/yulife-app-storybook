26f292a0c247c4e01abacf15f45ea37880acd21e
vitalytr <vitaly.trus@gmail.com>
2018-10-01 11:08:34 +0100

    Bugfix: fix milestones when level unavailable

--- src/components/containers/member/quests/challenges-list/challenges-list.helpers.ts
+++ src/components/containers/member/quests/challenges-list/challenges-list.helpers.ts
@@ -14,25 +14,28 @@ export const formatMilestones = (milestones: GetCurrentWorld_getCurrentWorld_slo
 
 export const secondsToMinutes = (seconds: number): number => Math.floor(seconds / 60);
 
-export const getSlotDuration = (slot: GetCurrentWorld_getCurrentWorld_slots): string => {
-    switch (slot.subtype) {
+export const getSlotDuration = ({ subtype, milestones, timeLimit }: GetCurrentWorld_getCurrentWorld_slots): string => {
+    switch (subtype) {
         case "meditation":
-            const firstMilestone = slot.milestones[0].target.meditation;
-            const meditationTime = secondsToMinutes(firstMilestone);
+            if (milestones) {
+                const firstMilestone = milestones[0].target.meditation;
+                const meditationTime = secondsToMinutes(firstMilestone);
 
-            if (slot.milestones.length > 1) {
-                const lastMilestone = slot.milestones[slot.milestones.length - 1].target.meditation;
+                if (milestones.length > 1) {
+                    const lastMilestone = milestones[milestones.length - 1].target.meditation;
 
-                return `${meditationTime}-${secondsToMinutes(lastMilestone)} mins`;
+                    return `${meditationTime}-${secondsToMinutes(lastMilestone)} mins`;
+                }
+
+                return `${meditationTime} min${meditationTime > 1 ? "s" : ""}`;
             }
 
-            return `${meditationTime} min${meditationTime > 1 ? "s" : ""}`;
-
+            return "";
         case "day walk":
             return "all day";
 
         default:
-            const defaultTime = secondsToMinutes(slot.timeLimit);
+            const defaultTime = secondsToMinutes(timeLimit);
             return `${defaultTime} min${defaultTime > 1 ? "s" : ""}`;
     }
 };
