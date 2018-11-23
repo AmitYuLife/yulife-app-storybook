5ad112eb85c9279ca006869f11150a7279204e32
Drew Miller <poppiestar@users.noreply.github.com>
2018-11-23 15:43:48 +0000

    Task: Fix push notifications in iOS

--- src/redux/device/device.channels.ts
+++ src/redux/device/device.channels.ts
@@ -1,4 +1,4 @@
-import { Platform } from "react-native";
+import { Platform, PushNotificationIOS } from "react-native";
 import Config from "react-native-config";
 import PushNotification from "react-native-push-notification";
 import { eventChannel } from "redux-saga";
@@ -6,7 +6,10 @@ import { eventChannel } from "redux-saga";
 export function createPushNotificationsChannel() {
     return eventChannel((emitter) => {
         PushNotification.configure({
-            onNotification: (notification) => emitter(notification),
+            onNotification: (notification) => {
+                emitter(notification);
+                notification.finish(PushNotificationIOS.FetchResult.NoData);
+            },
             onRegister: (result) => {
                 emitter(result);
             },
