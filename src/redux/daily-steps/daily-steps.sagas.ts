239979bfc93217781644f7ba142d3d5708ead947
vitalytr <vitaly.trus@gmail.com>
2018-11-28 20:13:19 +0000

    Bugfix: old steps not updating

--- src/redux/daily-steps/daily-steps.sagas.ts
+++ src/redux/daily-steps/daily-steps.sagas.ts
@@ -2,6 +2,7 @@ import moment from "moment";
 import { PedometerResponse } from "react-native-dual-pedometer";
 import RNFitKit, { FitKitTypes } from "react-native-fitkit";
 import { Navigation } from "react-native-navigation";
+import { delay } from "redux-saga";
 import { call, put, select, takeLatest } from "redux-saga/effects";
 import { AddHistoricalSteps_addHistoricalSteps, ChallengePayload } from "../../graphql/_core/schema";
 import addHistoricalSteps from "../../graphql/challenges/addHistoricalSteps.gql";
@@ -31,6 +32,7 @@ export function* oldDaysUpdate() {
     const startOfDay = moment().startOf("day");
 
     if (moment(lastUpdated).isBefore(startOfDay)) {
+        let isUpdated = false;
         // get steps from start of last updated date until the end of previous day
         const startTime = moment(lastUpdated)
             .startOf("day")
@@ -55,33 +57,41 @@ export function* oldDaysUpdate() {
                 startTime
             });
 
-            const res = yield call(addHistoricalSteps, results.map(mapPedometerResults), true);
-            const response: HistoricalSteps = pathOr<HistoricalSteps>(res, "data.addHistoricalSteps", {
-                endDateTime: "",
-                startDateTime: "",
-                yucoin: 0
-            });
-
-            if (response.yucoin > 0) {
-                const route = yield select(getRouteState);
-                if (route !== MODALS.collectReward) {
-                    const firstDay = moment(response.startDateTime).format("DD MMM");
-                    const lastDay = moment(response.endDateTime).format("DD MMM");
-                    const date = firstDay !== lastDay ? `${firstDay} - ${lastDay}` : firstDay;
-
-                    yield call(() => {
-                        Navigation.showModal({
-                            component: {
-                                id: MODALS.collectReward,
-                                name: MODALS.collectReward,
-                                passProps: {
-                                    date,
-                                    onPress: () => Navigation.dismissModal(MODALS.collectReward),
-                                    yucoin: response.yucoin
-                                }
-                            }
-                        });
+            while (!isUpdated) {
+                try {
+                    const res = yield call(addHistoricalSteps, results.map(mapPedometerResults), true);
+                    const response: HistoricalSteps = pathOr<HistoricalSteps>(res, "data.addHistoricalSteps", {
+                        endDateTime: "",
+                        startDateTime: "",
+                        yucoin: 0
                     });
+
+                    if (response.yucoin > 0) {
+                        const route = yield select(getRouteState);
+                        if (route !== MODALS.collectReward) {
+                            const firstDay = moment(response.startDateTime).format("DD MMM");
+                            const lastDay = moment(response.endDateTime).format("DD MMM");
+                            const date = firstDay !== lastDay ? `${firstDay} - ${lastDay}` : firstDay;
+
+                            yield call(() => {
+                                Navigation.showModal({
+                                    component: {
+                                        id: MODALS.collectReward,
+                                        name: MODALS.collectReward,
+                                        passProps: {
+                                            date,
+                                            onPress: () => Navigation.dismissModal(MODALS.collectReward),
+                                            yucoin: response.yucoin
+                                        }
+                                    }
+                                });
+                            });
+                        }
+                    }
+
+                    isUpdated = true;
+                } catch (e) {
+                    yield call(delay, 15000);
                 }
             }
         }
