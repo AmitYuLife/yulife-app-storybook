b36e41e78237e29fa64bb07139a0d27f377257ed
vitalytr <vitaly.trus@gmail.com>
2018-12-04 20:08:02 +0000

    Chore: add historical steps on signup

--- src/components/containers/onboarding/signup-reward/signup-reward.container.tsx
+++ src/components/containers/onboarding/signup-reward/signup-reward.container.tsx
@@ -1,15 +1,24 @@
+import moment from "moment";
 import * as React from "react";
 import { PureComponent } from "react";
 import { BackHandler, NativeEventSubscription } from "react-native";
+import RNFitKit, { FitKitTypes } from "react-native-fitkit";
 import { Navigation } from "react-native-navigation";
 import { connect } from "react-redux";
+import { AddHistoricalStepsMutationFunction } from "../../../../graphql/challenges/addHistoricalSteps.gql";
+import {
+    addHistoricalStepsGql,
+    AddHistoricalStepsMutation
+} from "../../../../graphql/challenges/addHistoricalSteps.gql";
 import UpsertOnboardingChallengeMutation, {
     upsertOnboardingChallengeGql,
     UpsertOnboardingChallengeMutationType,
     UpsertOnboardingChallengeStateType
 } from "../../../../graphql/challenges/upsertOnboardingChallenge.gql";
 import { setNextRoot } from "../../../../navigation/root";
+import { mapPedometerResults } from "../../../../redux/daily-steps/daily-steps.sagas";
 import { getUserStart } from "../../../../redux/user/user.actions";
+import Logger from "../../../../services/logging/logger";
 import { Loading } from "../../../atoms";
 import { SignUpRewardScreen } from "../../../screens";
 
@@ -57,15 +66,51 @@ class SignUpRewardContainerChild extends PureComponent<IChildProps> {
         }
 
         return (
-            <SignUpRewardScreen
-                onCollectPress={this.onCollect}
-                reward={data.upsertPassiveChallenge.challenge.yuCoinAwarded}
-            />
+            <AddHistoricalStepsMutation mutation={addHistoricalStepsGql}>
+                {(addHistoricalSteps) => (
+                    <SignUpRewardScreen
+                        onCollectPress={this.onCollect(addHistoricalSteps)}
+                        reward={data.upsertPassiveChallenge.challenge.yuCoinAwarded}
+                    />
+                )}
+            </AddHistoricalStepsMutation>
         );
     }
 
-    private onCollect = () => {
-        setNextRoot();
+    private onCollect = (addHistoricalSteps: AddHistoricalStepsMutationFunction) => async () => {
+        try {
+            const authorised = await RNFitKit.authorise({
+                read: [FitKitTypes.Types.Steps]
+            });
+
+            if (authorised) {
+                const startTime = moment()
+                    .subtract(30, "days")
+                    .startOf("day")
+                    .format();
+                const endTime = moment()
+                    .subtract(1, "days")
+                    .endOf("day")
+                    .format();
+                const results = await RNFitKit.aggregateQuery({
+                    aggregateBy: {
+                        bucketSize: { value: 1, type: FitKitTypes.TimeRange.DAYS },
+                        type: FitKitTypes.AggregateType.Time
+                    },
+                    endTime,
+                    sampleType: FitKitTypes.Types.Steps,
+                    startTime
+                });
+
+                await addHistoricalSteps({
+                    variables: { payload: results.map(mapPedometerResults as any), shouldAward: false }
+                });
+            }
+        } catch (e) {
+            Logger.logIntercomEvent("historical_steps_sync_failed", { message: e.message });
+        }
+
+        await setNextRoot();
     }
 }
 
