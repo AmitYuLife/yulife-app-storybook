debac58993b8a4a352b5709d431813819cbe1997
Vitaly <vitalytr@Vitalys-MBP.local>
2019-03-05 12:07:50 +0000

    Task: change back the notification icon

--- src/components/molecules/nav-bar/assets/notification.tsx
+++ src/components/molecules/nav-bar/assets/notification.tsx
@@ -10,8 +10,8 @@ interface IProps {
 export default function Notification({ isVisible }: IProps) {
     return !isVisible ? null : (
         <>
-            <Circle fill={Colours.navNotification.border} cx="251.6" cy="28" r="19" />
-            <Circle fill={Colours.navNotification.body} cx="251.6" cy="28" r="16" />
+            <Circle fill={Colours.navNotification.border} cx="286.1" cy="61.8" r="19" />
+            <Circle fill={Colours.navNotification.body} cx="285.8" cy="61.8" r="16" />
             <Path
                 fill={Colours.navNotification.border}
                 d="M248.7,24.4c-0.2,0.2-0.5,0.3-0.8,0.3c-0.7,0-1.3-0.6-1.3-1.3c0-0.5,0.2-0.9,0.6-1.1l3.5-2.2
