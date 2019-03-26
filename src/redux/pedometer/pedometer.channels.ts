911fbcab616ca3570de0325c5d9fb08a73e8aa91
Vitaly <vitalytr@Vitalys-MBP.local>
2019-03-26 09:50:35 +0000

    Bugfix MV-532: steps fetch

--- src/redux/pedometer/pedometer.channels.ts
+++ src/redux/pedometer/pedometer.channels.ts
@@ -1,16 +1,17 @@
 import RNFitKit from "@services/fitkit/fitkit.service";
 import { eventChannel } from "redux-saga";
 
-export function stepsChannel(startTime: string) {
+// export function stepsChannel(startTime: string) {
+export function stepsChannel() {
     return eventChannel((emitter) => {
         const { PEDOMETER_UPDATE } = RNFitKit.constants;
 
         const subscriber = RNFitKit.addListener(PEDOMETER_UPDATE, emitter);
-        RNFitKit.startPedometerUpdatesFromDate(startTime);
+        RNFitKit.startFetchingStepsData();
 
         const unlisten = () => {
             subscriber.remove();
-            RNFitKit.stopPedometerUpdates();
+            RNFitKit.stopFetchingStepsData();
         };
 
         return unlisten;
