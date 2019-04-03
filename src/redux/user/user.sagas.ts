33ab5fb91c7e948f446770e765bdc7773d63c8db
Vitaly Trus <vitaly@yulife.com>
2019-04-03 10:34:13 +0100

    Chore: check for bitrise build number

--- src/redux/user/user.sagas.ts
+++ src/redux/user/user.sagas.ts
@@ -102,6 +102,8 @@ function* getUserData() {
         if (token) {
             const { data } = yield call(getCurrentUserWithClient);
 
+            yield spawn(Logger.logMixpanelEvent, "CONFIG_FILE", Config);
+            yield spawn(Logger.logMixpanelEvent, "ENV_FILE", process.env);
             yield spawn(setTestFairyId, data.getCurrentUser.id);
             yield spawn(
                 setLoggerIdentity,
