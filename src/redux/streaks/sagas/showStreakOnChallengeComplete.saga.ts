273daa0bdffd93448829f544e7e659891105c240
Vitaly Trus <vitaly@yulife.com>
2019-04-10 14:47:38 +0100

    Bugfix: streaks modal shown when streak completed

--- src/redux/streaks/sagas/showStreakOnChallengeComplete.saga.ts
+++ src/redux/streaks/sagas/showStreakOnChallengeComplete.saga.ts
@@ -3,20 +3,24 @@ import { Navigation } from "react-native-navigation";
 import { delay } from "redux-saga";
 import { call, select, take } from "redux-saga/effects";
 import { getRouteState } from "../../app/app.selectors";
-import { getChallengesStatus } from "../../levels/levels.selectors";
 import { GET_USER_SUCCESS } from "../../user/user.actions";
 import { getUserFeatures } from "../../user/user.selectors";
 import { getStreaks } from "../streaks.selectors";
 
 export default function* showStreakOnChallengeCompleteSaga() {
+    const streaksBeforeUpdate = yield select(getStreaks);
+
     yield take(GET_USER_SUCCESS);
 
     const streaks = yield select(getStreaks);
     const currentRoute = yield select(getRouteState);
     const features = yield select(getUserFeatures);
-    const { done } = yield select(getChallengesStatus);
 
-    if (done === 1 && features.showStreaks && streaks.isAvailable && currentRoute !== MODALS.streaks) {
+    if (
+        features.showStreaks &&
+        streaksBeforeUpdate.currentStreak !== streaks.currentStreak &&
+        currentRoute !== MODALS.streaks
+    ) {
         if (currentRoute === MODALS.chest) {
             yield call(delay, 4000);
         }
