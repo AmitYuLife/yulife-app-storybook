76615cc57d2f3c3714cc9619300f585ce3a14ceb
Vitaly Trus <vitaly@yulife.com>
2019-05-09 08:59:34 +0100

    Chore: populate the copy persisted state with new data

--- src/redux/copy/copy.reducer.ts
+++ src/redux/copy/copy.reducer.ts
@@ -1,4 +1,5 @@
 import { GetMobileCopy_getMobileCopy } from "@graphql/_core/schema";
+import { REHYDRATE } from "redux-persist";
 import { SyncAction } from "../_core/types";
 import { UPDATE_COPY } from "./copy.actions";
 import defaultData from "./copy.data";
@@ -12,6 +13,11 @@ export const initialState: ICopyStore = {
 
 const copyReducer = (state: ICopyStore = initialState, action: SyncAction): ICopyStore => {
     switch (action.type) {
+        case REHYDRATE:
+            if (action.payload && action.payload.copy) {
+                return rehydrate(action.payload.copy as ICopyStore);
+            }
+            return state;
         case UPDATE_COPY:
             return action.payload.getMobileCopy;
         default:
@@ -20,3 +26,15 @@ const copyReducer = (state: ICopyStore = initialState, action: SyncAction): ICop
 };
 
 export default copyReducer;
+
+const rehydrate = (persistedState: ICopyStore) => {
+    const defaultScreensLength = Object.keys(defaultData).length;
+    const persistedScreensLength = Object.keys(persistedState.screens).length - 1;
+
+    if (defaultScreensLength > persistedScreensLength) {
+        // when adding new screens, the persisted state doesn't have them
+        return { version: persistedState.version, screens: { ...persistedState.screens, ...defaultData } };
+    }
+
+    return persistedState;
+};
