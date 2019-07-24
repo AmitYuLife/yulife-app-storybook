794346e58a372488c101f50ec464f59962bb3148
Boris <boris@yulife.com>
2019-07-24 14:11:22 +0000

    use spawn to listen for app state, network and navigation state

--- src/redux/token/token.sagas.ts
+++ src/redux/token/token.sagas.ts
@@ -10,39 +10,45 @@ import { Platform } from "react-native";
 import { call, takeLatest } from "redux-saga/effects";
 
 export function* checkTokenExpiry() {
-    const session = yield call(getSession);
+    try {
+        const session = yield call(getSession);
 
-    if (session.errors && session.errors.length) {
-        yield call(() => logger.logMixpanelError("error trying to get user session", session.errors));
-        return;
-    }
-
-    const isExpired = moment().isAfter(moment.unix(session.data.expires));
-    const needsRefreshing =
-        !isExpired &&
-        moment()
-            .add(14, "days")
-            .isAfter(moment.unix(session.data.expires));
-
-    if (isExpired) {
-        yield call(clearToken);
-        return;
-    }
-
-    if (needsRefreshing) {
-        const result: ExecutionResult = yield call(refreshSession, {
-            tokenExpiration: TOKEN_EXPIRATION,
-            intercomHashMethod: Platform.OS as IntercomHashMethod
-        });
-
-        if (result.errors && result.errors.length) {
-            yield call(() => logger.logMixpanelError("error trying to refresh session", result.errors));
+        if (session.errors && session.errors.length) {
+            yield call(() => logger.logMixpanelError("error trying to get user session", session.errors));
             return;
         }
 
-        if (result.data.refreshSession.token) {
-            yield call(setToken, result.data.refreshSession.token);
+        const isExpired = moment().isAfter(moment.unix(session.data.expires));
+        const needsRefreshing =
+            !isExpired &&
+            moment()
+                .add(14, "days")
+                .isAfter(moment.unix(session.data.expires));
+
+        if (isExpired) {
+            yield call(clearToken);
+            return;
         }
+
+        if (needsRefreshing) {
+            const result: ExecutionResult = yield call(refreshSession, {
+                tokenExpiration: TOKEN_EXPIRATION,
+                intercomHashMethod: Platform.OS as IntercomHashMethod
+            });
+
+            if (result.errors && result.errors.length) {
+                yield call(() => logger.logMixpanelError("error trying to refresh session", result.errors));
+                return;
+            }
+
+            if (result.data.refreshSession.token) {
+                yield call(setToken, result.data.refreshSession.token);
+            }
+        }
+    } catch (e) {
+        // Added that to catch Unhandled Promise Rejection when Network request failed.
+        // Sagas stop to work because a request fails when app is opened in offline mode
+        // because of that we should wrap it into a try/catch block.
     }
 }
 
