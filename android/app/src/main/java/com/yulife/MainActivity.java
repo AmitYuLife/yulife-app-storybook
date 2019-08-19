822cff18d8c49472dbb34b64a9eed5941c6a5316
Leomar Alejar <leomar@yulife.com>
2019-08-19 07:28:58 +0000

    Fix ENG-102: prevent fontScaling on Android

--- android/app/src/main/java/com/yulife/MainActivity.java
+++ android/app/src/main/java/com/yulife/MainActivity.java
@@ -2,6 +2,14 @@ package com.yulife;
 
 import com.reactnativenavigation.NavigationActivity;
 
-public class MainActivity extends NavigationActivity {
+import android.content.res.Configuration;
+import android.content.Context;
 
+public class MainActivity extends NavigationActivity {
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(newBase);
+        final Configuration override = new Configuration(newBase.getResources().getConfiguration());
+        override.fontScale = 1.0f;
+        applyOverrideConfiguration(override);
+    }
 }
