0fa44a5054290b8b09c3993d8465e59fc1705315
Vitaly Trus <vitaly@yulife.com>
2019-08-27 17:02:23 +0100

    Bugfix ENG-102: fontScale change

--- android/app/src/main/java/com/yulife/MainActivity.java
+++ android/app/src/main/java/com/yulife/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends NavigationActivity {
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(newBase);
         final Configuration override = new Configuration(newBase.getResources().getConfiguration());
-        override.fontScale = 1.0f;
+        override.fontScale = 0.88f; // makes it as it used to be before overriding
         applyOverrideConfiguration(override);
     }
 }
