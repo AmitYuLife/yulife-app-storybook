80b72e1fcca7b19e9d7257229215af5cdfb2efdb
Vitaly Trus <vitaly@yulife.com>
2019-08-29 13:39:10 +0100

    Chore: change back the fontScale

--- android/app/src/main/java/com/yulife/MainActivity.java
+++ android/app/src/main/java/com/yulife/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends NavigationActivity {
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(newBase);
         final Configuration override = new Configuration(newBase.getResources().getConfiguration());
-        override.fontScale = 0.88f; // makes it as it used to be before overriding
+        override.fontScale = 1.0f;
         applyOverrideConfiguration(override);
     }
 }
