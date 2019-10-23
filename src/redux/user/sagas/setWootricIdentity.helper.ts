9350d1e028675941877eb80b41754377ae03b71e
Simon Wilson <simon@yulife.com>
2019-10-23 10:27:02 +0100

    ENG-95 add killswitch for wootric via user feture toggles

--- src/redux/user/sagas/setWootricIdentity.helper.ts
+++ src/redux/user/sagas/setWootricIdentity.helper.ts
@@ -1,7 +1,11 @@
 import { GetCurrentUser_getCurrentUser } from "@graphql/_core/schema";
 import Wootric from "@services/wootric";
-import { call } from "redux-saga/effects";
+import { call, select } from "redux-saga/effects";
+import { getUserFeatures } from "../user.selectors";
 
 export default function* setWootricIdentity(user: GetCurrentUser_getCurrentUser) {
-    yield call(Wootric.setUserProperties, { ...user.business, ...user.coinLedger, ...user });
+    const features = yield select(getUserFeatures);
+    if (!features.suppressWootric) {
+        yield call(Wootric.setUserProperties, { ...user.business, ...user.coinLedger, ...user });
+    }
 }
