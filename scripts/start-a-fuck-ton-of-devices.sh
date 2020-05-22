7db5a2910e506704751b13b40e16faf3042d3826
Andrico Karoulla <andrico@yulife.com>
2020-05-22 13:29:06 +0000

    TASK: Create script to run multiple devices

---
+++ scripts/start-a-fuck-ton-of-devices.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+declare -A DEVICES=(["iPhone 8"]="ios" ["iPhone 8 Plus"]="ios" ["iPhone 11"]="ios" ["iPhone 11 Pro"]="ios" ["default"]="android")
+
+start_device() {
+  echo "Starting device $2 on $1"
+  if [[ "$1" == "android" ]]; then
+    yarn react-native run-$1 &
+  else
+    yarn react-native run-$1 "--simulator=$2"
+  fi
+}
+
+for key in "${!DEVICES[@]}"; do
+  start_device "${DEVICES[$key]}" "${key}"
+done
+
+exit 0
