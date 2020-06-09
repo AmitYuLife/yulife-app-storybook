1be16f61c74cb39b7121e7d3b63dfbbbdac824be
Vitaly Tr <vitaly@yulife.com>
2020-06-09 16:20:51 +0100

    Hotfix: steps not authorised crash

--- src/redux/pedometer/sagas/listenToSteps.helper.ts
+++ src/redux/pedometer/sagas/listenToSteps.helper.ts
@@ -3,41 +3,49 @@ import moment from "moment";
 import { call, cancelled, put, select, spawn, take } from "redux-saga/effects";
 import { getUserFeatures } from "../../user/user.selectors";
 import {
-    updatePedometerNoNewDataAction,
-    updatePedometerStartAction,
-    updatePedometerSuccessAction
+  updatePedometerNoNewDataAction,
+  updatePedometerStartAction,
+  updatePedometerSuccessAction,
 } from "../pedometer.actions";
 import { stepsChannel } from "../pedometer.channels";
 import { getSteps } from "../pedometer.selectors";
 
+const ERROR_NOT_AUTHORISED = "Pedometer not authorised";
+
 export default function* listenToSteps() {
-    yield put(updatePedometerStartAction());
+  yield put(updatePedometerStartAction());
 
-    const features = yield select(getUserFeatures);
-    const momentStartDay = moment().startOf("day");
-    const startOfDay = momentStartDay.format();
-    const channel = yield call(stepsChannel, startOfDay);
+  const features = yield select(getUserFeatures);
+  const momentStartDay = moment().startOf("day");
+  const startOfDay = momentStartDay.format();
+  const channel = yield call(stepsChannel, startOfDay);
 
-    while (true) {
-        try {
-            const results = yield take(channel);
-            const currentSteps = yield select(getSteps);
+  while (true) {
+    try {
+      const results = yield take(channel);
+      const currentSteps = yield select(getSteps);
 
-            if (features.loggingEnabled) {
-                yield spawn(() => Logger.logMixpanelEvent("raw_steps_results_passive", results));
-            }
+      if (results === ERROR_NOT_AUTHORISED) {
+        yield spawn(() => Logger.logMixpanelError(ERROR_NOT_AUTHORISED, "listenToSteps"));
+        yield put(updatePedometerNoNewDataAction());
+        continue;
+      }
 
-            if (results.steps !== currentSteps) {
-                yield put(updatePedometerSuccessAction(results));
-            } else {
-                yield put(updatePedometerNoNewDataAction());
-            }
-        } catch (e) {
-            yield spawn(() => Logger.logMixpanelError(e, "listenToSteps"));
-        } finally {
-            if (yield cancelled()) {
-                channel.close();
-            }
-        }
+      if (features.loggingEnabled) {
+        yield spawn(() => Logger.logMixpanelEvent("raw_steps_results_passive", results));
+      }
+
+      if (results.steps !== currentSteps) {
+        yield put(updatePedometerSuccessAction(results));
+      } else {
+        yield put(updatePedometerNoNewDataAction());
+      }
+    } catch (e) {
+      yield spawn(() => Logger.logMixpanelError(e, "listenToSteps"));
+    } finally {
+      if (yield cancelled()) {
+        channel.close();
+      }
     }
+  }
 }
