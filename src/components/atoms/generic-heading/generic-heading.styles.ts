944fd2851e298d3db5a6e6938cce5f91c652eaa1
John <john@yulife.com>
2020-06-26 09:42:33 +0000

    Bugfix TP-134: Fix avatar heading borders

--- src/components/atoms/generic-heading/generic-heading.styles.ts
+++ src/components/atoms/generic-heading/generic-heading.styles.ts
@@ -1,4 +1,4 @@
-import { StyleSheet, TextStyle, ViewStyle } from "react-native";
+import { StyleSheet, TextStyle, ViewStyle, Platform } from "react-native";
 import { Colours, Style } from "../../../styles";
 
 const styles = StyleSheet.create({
@@ -26,18 +26,36 @@ const styles = StyleSheet.create({
   } as ViewStyle,
   leftIcon: {
     position: "absolute",
-    top: Style.adjust(17),
+    top: Platform.select({ ios: Style.adjust(18), android: Style.SCALE_Y_UP_AND_DOWN(22) }),
     alignSelf: "flex-start",
-    height: 32,
-    width: 32,
+    height: Style.adjust(32),
+    width: Style.adjust(32),
   },
   rightIcon: {
     position: "absolute",
-    top: Style.SCALE_UP_AND_DOWN(10),
+    top: Style.adjust(10),
     alignSelf: "flex-end",
-    height: 32,
-    width: 32,
+    height: Style.adjust(32),
+    minWidth: Style.adjust(32),
   },
+  backAdjust: {
+    marginTop: Style.adjust(-4),
+  } as ViewStyle,
+  newBorderPad: {
+    height: 8,
+  } as ViewStyle,
+  newBorderShadow: {
+    position: "absolute",
+    height: 1,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "rgba(0,0,0,0.1)",
+  } as ViewStyle,
+  rightTextIcon: {
+    fontSize: Style.adjust(20),
+    marginTop: Style.adjust(6),
+  } as TextStyle,
 });
 
 export default styles;
