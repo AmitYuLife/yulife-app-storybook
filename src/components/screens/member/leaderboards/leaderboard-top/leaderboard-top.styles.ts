7db2ad7e01801a29d84cb09e30cac65109496df1
Vitaly Tr <vitaly@yulife.com>
2020-07-01 20:13:09 +0100

    Hotfix: Leaderboards Top 3 UI

--- src/components/screens/member/leaderboards/leaderboard-top/leaderboard-top.styles.ts
+++ src/components/screens/member/leaderboards/leaderboard-top/leaderboard-top.styles.ts
@@ -1,4 +1,5 @@
 import { StyleSheet, ViewStyle, Platform } from "react-native";
+import { hasNotch } from "react-native-device-info";
 import { Style } from "../../../../../styles";
 import { LIST_PAD_HEIGHT } from "../leaderboards.screen.styles";
 
@@ -13,27 +14,39 @@ export const avatarStyles: Record<string, ViewStyle> = StyleSheet.create({
     top: 28,
   } as ViewStyle,
   avatar2: {
-    marginTop: Style.SCALE_UP_AND_DOWN(Platform.select({ android: Style.isShortAndroid() ? 75 : 77, ios: 75 })),
-    marginLeft: Style.SCALE_UP_AND_DOWN(84),
+    marginTop: Style.SCALE_UP_AND_DOWN(
+      Platform.select({ android: Style.isShortAndroid() ? 73 : 75, ios: hasNotch() ? 73 : 75 })
+    ),
+    marginLeft: Style.SCALE_UP_AND_DOWN(71),
   } as ViewStyle,
   avatarEmpty2: {
-    marginTop: Style.SCALE_UP_AND_DOWN(Platform.select({ android: Style.isShortAndroid() ? 73 : 76, ios: 76 })),
-    marginLeft: Style.SCALE_UP_AND_DOWN(85),
+    marginTop: Style.SCALE_UP_AND_DOWN(
+      Platform.select({ android: Style.isShortAndroid() ? 72 : 74, ios: hasNotch() ? 71 : 73 })
+    ),
+    marginLeft: Style.SCALE_UP_AND_DOWN(80),
   } as ViewStyle,
   avatar1: {
-    marginTop: Style.SCALE_UP_AND_DOWN(Platform.select({ android: Style.isShortAndroid() ? 50 : 53, ios: 51 })),
-    marginLeft: Style.SCALE_UP_AND_DOWN(162),
+    marginTop: Style.SCALE_UP_AND_DOWN(
+      Platform.select({ android: Style.isShortAndroid() ? 49.5 : 51, ios: hasNotch() ? 49 : 51 })
+    ),
+    marginLeft: Style.SCALE_UP_AND_DOWN(159),
   } as ViewStyle,
   avatarEmpty1: {
-    marginTop: Style.SCALE_UP_AND_DOWN(Platform.select({ android: Style.isShortAndroid() ? 48 : 51.5, ios: 50 })),
-    marginLeft: Style.SCALE_UP_AND_DOWN(170),
+    marginTop: Style.SCALE_UP_AND_DOWN(
+      Platform.select({ android: Style.isShortAndroid() ? 47.5 : 49, ios: hasNotch() ? 47 : 49 })
+    ),
+    marginLeft: Style.SCALE_UP_AND_DOWN(167),
   } as ViewStyle,
   avatar3: {
-    marginTop: Style.SCALE_UP_AND_DOWN(Platform.select({ android: Style.isShortAndroid() ? 86 : 89, ios: 86 })),
-    marginLeft: Style.SCALE_UP_AND_DOWN(236),
+    marginTop: Style.SCALE_UP_AND_DOWN(
+      Platform.select({ android: Style.isShortAndroid() ? 84 : 86, ios: hasNotch() ? 84.5 : 86 })
+    ),
+    marginLeft: Style.SCALE_UP_AND_DOWN(244),
   } as ViewStyle,
   avatarEmpty3: {
-    marginTop: Style.SCALE_UP_AND_DOWN(Platform.select({ android: Style.isShortAndroid() ? 84 : 87, ios: 86 })),
-    marginLeft: Style.SCALE_UP_AND_DOWN(247),
+    marginTop: Style.SCALE_UP_AND_DOWN(
+      Platform.select({ android: Style.isShortAndroid() ? 83.5 : 85, ios: hasNotch() ? 83 : 85 })
+    ),
+    marginLeft: Style.SCALE_UP_AND_DOWN(252),
   } as ViewStyle,
 });
