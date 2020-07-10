dcf4003125e4645eceb5396d69eac59a30974d53
Cris Naranjo <cris@yulife.com>
2020-07-10 08:40:21 +0000

    Task: TP-193 Browse Packages fixes (FAQ)

--- src/components/screens/products/fib/faq/fib.faq.screen.tsx
+++ src/components/screens/products/fib/faq/fib.faq.screen.tsx
@@ -19,6 +19,7 @@ interface IFibFaqScreenProps {
 
 export const FibFaqScreen = memo(function (props: IFibFaqScreenProps) {
   const { onNavigateBack, faq, redirectToAnotherFAQ } = props;
+
   return (
     <View style={styles.wrapper}>
       <GenericHeading heading="FAQ" onLeftIconPress={onNavigateBack} />
@@ -26,7 +27,11 @@ export const FibFaqScreen = memo(function (props: IFibFaqScreenProps) {
         <Heading label={faq.question} style={styles.header} />
         <Markdown text={faq.answer} containerStyle={styles.markdownContainer} markdownStyles={styles} />
         {!redirectToAnotherFAQ ? null : (
-          <Faq label={redirectToAnotherFAQ.faq.question} onPress={redirectToAnotherFAQ.onPress} />
+          <Faq
+            label={redirectToAnotherFAQ.faq.question}
+            onPress={redirectToAnotherFAQ.onPress}
+            styles={redirectFAQStyles}
+          />
         )}
       </ScrollView>
     </View>
@@ -39,9 +44,11 @@ const styles = StyleSheet.create({
     top: Platform.select({ ios: Style.getSafeAreaStart(), android: 0 }),
     left: 0,
     right: 0,
+    height: "100%",
   },
   markdownContainer: {
     marginHorizontal: 32,
+    marginBottom: 32,
   },
   text: {
     fontFamily: Style.FONT_FAMILY_PRIMARY,
@@ -67,7 +74,7 @@ const styles = StyleSheet.create({
   listItem: {
     flexDirection: "row",
     justifyContent: "flex-start",
-    marginVertical: 8,
+    marginVertical: 4,
   },
   listItemBullet: {
     marginTop: 10,
@@ -76,5 +83,15 @@ const styles = StyleSheet.create({
     backgroundColor: "black",
     borderRadius: 2,
     marginRight: 10,
+  },
+});
+
+const redirectFAQStyles = StyleSheet.create({
+  wrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "white",
+    paddingLeft: Style.adjust(16),
+    minHeight: Style.adjust(64),
   },
 });
---
+++ src/components/screens/products/fib/faq/fib.faq.stories.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { storiesOf } from "@storybook/react-native";
+import { FibFaqScreen, IRedirectFAQ } from "./fib.faq.screen";
+import faqData from "@containers/products/fib/faq-fib-data";
+
+const fillers = {
+  onNavigateBack: (): null => null,
+  redirectToAnotherFAQ: null as IRedirectFAQ,
+};
+
+const getFaq = (searchedId: string) => faqData.find(({ id }) => id === searchedId);
+const faq = {
+  increaseCover: getFaq("increase-cover"),
+  salaryIncrease: getFaq("salary-increase"),
+};
+
+storiesOf("Fib Faq")
+  .add("with list items and subheading", () => <FibFaqScreen {...fillers} faq={faq.increaseCover} />)
+  .add("with redirect", () => (
+    <FibFaqScreen
+      {...fillers}
+      faq={faq.salaryIncrease}
+      redirectToAnotherFAQ={{ onPress: () => null, faq: faq.increaseCover }}
+    />
+  ));
