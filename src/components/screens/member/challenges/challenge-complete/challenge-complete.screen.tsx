dda1b764ce949c784cca3aba92dc588447159501
Boris <boris@yulife.com>
2020-07-31 17:27:08 +0000

    Add logs for timesup screen
    ENG-977

--- src/components/screens/member/challenges/challenge-complete/challenge-complete.screen.tsx
+++ src/components/screens/member/challenges/challenge-complete/challenge-complete.screen.tsx
@@ -1,9 +1,10 @@
-import * as React from "react";
+import React, { useEffect } from "react";
 import { Image, View } from "react-native";
 import { AnimatableImage } from "@services/animatable";
 import { GetMobileCopy_getMobileCopy_screens_challenges_completed } from "../../../../../graphql/_core/schema";
 import { Button, Text } from "../../../../atoms";
 import styles from "./challenge-complete.styles";
+import Logger from "@services/logging/logger";
 
 interface IProps {
   onCtaPress: () => void;
@@ -11,32 +12,38 @@ interface IProps {
   copy: GetMobileCopy_getMobileCopy_screens_challenges_completed;
 }
 
-const ChallengeCompleteScreen: React.SFC<IProps> = ({ isLoading, onCtaPress, copy }) => (
-  <View style={styles.wrapper}>
-    <View>
-      <AnimatableImage
-        useNativeDriver={true}
-        style={styles.alarm}
-        delay={1000}
-        iterationCount="infinite"
-        animation="swing"
-        source={require("../../../../../../assets/challenge-complete/clock.png")}
+const ChallengeCompleteScreen: React.SFC<IProps> = ({ isLoading, onCtaPress, copy }) => {
+  useEffect(() => {
+    Logger.logEvent("screen_view", { name: "timesUp" });
+  }, []);
+
+  return (
+    <View style={styles.wrapper}>
+      <View>
+        <AnimatableImage
+          useNativeDriver={true}
+          style={styles.alarm}
+          delay={1000}
+          iterationCount="infinite"
+          animation="swing"
+          source={require("../../../../../../assets/challenge-complete/clock.png")}
+        />
+        <Image style={styles.base} source={require("../../../../../../assets/challenge-complete/base.png")} />
+      </View>
+      <View style={styles.headingWrapper}>
+        <Text bold={true} style={styles.text}>
+          {copy.heading}
+        </Text>
+      </View>
+      <Button
+        isLoading={isLoading}
+        wrapperStyle={styles.ctaWrapper}
+        onPress={onCtaPress}
+        label={copy.ctaLabel}
+        type="PrimarySmall"
       />
-      <Image style={styles.base} source={require("../../../../../../assets/challenge-complete/base.png")} />
     </View>
-    <View style={styles.headingWrapper}>
-      <Text bold={true} style={styles.text}>
-        {copy.heading}
-      </Text>
-    </View>
-    <Button
-      isLoading={isLoading}
-      wrapperStyle={styles.ctaWrapper}
-      onPress={onCtaPress}
-      label={copy.ctaLabel}
-      type="PrimarySmall"
-    />
-  </View>
-);
+  );
+};
 
 export default ChallengeCompleteScreen;
