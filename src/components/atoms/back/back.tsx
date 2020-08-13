6c1f98c0952f6812bc92d3d31fd0fa60847bfcc4
Vitaly Trus <vitaly@yulife.com>
2020-08-13 18:37:56 +0000

    Chore: have one back button only

--- src/components/atoms/back/back.tsx
+++ src/components/atoms/back/back.tsx
@@ -2,10 +2,15 @@ import * as React from "react";
 import Svg, { Path } from "react-native-svg";
 import { BACK_BUTTON } from "@ids";
 
-function Back() {
+interface Props {
+  color?: string;
+  scale?: number;
+}
+
+function Back({ color = "#464647", scale = 1 }: Props) {
   return (
-    <Svg width="12" height="22" viewBox="0 0 12 22" fill="none" testID={BACK_BUTTON}>
-      <Path d="M11.25 0.5L0.75 11L11.25 21.5" stroke="#464647" strokeMiterlimit="10" strokeLinecap="round" />
+    <Svg height={String(22 * scale)} width={String(12 * scale)} viewBox="0 0 12 22" fill="none" testID={BACK_BUTTON}>
+      <Path d="M11.25 0.5L0.75 11L11.25 21.5" stroke={color} strokeMiterlimit="10" strokeLinecap="round" />
     </Svg>
   );
 }
--- src/components/organisms/top-bar/assets/index.ts
+++ src/components/organisms/top-bar/assets/index.ts
@@ -1,4 +1,3 @@
 export { default as Menu } from "./menu";
 export { default as Coins } from "./coins";
-export { default as Back } from "./back";
 export { default as Clock } from "./clock";
