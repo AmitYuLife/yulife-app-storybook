77dff7143066a9452c8ec640445d914df6246a51
Vitaly Trus <vitaly@yulife.com>
2020-09-01 11:30:54 +0000

    Chore: move previous question to topbar

--- src/components/screens/products/fib/underwriting-journey/fib.underwriting-journey.screen.tsx
+++ src/components/screens/products/fib/underwriting-journey/fib.underwriting-journey.screen.tsx
@@ -1,5 +1,5 @@
 import React, { memo } from "react";
-import { StyleSheet, ScrollView, ViewStyle, Platform } from "react-native";
+import { StyleSheet, ScrollView, ViewStyle } from "react-native";
 import { useBackHandler } from "../../../../../services/hooks/useBackHandler";
 import TitleWithIcon from "@atoms/fib/title-with-icon/title-with-icon";
 import FibTitle from "@atoms/fib/title/title";
@@ -24,6 +24,7 @@ import MedicalChipList from "./subcomponents/medical-chip-list/medical-chip-list
 import { AlcoholIntakeInput } from "@components/organisms/fib/input/alcohol/alcohol-intake-input";
 import { CopyBirthday } from "@organisms/fib/copy/birthday";
 import { CopyFullName } from "@organisms/fib/copy/full-name";
+import { Style } from "@styles";
 
 export interface IFibUnderwritingJourneyScreenProps {
   onNavigateBack: () => void;
@@ -68,32 +69,22 @@ const _FibUnderwritingJourneyScreen = memo(function (props: IFibUnderwritingJour
     return <CustomComponent {...props} />;
   }
 
-  let hideOnKeyboardOpen = false;
-
   return (
     <FibUnderwritingJourneyLayout
       heading={data.heading}
-      onNavigateBack={onNavigateBack}
+      onClose={onNavigateBack}
       progressBar={props.progressBar}
+      onPreviousQuestion={onPreviousButtonPressed}
     >
-      <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={styles.scrollViewContentStyle}>
+      <ScrollView keyboardShouldPersistTaps="handled" style={styles.wrapper}>
         <TitleWithIcon icon={data.icon} title={data.title} />
         <FibTitle title={data.question} />
         {data.children?.map((child: UnderwritingJourneyChild, i) => {
-          if (["inputHeight", "inputWeight"].includes(child.type) || Platform.OS === "android") {
-            hideOnKeyboardOpen = true;
-          }
-
           const key = data.id + i;
           return renderChildren(child, key);
         })}
       </ScrollView>
-      <Footer
-        firstButton={firstButton}
-        secondButton={secondButton}
-        onPreviousButtonPressed={onPreviousButtonPressed}
-        hideOnKeyboardOpen={hideOnKeyboardOpen}
-      />
+      <Footer firstButton={firstButton} secondButton={secondButton} />
     </FibUnderwritingJourneyLayout>
   );
 });
@@ -116,9 +107,11 @@ function renderChildren(child: UnderwritingJourneyChild, key: string) {
   return FIELDS[child.type] || null;
 }
 
+const MARGIN_BOTTOM = Style.hasNotch ? 60 : 100;
+
 const styles = StyleSheet.create({
-  scrollViewContentStyle: {
-    paddingBottom: 130,
+  wrapper: {
+    marginBottom: MARGIN_BOTTOM,
     paddingHorizontal: 32,
   } as ViewStyle,
 });
