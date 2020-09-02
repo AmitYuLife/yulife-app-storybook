c2e55ac358ad2475012c0e4d9564070a47965cb3
Leomar Alejar <leomar@yulife.com>
2020-09-02 15:28:34 +0000

    Feature TE-603: Update unity level gems

--- src/components/screens/member/quests/quests-scroll-screen/assets/level/level.content.tsx
+++ src/components/screens/member/quests/quests-scroll-screen/assets/level/level.content.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import { StyleSheet, View } from "react-native";
-import { getCurrentWorld } from "../../../../../../../services/utils";
+import { getCurrentWorld, getNormalizedLevel } from "../../../../../../../services/utils";
 import { Chest, DoubleLock, Lock, Text } from "../../../../../../atoms";
 import { IChallenge } from "../../quests-screen";
 import { getTime } from "./level.helpers";
 import LevelStar from "./level.star";
 import styles from "./level.styles";
+import { ForestGem, OceanGem, DesertGem, MountainGem } from "./level.gem";
 
 const getTextColor = (level: number) => {
   switch (true) {
@@ -32,41 +33,58 @@ const getStarColor = (level: number, isCompleted: boolean) => {
   }
 };
 
-const getLevelLockIcon = (currentLevel: number, level: IChallenge) => {
+const getLevelLockIcon = (currentLevel: number, normalizedLevel: number) => {
+  const normalizedCurrentLevel = getNormalizedLevel(currentLevel);
   switch (true) {
-    case level.level === 20 && currentLevel < 19:
-    case level.level === 41 && currentLevel < 40:
-    case level.level === 45 && currentLevel < 44:
-    case level.level === 48 && currentLevel < 47:
-    case level.level === 70 && currentLevel < 69:
-    case level.level === 91 && currentLevel < 89:
-    case level.level === 95 && currentLevel < 94:
-    case level.level === 98 && currentLevel < 97:
-    case level.level === 120 && currentLevel < 118:
-    case level.level === 145 && currentLevel < 144:
-    case level.level === 148 && currentLevel < 147:
-    case level.level === 170 && currentLevel < 169:
-    case level.level === 191 && currentLevel < 190:
-    case level.level === 195 && currentLevel < 194:
-    case level.level === 198 && currentLevel < 197:
-      return <DoubleLock colour={getLockColor(level.level)} />;
+    case normalizedLevel === 20 && normalizedCurrentLevel < 19:
+    case normalizedLevel === 41 && normalizedCurrentLevel < 40:
+    case normalizedLevel === 45 && normalizedCurrentLevel < 44:
+    case normalizedLevel === 48 && normalizedCurrentLevel < 47:
+    case normalizedLevel === 70 && normalizedCurrentLevel < 69:
+    case normalizedLevel === 91 && normalizedCurrentLevel < 89:
+    case normalizedLevel === 95 && normalizedCurrentLevel < 94:
+    case normalizedLevel === 98 && normalizedCurrentLevel < 97:
+    case normalizedLevel === 120 && normalizedCurrentLevel < 118:
+    case normalizedLevel === 145 && normalizedCurrentLevel < 144:
+    case normalizedLevel === 148 && normalizedCurrentLevel < 147:
+    case normalizedLevel === 170 && normalizedCurrentLevel < 169:
+    case normalizedLevel === 191 && normalizedCurrentLevel < 190:
+    case normalizedLevel === 195 && normalizedCurrentLevel < 194:
+    case normalizedLevel === 198 && normalizedCurrentLevel < 197:
+      return <DoubleLock colour={getLockColor(normalizedLevel)} />;
     default:
-      return <Lock colour={getLockColor(level.level)} />;
+      return <Lock colour={getLockColor(normalizedLevel)} />;
   }
 };
 
-export default function getLevelButton(nextAvailable: number, currentLevel: number, level: IChallenge): any {
+export default function getLevelButton(
+  nextAvailable: number,
+  currentLevel: number,
+  level: IChallenge,
+  normalizedLevel: number
+): any {
   // step right up, we have more horrible logic, come and see the horrible logic!
   if (level.level % 50 === 0) {
     if (level.level > currentLevel) {
-      return <Lock colour={getLockColor(level.level)} />;
+      return <Lock colour={getLockColor(normalizedLevel)} />;
     }
 
-    return (
-      <Text style={styles.text} bold={true}>
-        {level.level}
-      </Text>
-    );
+    switch (normalizedLevel) {
+      case 50:
+        return <ForestGem />;
+      case 100:
+        return <OceanGem />;
+      case 150:
+        return <DesertGem />;
+      case 200:
+        return <MountainGem />;
+      default:
+        return (
+          <Text style={styles.text} bold={true}>
+            {level.level}
+          </Text>
+        );
+    }
   }
 
   if (level.level === currentLevel) {
@@ -102,13 +120,13 @@ export default function getLevelButton(nextAvailable: number, currentLevel: numb
   }
 
   if (level.level < currentLevel) {
-    const color = getTextColor(level.level);
+    const color = getTextColor(normalizedLevel);
     return (
       <View style={{ flexDirection: "column" }}>
         <Text style={StyleSheet.flatten([styles.text, { textAlign: "center", color }])}>{level.level}</Text>
         <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
           {Array.from({ length: 3 }).map((_, i) => (
-            <LevelStar key={`${level.id}_${i}`} colour={getStarColor(level.level, level.rating > i)} />
+            <LevelStar key={`${level.id}_${i}`} colour={getStarColor(normalizedLevel, level.rating > i)} />
           ))}
         </View>
       </View>
@@ -116,10 +134,10 @@ export default function getLevelButton(nextAvailable: number, currentLevel: numb
   }
 
   if (level.isChestLevel) {
-    return <Chest colour={getLockColor(level.level)} />;
+    return <Chest colour={getLockColor(normalizedLevel)} />;
   }
 
-  return getLevelLockIcon(currentLevel, level);
+  return getLevelLockIcon(currentLevel, normalizedLevel);
 }
 
 function getLockColor(level: number) {
@@ -127,7 +145,9 @@ function getLockColor(level: number) {
     return "rgb(118, 82, 48)";
   }
 
-  switch (getCurrentWorld(level)) {
+  const world = getCurrentWorld(level);
+
+  switch (world) {
     case 3:
       return "rgb(87, 133, 188)";
     case 2:
