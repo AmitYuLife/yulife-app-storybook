e3e4e472dc911882239cbcef4aec6bf8eab45c94
John <john@yulife.com>
2020-09-17 13:58:30 +0000

    Bugfix: Make keyboard adaptive for external links

--- src/redux/user/sagas/openMemberZone.saga.ts
+++ src/redux/user/sagas/openMemberZone.saga.ts
@@ -6,6 +6,14 @@ import { handleOpenWebView } from "@navigation/utils";
 export default function* openMemberZoneSaga() {
   try {
     const { data } = yield call(getMagicLinkWithClient);
+
+    if (!data?.getMagicLink) {
+      // guard even w/ low chance of happening
+      // if user was able to sign in to the app,
+      // they should be able to get a magic link
+      return;
+    }
+
     yield call(handleOpenWebView, { uri: data.getMagicLink, title: "member zone" });
   } catch (e) {
     yield spawn(() => Logger.logMixpanelError(e, "openMemberZone"));
