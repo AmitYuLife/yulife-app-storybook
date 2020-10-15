8d0bc1e24b596f0dd4592f6b625bf67e9ce4a8cc
Leomar Alejar <leomar@yulife.com>
2020-10-15 13:29:52 +0000

    Bugfix TE-623: Duel fixes

--- src/components/screens/member/duels-hub/subcomponents/duel-tabs/duels-tabs.styles.ts
+++ src/components/screens/member/duels-hub/subcomponents/duel-tabs/duels-tabs.styles.ts
@@ -1,15 +1,13 @@
 import { StyleSheet, ViewStyle } from "react-native";
 import { Colours } from "@styles";
 
-export const DUEL_TAB_HEIGHT = 72;
-
 export default StyleSheet.create({
   wrapper: {
     display: "flex",
     flexDirection: "row",
     width: "100%",
     backgroundColor: Colours.neutral.white,
-    height: DUEL_TAB_HEIGHT,
+    height: 72,
   } as ViewStyle,
   tab: {
     paddingRight: 24,
