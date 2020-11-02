f4baf899e4db7ee2687a57a168923cb267b32e06
Leomar Alejar <leomar@yulife.com>
2020-11-02 10:55:51 +0000

    Bugfix TE-623: Duels fixes

--- src/components/screens/member/duels-hub/subcomponents/duel-heading/duels-heading.styles.ts
+++ src/components/screens/member/duels-hub/subcomponents/duel-heading/duels-heading.styles.ts
@@ -3,17 +3,11 @@ import { StyleSheet, TextStyle } from "react-native";
 
 export default StyleSheet.create({
   heading: {
-    fontFamily: Style.FONT_FAMILY_PRIMARY_BOLD,
-    fontStyle: "normal",
     fontSize: 22,
     lineHeight: 24,
     letterSpacing: 1,
     color: "#5A5A5C",
     paddingVertical: Style.adjust(24),
     backgroundColor: Colours.neutral.white,
-  } as TextStyle,
-  inactive: {
-    fontFamily: Style.FONT_FAMILY_PRIMARY,
-    fontWeight: "normal",
   } as TextStyle,
 });
