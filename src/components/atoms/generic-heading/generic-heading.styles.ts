2660cafcbfe8ba48cd56a98220bb122a8906b831
John Patrick Salcedo <john@yulife.com>
2020-11-02 02:50:02 +0000

    Adjust FIB Top Bar size

--- src/components/atoms/generic-heading/generic-heading.styles.ts
+++ src/components/atoms/generic-heading/generic-heading.styles.ts
@@ -1,21 +1,21 @@
-import { StyleSheet, TextStyle, ViewStyle } from "react-native";
-import { Colours, Style } from "../../../styles";
+import { StyleSheet, TextStyle, ViewStyle, Platform } from "react-native";
+import { Colours, Style } from "@styles";
 
-export const GENERIC_HEADING_HEIGHT = 56;
+export const GENERIC_HEADING_HEIGHT = Style.adjust(56);
 
 const styles = StyleSheet.create({
   wrapper: {
     height: GENERIC_HEADING_HEIGHT,
-    paddingVertical: 16,
+    paddingVertical: Style.adjust(16),
     flexDirection: "row",
     alignContent: "center",
     borderBottomColor: "rgba(0,0,0,0.1)",
-    paddingHorizontal: 16,
-    marginTop: Style.isAnyIphoneX() ? -10 : 0,
+    paddingHorizontal: Style.adjust(16),
+    marginTop: Platform.OS === "ios" && Style.hasNotch ? -10 : 0,
   } as ViewStyle,
   leftIconWrapper: {
     alignSelf: "center",
-    width: 32,
+    width: Style.adjust(32),
   } as ViewStyle,
   centerWrapper: { flexDirection: "row", flex: 1, justifyContent: "center" } as ViewStyle,
   relative: { position: "relative" } as ViewStyle,
@@ -24,14 +24,15 @@ const styles = StyleSheet.create({
   heading: {
     color: Colours.products.fib.n900,
     letterSpacing: 1,
-    lineHeight: 24,
+    fontSize: Style.adjust(20),
+    lineHeight: Style.adjust(24),
     fontFamily: Style.FONT_FAMILY_PRIMARY_BOLD,
   } as TextStyle,
   rightIconWrapper: { width: 32, alignSelf: "center" } as ViewStyle,
   rightIconTouchable: { alignSelf: "flex-end" },
   rightIconText: {
-    fontSize: 20,
-    lineHeight: 22,
+    fontSize: Style.adjust(20),
+    lineHeight: Style.adjust(22),
     letterSpacing: 1,
     fontFamily: Style.FONT_FAMILY_PRIMARY,
     flexWrap: "nowrap",
