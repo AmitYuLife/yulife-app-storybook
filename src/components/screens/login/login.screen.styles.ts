1bf3460743a1b5b0655554bee583d9db56162377
Jeff Filho <jeff@yulife.com>
2020-12-16 16:35:40 +0000

    Bugfix ENG-1481: Nonsensical copy in the "Welcome" screen of the app

--- src/components/screens/login/login.screen.styles.ts
+++ src/components/screens/login/login.screen.styles.ts
@@ -1,11 +1,7 @@
 import { StyleSheet } from "react-native";
-import { Style } from "../../../styles";
 
 export default StyleSheet.create({
   flex: {
     flex: 1,
-  },
-  blurbWrapper: {
-    paddingHorizontal: Style.SCALE_UP_AND_DOWN(85),
   },
 });
