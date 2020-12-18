790b47c2ab60026fb31f2eec37c48f5f9d6e55d5
Osama <osama@yulife.com>
2020-12-18 11:52:32 +0000

    Copy update

--- e2e/00_Smoke/01_login_and_routing/_steps/given.ts
+++ e2e/00_Smoke/01_login_and_routing/_steps/given.ts
@@ -47,7 +47,7 @@ export const loginToDailySteps = async (): Promise<void> => {
 }
 
 export const onLoginScreen = async () => {
-    const welcomeLabel = element(by.label("welcome!"));
+    const welcomeLabel = element(by.label("Welcome!"));
     const loginField = element(by.id(INPUT_LOGIN_EMAIL));
     const passwordField = element(by.id(INPUT_LOGIN_PASSWORD("Password")));
     const needHelp = "need help logging in?"
