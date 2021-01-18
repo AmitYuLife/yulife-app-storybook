230a41377a48ca3ce9036444460aaaa6bfabf5bd
Vitaly Tr <vitaly@yulife.com>
2021-01-18 08:59:44 +0000

    Hotfix: removed the network state listener

--- src/redux/app/sagas/listenToNetworkState.saga.ts
+++ src/redux/app/sagas/listenToNetworkState.saga.ts
@@ -1,20 +1,9 @@
 import NetInfo, { NetInfoState } from "@react-native-community/netinfo";
-import { call, put, take, spawn } from "redux-saga/effects";
-import Logger from "@services/logging/logger";
+import { call, put } from "redux-saga/effects";
 import { updateOfflineState } from "../app.actions";
-import { appNetworkChannel } from "../app.channels";
 
 export default function* listenToNetworkStateSaga() {
   const connectionInfo: NetInfoState = yield call(NetInfo.fetch);
 
   yield put(updateOfflineState(!connectionInfo.isConnected));
-
-  const networkChannel = yield call(appNetworkChannel);
-
-  while (true) {
-    const network: NetInfoState = yield take(networkChannel);
-
-    const networkType = connectionInfo.type;
-    yield spawn(() => Logger.logMixpanelEvent("app_network_changed", { networkType, network }));
-  }
 }
