3bed1fe112efce6f6cd8ad2fb623233a39fe6f9d
Dan Greane <daniel@greane.co.uk>
2021-02-27 14:47:37 +0000

    [HOTFIX] payment method no longer needed for detox

--- e2e/_utils/data/index.ts
+++ e2e/_utils/data/index.ts
@@ -24,7 +24,6 @@ export * from "./postgres/business";
 export * from "./postgres/customers";
 export * from "./postgres/business_employees";
 export * from "./postgres/customers"
-export * from "./postgres/payment_method"
 export * from "./postgres/consent"
 export * from "./postgres/customer_product_entity"
 export * from "./postgres/customer_quote"
