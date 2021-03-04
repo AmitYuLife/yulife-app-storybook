ff7359ee2ac78221a265bffa401c59885e560750
Osama <osama@yulife.com>
2021-03-04 11:17:09 +0000

    added customer_quote_screening

---
+++ e2e/_utils/data/postgres/customer_quote_screening.ts
@@ -0,0 +1,12 @@
+import { IDatabaseItem } from "@yu-life/yulife-bdd-framework";
+import moment from "moment";
+import { CUSTOMER_9 } from "./customers";
+
+
+export const QUOTE_SCREENING_1 = {
+    type:"postgres",
+    modelName:"customer_quote_screening",
+    data:{
+        quote_id:CUSTOMER_9.data.customerId,
+    }
+} as IDatabaseItem
