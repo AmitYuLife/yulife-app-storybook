f77725fbbd4a5d1a557b09496fcd1f9ea50422f6
Cris Naranjo <cris@yulife.com>
2021-03-11 14:59:05 +0000

    Task ENG-1822: Request rewards when view is on screen

---
+++ src/services/hooks/useQueryOnceScreenSeen.ts
@@ -0,0 +1,34 @@
+import { OperationVariables } from "@apollo/react-common";
+import { LazyQueryHookOptions, QueryTuple, useLazyQuery } from "@apollo/react-hooks";
+import { getRouteState } from "@redux/app/app.selectors";
+import { DocumentNode } from "graphql";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+
+/**
+ *
+ * @param gqlQuery GQL query
+ * @param activeView ROUTES or MODALS values
+ */
+export function useQueryOnceScreenSeen<T = any, TVariables = OperationVariables>(
+  gqlQuery: DocumentNode,
+  screenName: string,
+  queryOptions: LazyQueryHookOptions<T, TVariables> = {}
+): QueryTuple<T, TVariables> {
+  const currentScreen = useSelector(getRouteState);
+  const isScreenActive = currentScreen === screenName;
+
+  const [query, queryResult] = useLazyQuery<T, TVariables>(gqlQuery, {
+    fetchPolicy: "cache-and-network",
+    ...queryOptions,
+  });
+  const { data } = queryResult;
+
+  useEffect(() => {
+    if (!data && isScreenActive) {
+      query();
+    }
+  }, [data, query, isScreenActive]);
+
+  return [query, queryResult];
+}
