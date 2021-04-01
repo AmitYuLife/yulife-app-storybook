dd668dc8064801f46d20dd8c77f7d681be06d5b3
John <john@yulife.com>
2021-04-01 10:59:34 +0000

    Feature TP-751: Redesign Apple Health prompt after first permission request

--- src/components/organisms/daily-steps/daily-steps-content/subcontainers/fitkit-unauthorised.tsx
+++ src/components/organisms/daily-steps/daily-steps-content/subcontainers/fitkit-unauthorised.tsx
@@ -1,25 +1,19 @@
 import React from "react";
-import { connect } from "react-redux";
-import { Button, Text } from "@atoms";
-import { Alert, Platform, StyleSheet, TextStyle } from "react-native";
+import { Alert, StyleSheet, TextStyle, Platform, View } from "react-native";
+import { Button, TextTemplate } from "@atoms";
 import { Style } from "@styles";
-import { getCopy } from "@redux/copy/copy.selectors";
-import { IReduxState } from "@redux/_core/reducers";
 import { openGoogleFit } from "@services/app-link";
-import { androidAlertCopy, fitKitNotAuthorizedCopy } from "./copy";
+import { androidAlertCopy, getFitKitNotAuthorizedCopy } from "./copy";
 
-interface OwnProps {
+interface Props {
   onPress: () => void;
+  hasRequestedPermission: boolean;
 }
 
-type ConnectedProps = ReturnType<typeof mapStateToProps>;
+export const FitkitUnauthorised = ({ onPress, hasRequestedPermission }: Props) => {
+  const { buttonLabel, message } = getFitKitNotAuthorizedCopy(hasRequestedPermission);
 
-type Props = OwnProps & ConnectedProps;
-
-const _FitkitUnauthorised = ({ onPress }: Props) => {
-  const { buttonLabel, message } = fitKitNotAuthorizedCopy;
-
-  const _onPress = () => {
+  const handlePress = () => {
     const { title, message: alertMessage, dismissLabel, downloadLabel, confirmLabel } = androidAlertCopy;
     if (Platform.OS === "android") {
       const buttons = [
@@ -43,26 +37,19 @@ const _FitkitUnauthorised = ({ onPress }: Props) => {
 
   return (
     <>
-      <Text style={styles.permissionText}>{message}</Text>
-      <Button label={buttonLabel} onPress={_onPress} type="Primary" size="Medium" />
+      <View style={styles.wrapper}>
+        <TextTemplate textAlign="center" type="b2">
+          {message}
+        </TextTemplate>
+      </View>
+      <Button label={buttonLabel} onPress={handlePress} type="Primary" size="Medium" />
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  permissionText: {
-    color: "rgb(96,96,96)",
-    lineHeight: Style.SCALE_UP_AND_DOWN(22),
+  wrapper: {
     marginBottom: Style.SCALE_UP_AND_DOWN(20),
-    textAlign: "center",
     width: Style.SCALE_UP_AND_DOWN(300),
   } as TextStyle,
 });
-
-const mapStateToProps = (state: IReduxState) => ({
-  copy: getCopy(state, "dailyStepsFitKitAuthorise"),
-});
-
-const redux = connect<ConnectedProps, null, OwnProps>(mapStateToProps);
-
-export const FitkitUnauthorised = redux(_FitkitUnauthorised);
