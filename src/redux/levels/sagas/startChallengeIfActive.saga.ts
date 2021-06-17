f120bb4c5cc90f535a7ad25c6d03c03738aae72f
John <john@yulife.com>
2021-06-17 15:51:15 +0100

    wrap startChallengeIfActiveSaga with try-catch, send error to Bugsnag

--- src/redux/levels/sagas/startChallengeIfActive.saga.ts
+++ src/redux/levels/sagas/startChallengeIfActive.saga.ts
@@ -1,16 +1,21 @@
+import Logger from "@services/logging/logger";
 import { call, select } from "redux-saga/effects";
 import { getActiveLevel } from "../levels.selectors";
 import startChallenge from "./startChallenge.helper";
 
 export default function* startChallengeIfActiveSaga() {
-  const { endDateTime, levelSlotId, startDateTime, status, subtype, timeUp } = yield select(getActiveLevel);
+  try {
+    const { endDateTime, levelSlotId, startDateTime, status, subtype, timeUp } = yield select(getActiveLevel);
 
-  if (levelSlotId && !timeUp && !status) {
-    yield call(startChallenge, {
-      endDateTime,
-      subtype,
-      levelSlotId,
-      startDateTime,
-    });
+    if (levelSlotId && !timeUp && !status) {
+      yield call(startChallenge, {
+        endDateTime,
+        subtype,
+        levelSlotId,
+        startDateTime,
+      });
+    }
+  } catch (error) {
+    Logger.error(error, { file: "startChallengeIfActive" });
   }
 }
