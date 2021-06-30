89d6db4d35c1c0e3832007898a985b686bede75f
Boris <boris@yulife.com>
2021-06-30 12:00:17 +0000

    Task TP-874: Move content button to common folder

--- src/components/atoms/icon/arrow-right.tsx
+++ src/components/atoms/icon/arrow-right.tsx
@@ -6,7 +6,7 @@ interface IProps {
   color?: string;
 }
 
-export const ArrowRight = memo(({ color = Colours.neutral.n700 }: IProps) => (
+export const ArrowRight = memo(({ color = Colours.primary.p600 }: IProps) => (
   <Svg width={Style.adjust(24)} height={Style.adjust(24)} viewBox="0 0 24 24">
     <Path d="M8 20l8-8-8-8" stroke={color} strokeMiterlimit={10} strokeLinecap="round" strokeLinejoin="round" />
   </Svg>
