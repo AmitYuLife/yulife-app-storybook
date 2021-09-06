b797d0f3e996423d69f700e4c4f69212114dfba1
Jeff Filho <jeff@yulife.com>
2021-09-06 12:11:27 +0100

    Bugfix: TextTemplate shouldnt extends TextProps

--- src/components/atoms/text/text-template.tsx
+++ src/components/atoms/text/text-template.tsx
@@ -1,10 +1,10 @@
 import React, { memo } from "react";
-import { StyleProp, StyleSheet, Text, TextProps, TextStyle } from "react-native";
+import { StyleProp, StyleSheet, Text, TextStyle } from "react-native";
 import { Style, Colours } from "@styles";
 
 export type ITextTemplateType = "h1" | "h2" | "h3" | "b1" | "b1b" | "b2" | "b2b" | "l1" | "l1b" | "l2" | "l2b" | "l3";
 
-interface IProps extends TextProps {
+interface IProps {
   testID?: string;
   children: React.ReactNode;
   type: ITextTemplateType;
