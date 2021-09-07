88f8b6f86d2320f66f3c3a4ecef9b5842554d771
Jeff Filho <jeff@yulife.com>
2021-09-07 09:53:26 +0100

    Task GS-80: Update Health Syncing Copy

--- src/components/screens/onboarding/fitkit-connect/copy.ts
+++ src/components/screens/onboarding/fitkit-connect/copy.ts
@@ -12,6 +12,6 @@ const healthAppName = Platform.select({ ios: "Apple Health", android: "Google Fi
 const heading = Platform.select({ ios: "Sync to Apple Health", android: "Connect to Google Fit" });
 export const fitKitConnectCopy = {
   heading,
-  connectMessage: `By clicking connect you are allowing YuLife to access your health data from ${healthAppName}, so that we can track your daily activities and provide you with rewards. You will be able to adjust what data we can see in your settings.`,
+  connectMessage: `Connect to ${healthAppName} so we can reward you for your daily activities. You can adjust this in your device settings at any time.`,
   connectButton: "Let's connect",
 };
