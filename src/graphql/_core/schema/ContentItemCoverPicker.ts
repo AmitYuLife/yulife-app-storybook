2fbffef3236a2ab41dd8d64e9aca620572096e55
Vitaly Tr <vitaly@yulife.com>
2021-09-19 14:35:04 +0100

    Chore: unify select cover

--- src/graphql/_core/schema/ContentItemCoverPicker.ts
+++ src/graphql/_core/schema/ContentItemCoverPicker.ts
@@ -3,7 +3,7 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { CoverType } from "./globalTypes";
+import { CoverType, ContentItemButtonType, ContentItemSDUIAction, ContentItemButtonSize } from "./globalTypes";
 
 // ====================================================
 // GraphQL fragment: ContentItemCoverPicker
@@ -21,10 +21,118 @@ export interface ContentItemCoverPicker_options {
   heading: string;
 }
 
+export interface ContentItemCoverPicker_coverPickerTitle_styles {
+  property: string;
+  value: string;
+}
+
+export interface ContentItemCoverPicker_coverPickerTitle {
+  id: string;
+  textType: string;
+  text: string;
+  colour: string | null;
+  textAlign: string | null;
+  underline: boolean | null;
+  numberOfLines: number | null;
+  styles: ContentItemCoverPicker_coverPickerTitle_styles[] | null;
+}
+
+export interface ContentItemCoverPicker_customCover_button_onPress {
+  type: ContentItemSDUIAction;
+  payload: string | null;
+}
+
+export interface ContentItemCoverPicker_customCover_button_icon {
+  id: string;
+  uri: string | null;
+}
+
+export interface ContentItemCoverPicker_customCover_button_rightIcon {
+  id: string;
+  uri: string | null;
+}
+
+export interface ContentItemCoverPicker_customCover_button_styles {
+  property: string;
+  value: string;
+}
+
+export interface ContentItemCoverPicker_customCover_button {
+  id: string;
+  label: string;
+  buttonType: ContentItemButtonType | null;
+  value: string | null;
+  onPress: ContentItemCoverPicker_customCover_button_onPress | null;
+  icon: ContentItemCoverPicker_customCover_button_icon | null;
+  rightIcon: ContentItemCoverPicker_customCover_button_rightIcon | null;
+  styles: ContentItemCoverPicker_customCover_button_styles[] | null;
+  buttonSize: ContentItemButtonSize | null;
+}
+
+export interface ContentItemCoverPicker_customCover_itemsPicker_styles {
+  property: string;
+  value: string;
+}
+
+export interface ContentItemCoverPicker_customCover_itemsPicker_range {
+  min: number;
+  max: number;
+  step: number;
+}
+
+export interface ContentItemCoverPicker_customCover_itemsPicker_styleVariants_item {
+  color: string;
+}
+
+export interface ContentItemCoverPicker_customCover_itemsPicker_styleVariants_overlay_backdropStyles {
+  property: string;
+  value: string;
+}
+
+export interface ContentItemCoverPicker_customCover_itemsPicker_styleVariants_overlay_overlayTitleWrapperStyles {
+  property: string;
+  value: string;
+}
+
+export interface ContentItemCoverPicker_customCover_itemsPicker_styleVariants_overlay {
+  backdropStyles: ContentItemCoverPicker_customCover_itemsPicker_styleVariants_overlay_backdropStyles[] | null;
+  highlightLabel: string;
+  highlightLabelColor: string;
+  overlayTitle: string;
+  overlayTitleWrapperStyles:
+    | ContentItemCoverPicker_customCover_itemsPicker_styleVariants_overlay_overlayTitleWrapperStyles[]
+    | null;
+}
+
+export interface ContentItemCoverPicker_customCover_itemsPicker_styleVariants {
+  id: string;
+  minVisibleIndex: number | null;
+  maxVisibleIndex: number | null;
+  item: ContentItemCoverPicker_customCover_itemsPicker_styleVariants_item;
+  overlay: ContentItemCoverPicker_customCover_itemsPicker_styleVariants_overlay;
+}
+
+export interface ContentItemCoverPicker_customCover_itemsPicker {
+  id: string;
+  answerKey: string;
+  styles: ContentItemCoverPicker_customCover_itemsPicker_styles[] | null;
+  range: ContentItemCoverPicker_customCover_itemsPicker_range;
+  styleVariants: ContentItemCoverPicker_customCover_itemsPicker_styleVariants[] | null;
+}
+
+export interface ContentItemCoverPicker_customCover {
+  title: string;
+  button: ContentItemCoverPicker_customCover_button;
+  itemsPicker: ContentItemCoverPicker_customCover_itemsPicker;
+}
+
 export interface ContentItemCoverPicker {
   id: string;
   answerKey: string;
   answerKeyDefaultValue: number;
+  hasSelectedCustomCover: boolean;
   styles: ContentItemCoverPicker_styles[] | null;
   options: ContentItemCoverPicker_options[] | null;
+  coverPickerTitle: ContentItemCoverPicker_coverPickerTitle;
+  customCover: ContentItemCoverPicker_customCover | null;
 }
