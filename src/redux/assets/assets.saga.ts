4cd6d6a120e133a8054d0295cd16bb6f4e447b88
Ivan Varga <ivan@yulife.com>
2021-09-20 12:44:06 +0000

    Task: Batching assets preload

--- src/redux/assets/assets.saga.ts
+++ src/redux/assets/assets.saga.ts
@@ -1,15 +1,21 @@
 import getMobileAssets from "@graphql/assets/getMobileAssets.gql";
 import Logger from "@services/logging/logger";
 import FastImage from "react-native-fast-image";
-import { call, spawn, takeLatest } from "redux-saga/effects";
+import { call, delay, spawn, takeLatest } from "redux-saga/effects";
 import { AUTHENTICATED } from "../app/app.actions";
+
+const BATCH_SIZE = 10;
+const PRELOAD_TIMEOUT = 10000; //ms
 
 export function* prefetchAssets() {
   try {
     const { data } = yield call(getMobileAssets);
 
     if (data && data.getMobileAssets) {
-      FastImage.preload(data.getMobileAssets);
+      for (let i = 0; i < data.getMobileAssets.length; i += BATCH_SIZE) {
+        FastImage.preload(data.getMobileAssets.slice(i, i + BATCH_SIZE));
+        yield delay(PRELOAD_TIMEOUT);
+      }
     }
   } catch (e) {
     yield spawn(() => {
