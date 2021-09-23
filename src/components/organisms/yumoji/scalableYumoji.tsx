a43427ee74912b670a4bdc2e401755edcd8a3727
Ivan Varga <ivan@yulife.com>
2021-09-24 01:46:57 +0200

    scalable yumoji

---
+++ src/components/organisms/yumoji/scalableYumoji.tsx
@@ -0,0 +1,50 @@
+import { CroppedImage } from "@components/screens/member/yu-screen/yumoji-builder/components/croppedImage";
+import React, { memo, useMemo } from "react";
+import { StyleSheet, View } from "react-native";
+
+interface IYumojiPart {
+  order?: number;
+  remoteUrl: {
+    uri: string;
+    width: number;
+    height: number;
+  };
+}
+
+interface IProps {
+  items: IYumojiPart[];
+  width: number;
+  height: number;
+  preview?: {
+    top: number;
+    left: number;
+    zoom: number;
+  };
+}
+
+function _Yumoji(props: IProps) {
+  const { items, preview = { top: 0, left: 0, zoom: 1 }, height, width } = props;
+
+  const parts = useMemo(() => items.sort(({ order: order1 = 0 }, { order: order2 = 0 }) => order1 - order2), [items]);
+  return (
+    <View style={{ width, height }}>
+      {parts.map(({ remoteUrl: { uri, width: partWidth, height: partHeight } }) =>
+        !uri ? null : (
+          <View key={uri} style={[StyleSheet.absoluteFillObject, { width, height }]}>
+            <CroppedImage
+              previewPosition={preview}
+              key={uri}
+              width={partWidth}
+              height={partHeight}
+              containerWidth={width}
+              containerHeight={height}
+              source={{ uri }}
+            />
+          </View>
+        )
+      )}
+    </View>
+  );
+}
+
+export const Yumoji = memo(_Yumoji);
