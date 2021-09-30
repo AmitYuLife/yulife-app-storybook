1abaa188ea151739105ef636767398a5cd4825c2
John <john@yulife.com>
2021-09-30 12:50:58 +0000

    Bugfix: Prevent WhatsNew modal overlap

--- src/redux/onboarding/sagas/getMobileWhatsNewModal.saga.ts
+++ src/redux/onboarding/sagas/getMobileWhatsNewModal.saga.ts
@@ -1,10 +1,12 @@
 import FastImage from "react-native-fast-image";
 import { Navigation } from "react-native-navigation";
-import { call, delay, spawn } from "redux-saga/effects";
+import { call, delay, select, spawn, take } from "redux-saga/effects";
 import { getMobileWhatsNewModalClient } from "@graphql/onboardingSteps/getMobileWhatsNewModal.gql";
 import { MODALS } from "@navigation/constants";
 import Logger from "@services/logging/logger";
 import { Unpacked } from "@utils";
+import { getModalState } from "@redux/app/app.selectors";
+import { UPDATE_CURRENT_MODAL } from "@redux/app/app.actions";
 
 export function* getMobileWhatsNewModalSaga() {
   try {
@@ -14,6 +16,11 @@ export function* getMobileWhatsNewModalSaga() {
       const preloadAssets = data.getMobileWhatsNewModal.items.map((item) => item.backgroundImage);
       yield call(FastImage.preload, preloadAssets);
       yield delay(1000);
+
+      const activeModal: ReturnType<typeof getModalState> = yield select(getModalState);
+      if (activeModal) {
+        yield take(UPDATE_CURRENT_MODAL);
+      }
 
       yield call(() =>
         Navigation.showModal({
