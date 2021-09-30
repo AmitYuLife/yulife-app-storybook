030b109bceea25ca14cae17f6c79e4b3ef69f311
Jeff Filho <jeff@yulife.com>
2021-09-30 12:40:56 +0000

    Bugfix: Use TextTemplate on SlotIcon and improve letterSpacing on Android devices

--- src/components/atoms/text/text-template.tsx
+++ src/components/atoms/text/text-template.tsx
@@ -1,8 +1,21 @@
 import React, { memo } from "react";
-import { ColorValue, StyleSheet, Text, TextStyle } from "react-native";
+import { ColorValue, StyleSheet, Text, TextStyle, Platform } from "react-native";
 import { Style, Colours } from "@styles";
 
-export type ITextTemplateType = "h1" | "h2" | "h3" | "b1" | "b1b" | "b2" | "b2b" | "l1" | "l1b" | "l2" | "l2b" | "l3";
+export type ITextTemplateType =
+  | "h1"
+  | "h2"
+  | "h3"
+  | "b1"
+  | "b1b"
+  | "b2"
+  | "b2b"
+  | "l1"
+  | "l1b"
+  | "l2"
+  | "l2b"
+  | "l3"
+  | "l3b";
 
 interface IProps {
   testID?: string;
@@ -33,6 +46,9 @@ export const TextTemplate = memo(
   }
 );
 
+const getLetterSpacing = (spacing: number) =>
+  Platform.select({ ios: spacing, android: Number((spacing - 0.1).toFixed(1)) });
+
 export const styles = StyleSheet.create({
   default: {
     fontFamily: Style.FONT_FAMILY_PRIMARY,
@@ -41,68 +57,74 @@ export const styles = StyleSheet.create({
     fontFamily: Style.FONT_FAMILY_PRIMARY_BOLD,
     fontSize: Style.adjust(32),
     lineHeight: Style.adjust(40),
-    letterSpacing: Style.adjust(0.9),
+    letterSpacing: getLetterSpacing(Style.adjust(0.9)),
   },
   h2: {
     fontFamily: Style.FONT_FAMILY_PRIMARY_BOLD,
     fontSize: Style.adjust(28),
     lineHeight: Style.adjust(32),
-    letterSpacing: Style.adjust(0.9),
+    letterSpacing: getLetterSpacing(Style.adjust(0.9)),
   },
   h3: {
     fontFamily: Style.FONT_FAMILY_PRIMARY_BOLD,
     fontSize: Style.adjust(24),
     lineHeight: Style.adjust(32),
-    letterSpacing: Style.adjust(0.9),
+    letterSpacing: getLetterSpacing(Style.adjust(0.9)),
   },
   b1: {
     fontSize: Style.adjust(20),
     lineHeight: Style.adjust(24),
-    letterSpacing: Style.adjust(0.7),
+    letterSpacing: getLetterSpacing(Style.adjust(0.7)),
   },
   b1b: {
     fontFamily: Style.FONT_FAMILY_PRIMARY_BOLD,
     fontSize: Style.adjust(20),
     lineHeight: Style.adjust(24),
-    letterSpacing: Style.adjust(0.7),
+    letterSpacing: getLetterSpacing(Style.adjust(0.7)),
   },
   b2: {
     fontSize: Style.adjust(16),
     lineHeight: Style.adjust(24),
-    letterSpacing: Style.adjust(0.6),
+    letterSpacing: getLetterSpacing(Style.adjust(0.6)),
   },
   b2b: {
     fontFamily: Style.FONT_FAMILY_PRIMARY_BOLD,
     fontSize: Style.adjust(16),
     lineHeight: Style.adjust(24),
-    letterSpacing: Style.adjust(0.5),
+    letterSpacing: getLetterSpacing(Style.adjust(0.5)),
   },
   l1: {
     fontSize: Style.adjust(14),
     lineHeight: Style.adjust(18),
-    letterSpacing: Style.adjust(0.3),
+    letterSpacing: getLetterSpacing(Style.adjust(0.3)),
   },
   l1b: {
     fontFamily: Style.FONT_FAMILY_PRIMARY_BOLD,
     fontSize: Style.adjust(14),
     lineHeight: Style.adjust(18),
-    letterSpacing: Style.adjust(0.3),
+    letterSpacing: getLetterSpacing(Style.adjust(0.3)),
   },
   l2: {
     fontSize: Style.adjust(12),
     lineHeight: Style.adjust(16),
-    letterSpacing: Style.adjust(0.3),
+    letterSpacing: getLetterSpacing(Style.adjust(0.3)),
   },
   l2b: {
     fontFamily: Style.FONT_FAMILY_PRIMARY_BOLD,
     fontSize: Style.adjust(12),
     lineHeight: Style.adjust(16),
-    letterSpacing: Style.adjust(0.3),
+    letterSpacing: getLetterSpacing(Style.adjust(0.3)),
   },
   l3: {
     fontSize: Style.adjust(10),
     lineHeight: Style.adjust(16),
-    letterSpacing: Style.adjust(0.3),
+    letterSpacing: getLetterSpacing(Style.adjust(0.3)),
+  },
+  l3b: {
+    fontFamily: Style.FONT_FAMILY_PRIMARY_BOLD,
+    fontSize: Style.adjust(10),
+    lineHeight: Style.adjust(16),
+    letterSpacing: getLetterSpacing(Style.adjust(0.3)),
   },
   underline: {
     textDecorationLine: "underline",
