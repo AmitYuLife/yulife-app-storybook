de2c98ce4b0ade8d1bbf3581fad8b0a2a2d79c08
Michael McCabe <michael.mccabe@yulife.com>
2021-10-13 13:25:10 +0000

    Task XSE-424: Refresh YuCoin on PLI close

---
+++ src/graphql/user/getTotalCoins.ts
@@ -0,0 +1,17 @@
+import gql from "graphql-tag";
+import client from "../_core/client";
+import { GetTotalCoins } from "../_core/schema";
+
+export const GQL_QUERY_GET_TOTAL_COINS = gql`
+  query GetTotalCoins {
+    getTotalCoins
+  }
+`;
+
+export default function getTotalCoins() {
+  return client().query<GetTotalCoins>({
+    fetchPolicy: "network-only",
+    query: GQL_QUERY_GET_TOTAL_COINS,
+    variables: {},
+  });
+}
