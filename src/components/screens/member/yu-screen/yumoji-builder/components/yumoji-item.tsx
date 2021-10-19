660601dde308a1dc89fe9b33143e8221be92a5e3
Ivan Varga <ivan@yulife.com>
2021-10-19 09:20:22 +0000

    Bugfix GS-129: Yumoji - skin colour not loading

--- src/components/screens/member/yu-screen/yumoji-builder/components/yumoji-item.tsx
+++ src/components/screens/member/yu-screen/yumoji-builder/components/yumoji-item.tsx
@@ -1,11 +1,14 @@
-import React, { memo, useCallback, useMemo, useState } from "react";
+import React, { memo, useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { View, StyleSheet, ViewStyle } from "react-native";
 import { BoxOption, SkeletonLoading } from "@atoms";
 import { CroppedImage } from "./croppedImage";
 import ColorPreview from "./colorPreview";
 import { YumojiItemLabel } from "./yumoji-item-label";
 import { Colours, Style } from "@styles";
-import { GetYumojiBuilderItemsForCategory_getYumojiBuilderItemsForCategory_items as YumojiBuilderItemsForCategoryItems } from "@graphql/_core/schema";
+import {
+  GetYumojiBuilderItemsForCategory_getYumojiBuilderItemsForCategory_items as YumojiBuilderItemsForCategoryItems,
+  GetYumojiBuilderItemsForCategory_getYumojiBuilderItemsForCategory_items_parts as YumojiBuilderItemsForCategoryItemsParts,
+} from "@graphql/_core/schema";
 import { shallowEqual } from "react-redux";
 import FastImage from "react-native-fast-image";
 
@@ -26,20 +29,32 @@ const previewSize = boxWidth - Style.adjust(8);
 
 export const YumojiItem = memo(
   ({ item, loading, onItemPress }: IProps) => {
-    const [partsLoading, setPartsLoading] = useState(item?.parts?.filter((part) => part?.remoteUrl?.uri).length || 0);
+    const [partsLoading, setPartsLoading] = useState(true);
+    const loadingCounter = useRef(0);
     const onPress = useCallback(() => {
       onItemPress(item);
     }, [onItemPress, item]);
+
+    useEffect(() => {
+      // using ref to avoid multiple rerenders of items with multiple parts
+      loadingCounter.current = item?.parts?.filter((part) => part?.remoteUrl?.uri)?.length || 0;
+      if (loadingCounter.current === 0) {
+        setPartsLoading(false);
+      }
+    }, [item?.parts]);
 
     const partLoader = useMemo(() => {
       return (
         <View style={styles.hiddenImageStyle}>
           {item?.parts
-            ?.filter((loadingItem) => loadingItem?.remoteUrl?.uri)
+            ?.filter((part) => part?.remoteUrl?.uri)
             ?.map(({ remoteUrl: { uri } }) => (
               <FastImage
                 onLoad={() => {
-                  setPartsLoading((stillLoading) => stillLoading - 1);
+                  loadingCounter.current -= 1;
+                  if (loadingCounter.current <= 0) {
+                    setPartsLoading(false);
+                  }
                 }}
                 key={uri}
                 source={{ uri }}
@@ -90,8 +105,22 @@ export const YumojiItem = memo(
       </View>
     );
   },
-  ({ item: prevItem, ...prevProps }, { item: nextItem, ...nextProp }) =>
-    shallowEqual(prevProps, nextProp) && shallowEqual(prevItem, nextItem)
+  (
+    {
+      item: { parts: prevParts, ...prevItemProps } = { parts: [] as YumojiBuilderItemsForCategoryItemsParts[] },
+      ...prevProps
+    },
+    {
+      item: { parts: nextParts, ...nextItemProps } = { parts: [] as YumojiBuilderItemsForCategoryItemsParts[] },
+      ...nextProp
+    }
+  ) =>
+    shallowEqual(prevProps, nextProp) &&
+    shallowEqual(prevItemProps, nextItemProps) &&
+    shallowEqual(
+      prevParts?.map((p1) => p1?.remoteUrl?.uri),
+      nextParts?.map((p2) => p2?.remoteUrl?.uri)
+    )
 );
 
 const styles = StyleSheet.create({
