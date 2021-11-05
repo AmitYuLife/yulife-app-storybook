ef7cda95f6b8207420232bb2a5fd13101f93125e
Harry Todd <harry@yulife.com>
2021-11-05 10:49:05 +0000

    Detox: Change toggle from hasFibActive to hasCoveaFibActive

--- e2e/02_Extended/12_pli_journey/12_pli_journey_happy.spec.ts
+++ e2e/02_Extended/12_pli_journey/12_pli_journey_happy.spec.ts
@@ -7,7 +7,7 @@ import * as helper from "./_steps/helpers"
 import { CUSTOMER_37, AUTH_37 } from "@data";
 
 Feature("PLI POSITVE", async()=>{
-    Scenario("As a completely healthy male user with FIB enabled, I should be able to purchase PLI", scenario.start, async()=>{
+    Scenario("As a completely healthy male user with Covea FIB enabled, I should be able to purchase PLI", scenario.start, async()=>{
         Given("I login as a user with yuscreen v3", given.loginToYuScreen(true, CUSTOMER_37, AUTH_37), async()=>{
             Then("I should be on the yuscreen", then.onYuscreenV3(CUSTOMER_37))
             When("I create the default yumoji", when.createDefaultYumoji, async () => {
