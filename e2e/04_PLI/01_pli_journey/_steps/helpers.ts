d6af2c645d45cd36f26107b8d3497a81a91d2e26
Harry Todd <harry@yulife.com>
2021-11-16 12:04:24 +0000

    Detox: Fix PLI

--- e2e/04_PLI/01_pli_journey/_steps/helpers.ts
+++ e2e/04_PLI/01_pli_journey/_steps/helpers.ts
@@ -53,13 +53,19 @@ export const UNDERWRITING_NAME = async () => {
 }
 
 export const UNDERWRITING_DOB = async (age: number) => {
-    const nextScreen = (age > 65 || age < 18) ? "Sorry about this!" : "Enter your salary"
+    const nextScreen = (age > 65 || age < 18) ? "Please confirm your date of birth" : "Enter your salary"
 
     When("I choose the correct date of birth", when.chooseCorrectDoB(age), async () => {
         Then("I should be back on the dob screen", then.isOnDoBScreen)
         When("I tap Continue", when.scrollToAndTapText(PRODUCT_STEP_BODY_SCROLL_VIEW, "Continue", "down"), async () => {
             Then("I should be on the next screen", then.isOnScreen(nextScreen));
         })
+    })
+}
+
+export const UNDERWRITING_CONFIRM_DOB = async () => {
+    When("I tap Yes, this is my date of birth", when.tapText("Yes, this is my date of birth"), async () => {
+        Then("I should be on the rejection screen", then.isOnScreen("Sorry about this!"))
     })
 }
 
@@ -405,8 +411,7 @@ export const REVIEW_SCREEN = async () => {
 type coverLevel = "common" | "rare" | "epic"
 
 export const COVER_SELECTION = async (cover: coverLevel) => {
-    Then("I should be on the cover level screen", then.isOnCoverLevelScreen)
-    When(`I tap ${cover}`, when.tapID(SELECTED_PACKAGE_TITLE(cover)), async () => {
+    When(`I tap ${cover}`, [then.isOnCoverLevelScreen, when.tapID(SELECTED_PACKAGE_TITLE(cover))], async () => {
         When("I scroll to the bottom and tap Continue to checkout", when.scrollToAndTapText(PRODUCT_STEP_BODY_SCROLL_VIEW, "Continue to checkout", "down"), async () => {
             Then("I should be on the next page", then.textNotVisible("Choose your cover level"))
         })
@@ -453,7 +458,7 @@ type RejectionScreen = "Age" | "Answers"
 
 export const REJECTED = async (screen: RejectionScreen, date?: string, time?: string) => {
     const screenText = (screen === "Age") 
-        ? "Based on your age we are unable to offer you personal life insurance." 
+        ? "Unfortunately we are unable to offer personal life insurance to anyone under the age of 18, please try again in future!" 
         : "Based on your answers, we’re not able to offer you personal life insurance right now."
 
     When("I wait 5 seconds", when.wait(5000), async () => {
