33de3b3f8431448267d16e23e723007d56eeaa6b
Eugeniu Grosu <eugeniu@yulife.com>
2021-11-24 06:15:46 +0000

    detox:fix the matcher when go to yucoin core screen of app

--- e2e/00_Smoke/02_routing/02_routing.spec.ts
+++ e2e/00_Smoke/02_routing/02_routing.spec.ts
@@ -3,7 +3,7 @@ import * as scenario from "./_steps/scenario"
 import * as given from "./_steps/given"
 import * as when from "./_steps/when"
 import * as then from "./_steps/then"
-import { YUCOIN, BUTTON_CLOSE, QUESTS_SCREEN, NAV_BAR, LEADERBOARD_SCREEN, REWARDS_SCREEN, MENU_ICON, STATS_SCREEN, ACTIVITY_HISTORY_SCREEN, SETTINGS_SCREEN, YUMATTER_SCREEN, LEVEL_CHALLENGE_BUTTON, CHALLENGE_SET, CHALLENGE_TILE, GENERIC_SCREEN_HEADING, GENERIC_SCREEN_CTA, CHALLENGE_PROGRESS_BAR, BUTTON_CLOSE_CHALLENGE, CHECK_REWARDS_BUTTON, LEADERBOARD_TOP_SCREEN, BUTTON_CLOSE_HEADER, SETTINGS_NAME, SETTINGS_DESC, SETTINGS_SWITCH } from "@ids";
+import { YUCOIN, BUTTON_CLOSE, QUESTS_SCREEN, NAV_BAR, LEADERBOARD_SCREEN, REWARDS_SCREEN, MENU_ICON, STATS_SCREEN, ACTIVITY_HISTORY_SCREEN, SETTINGS_SCREEN, YUMATTER_SCREEN, LEVEL_CHALLENGE_BUTTON, CHALLENGE_SET, CHALLENGE_TILE, GENERIC_SCREEN_HEADING, GENERIC_SCREEN_CTA, CHALLENGE_PROGRESS_BAR, BUTTON_CLOSE_CHALLENGE, CHECK_REWARDS_BUTTON, LEADERBOARD_TOP_SCREEN, BUTTON_CLOSE_HEADER, SETTINGS_NAME, SETTINGS_DESC, SETTINGS_SWITCH, BACK_BUTTON } from "@ids";
 import { REWARDS_JOHN_LEWIS, REAWARDS_AVIOS, REWARDS_BLOOM_UNAVAILABLE, CUSTOMER_2, AUTH_2, CUSTOMER_8, AUTH_8, CUSTOMER_35, AUTH_35 } from "@data";
 Feature("As a user I can navigate through member routes correctly", async () => {
 
@@ -13,7 +13,7 @@ Feature("As a user I can navigate through member routes correctly", async () => 
             When("I tap the yucoin", when.tapID(YUCOIN), async () => {
                 Then("I should be on the yucoin screen", then.onTodaysYucoin())
             })
-            When("I close this screen", when.tapID(BUTTON_CLOSE), async () => {
+            When("I close this screen", when.tapID(BACK_BUTTON), async () => {
                 Then("I should see the daily steps screen", then.onDailySteps())
             })
             When("I tap quests in the tab", when.tapID(NAV_BAR("quests")), async () => {
--- e2e/00_Smoke/09_appreview/09_appreview.spec.ts
+++ e2e/00_Smoke/09_appreview/09_appreview.spec.ts
@@ -11,8 +11,7 @@ Feature("App store review should behave correctly", async()=>{
      
     Scenario("App store review pop up and i should tap Yeah!", scenario.start, async () => {
         Given("I am in the app should pop up app review", given.loginAsUser(CUSTOMER_36, AUTH_36), async () => {
-            Then("I wait for 3 sec", then.wait(3000))  
-            Then("I should see text Enjoying YuLife?", then.idVisible(APPREVIEW_TEXT("Enjoying YuLife?")))
+            Then("I should see text Enjoying YuLife?", then.idVisible(APPREVIEW_TEXT("Enjoying YuLife?"), 5000))
             Then("I should see text (We’d love to know either way!)", then.textVisible("(We’d love to know either way!)"))           
             When("I tap the button", when.tapText("Yeah!",1,true), async () => {
                 Then("I should not see a screen asking me Enjoying YuLife?", then.idNotVisible(APPREVIEW_TEXT("Enjoying YuLife?")))
@@ -24,8 +23,7 @@ Feature("App store review should behave correctly", async()=>{
     })
     Scenario("App store review pop up and i should tap on Not really and after that Give feedback", scenario.start, async () => {
         Given("I am in the app should pop up app review", given.loginAsUser(CUSTOMER_36, AUTH_36), async () => {    
-            Then("I wait for 3 sec", then.wait(3000))
-            Then("I should see text Enjoying YuLife?", then.idVisible(APPREVIEW_TEXT("Enjoying YuLife?")))
+            Then("I should see text Enjoying YuLife?", then.idVisible(APPREVIEW_TEXT("Enjoying YuLife?"), 5000))
             When("I tap the button Not really", when.tapText("Not really",1,true), async () => {
                 Then("I should see text We’re sorry to hear that", then.idVisible(APPREVIEW_TEXT("We’re sorry to hear that")))
                 Then("I should see text We’d love a chance to do better. Would you mind leaving us a few tips?", then.textVisible("We’d love a chance to do better. Would you mind leaving us a few tips?"))
@@ -41,15 +39,14 @@ Feature("App store review should behave correctly", async()=>{
 
     Scenario("App store review pop up and i should tap on Not really and after that No thanks", scenario.start, async () => {
         Given("I am in the app should pop up app review", given.loginAsUser(CUSTOMER_36, AUTH_36), async () => {    
-            When("I wait for 3 seconds", when.wait(6000), async () => {
-                Then("I should see text Enjoying YuLife?", then.idVisible(APPREVIEW_TEXT("Enjoying YuLife?")))
+                Then("I should see text Enjoying YuLife?", then.idVisible(APPREVIEW_TEXT("Enjoying YuLife?"), 5000))
                 When("I tap the button Not really", when.tapText("Not really",1,true), async () => {
                     Then("I should see text We’d love a chance to do better. Would you mind leaving us a few tips?", then.textVisible("We’d love a chance to do better. Would you mind leaving us a few tips?"))
                     When("I tap the button Give feedback", when.tapText("No thanks",1,true), async () => {
                         Then("I should  see the daily steps screen", then.onDailySteps())
                         Then("I should  not see intercom", then.textNotVisible("Start a conversation"))
                         When("I close and reopen the app", when.reloadOnly, async()=>{  
-                            When("I wait for 3 seconds", when.wait(6000), async () => {
+                            When("I wait for 6 seconds", when.wait(6000), async () => {
                                 Then("I should not see a screen asking me Enjoying YuLife?", then.idNotVisible(APPREVIEW_TEXT("Enjoying YuLife?")))
                             })
                         })
@@ -58,4 +55,3 @@ Feature("App store review should behave correctly", async()=>{
             })
         })
     })
-})
