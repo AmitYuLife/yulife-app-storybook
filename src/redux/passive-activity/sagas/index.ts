c9e95ab8a5d599e2df5091d45163d5665c32ee00
Boris <boris@yulife.com>
2021-11-26 12:37:57 +0000

    Revert passive last update call during authenticated

--- src/redux/passive-activity/sagas/index.ts
+++ src/redux/passive-activity/sagas/index.ts
@@ -1,5 +1,5 @@
 import { takeLatest } from "redux-saga/effects";
-import { UPDATE_APP_STATE } from "../../app/app.actions";
+import { AUTHENTICATED } from "../../app/app.actions";
 import sendPassiveActivity from "./sendPassiveActivitySinceLastUpdate.saga";
 
-export default [takeLatest(UPDATE_APP_STATE, sendPassiveActivity)];
+export default [takeLatest(AUTHENTICATED, sendPassiveActivity)];
