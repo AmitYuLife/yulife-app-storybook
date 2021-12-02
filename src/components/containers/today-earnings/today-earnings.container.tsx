14811381a5aa73f6228a4680b8c038bc26e40c10
Jeff Filho <jeff@yulife.com>
2021-12-02 15:35:05 +0000

    Bugfix GS-115: Update steps on Todays Earning Screen

--- src/components/containers/today-earnings/today-earnings.container.tsx
+++ src/components/containers/today-earnings/today-earnings.container.tsx
@@ -1,12 +1,16 @@
-import { useQuery } from "@apollo/react-hooks";
+import { useLazyQuery, useMutation } from "@apollo/react-hooks";
 import { TodayEarningLoadingScreen, TodayEarningsScreen } from "@components/screens";
 import { GQL_QUERY_GET_TODAY_EARNINGS } from "@graphql/todayEarnings";
-import { GetTodayEarnings } from "@graphql/_core/schema";
+import { GetTodayEarnings, UpsertPassiveChallenge, UpsertPassiveChallengeVariables } from "@graphql/_core/schema";
 import RNFitKit from "@yu-life/react-native-fitkit";
 import React, { useCallback, useEffect, useState } from "react";
 import { Navigation } from "react-native-navigation";
-import { FitKitType } from "@graphql/_core/schema/globalTypes";
+import { FitKitType, PassiveChallengeType } from "@graphql/_core/schema/globalTypes";
 import { useFitKit } from "@services/fitkit/fitkit.hooks";
+import { useSelector } from "react-redux";
+import { getDailySteps } from "@redux/daily-steps/daily-steps.selectors";
+import moment from "moment";
+import { GQL_MUTATION_UPSERT_PASSIVE_CHALLENGE } from "@graphql/challenges";
 
 interface IProps {
   componentId: string;
@@ -16,12 +20,29 @@ const TodayEarningsContainer = ({ componentId }: IProps) => {
   const [permissionIsLoading, setPermissionIsLoading] = useState(true);
   const [isGoogleFitAuthorised, setIsGoogleFitAuthorised] = useState(false);
   const { authoriseFitKitTypes } = useFitKit();
-  const { data, loading } = useQuery<GetTodayEarnings>(GQL_QUERY_GET_TODAY_EARNINGS, {
+  const dailySteps = useSelector(getDailySteps);
+  const [upsertPassiveChallenge] = useMutation<UpsertPassiveChallenge, UpsertPassiveChallengeVariables>(
+    GQL_MUTATION_UPSERT_PASSIVE_CHALLENGE
+  );
+  const [getTodaysEarnings, { data, loading }] = useLazyQuery<GetTodayEarnings>(GQL_QUERY_GET_TODAY_EARNINGS, {
     fetchPolicy: "cache-and-network",
   });
 
   useEffect(() => {
     (async () => {
+      await upsertPassiveChallenge({
+        variables: {
+          payload: [
+            {
+              value: dailySteps,
+              endDateTime: moment().format(),
+              startDateTime: moment().startOf("day").format(),
+            },
+          ],
+          type: PassiveChallengeType.STEPS,
+        },
+      });
+      await getTodaysEarnings();
       const googleFit = await RNFitKit.isAuthorised({
         read: [],
         platform: "GoogleFit",
