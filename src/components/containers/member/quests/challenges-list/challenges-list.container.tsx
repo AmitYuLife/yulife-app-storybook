ecd01853595239979ee2977210210974f8f5c931
Jeff Filho <jeff@yulife.com>
2021-12-15 14:27:05 +0000

    Bugfix GS-195: Close button position on Samsung s20

--- src/components/containers/member/quests/challenges-list/challenges-list.container.tsx
+++ src/components/containers/member/quests/challenges-list/challenges-list.container.tsx
@@ -20,6 +20,7 @@ import { bottomTabs, MODALS, ROUTES } from "@navigation/constants";
 import RNFitKit from "@yu-life/react-native-fitkit";
 import { FitKitType } from "@graphql/_core/schema/globalTypes";
 import { useFitKit } from "@services/fitkit/fitkit.hooks";
+import { showYuModal } from "@navigation/root";
 
 interface IProps {
   componentId: string;
@@ -135,7 +136,7 @@ const ChallengesListContainer: FC<Props> = ({ level, componentId }) => {
                       nonSamsungHealthTypesThatRequirePermissions.includes(type)
                     );
                     if (!activityFromGoogleFitAuthorised && isOtherTypesThenSteps && isSamsung()) {
-                      await Navigation.showModal({
+                      await showYuModal({
                         component: {
                           id: MODALS.switchToGoogleFit,
                           name: MODALS.switchToGoogleFit,
