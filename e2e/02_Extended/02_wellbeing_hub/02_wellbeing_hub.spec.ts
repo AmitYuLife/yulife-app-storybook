2a65aba26297dd0bdb49c4969eb0606e5f337832
Eugeniu Grosu <eugeniu@yulife.com>
2022-01-31 15:52:41 +0000

    detox: fix the failing assertion during the weekend

--- e2e/02_Extended/02_wellbeing_hub/02_wellbeing_hub.spec.ts
+++ e2e/02_Extended/02_wellbeing_hub/02_wellbeing_hub.spec.ts
@@ -71,8 +71,11 @@ Feature("Wellbeing Hub should be restricted for certain users", async () => {
                                 When("I tap Close", when.navigateViaButton("Close"), async () => {
                                     Then("I should see Activate your Fiit account", then.textVisible("Activate your Fiit account"))
                                     When("I replace the first name", when.replaceTextViaID(INPUT_AVIOS_FORM_FIELD("First name"), CUSTOMER_37.data.firstName), async () => {
+                                        Then("I should see my first name has been updated", then.textVisible(CUSTOMER_37.data.firstName, 2000))
                                         When("I replace the last name", when.replaceTextViaID(INPUT_AVIOS_FORM_FIELD("Last name"), CUSTOMER_37.data.lastName), async () => {
+                                            Then("I should see my last name has been updated", then.textVisible(CUSTOMER_37.data.lastName, 2000))
                                             When("I replace the email", when.replaceTextViaID(INPUT_AVIOS_FORM_FIELD("Email address"), CUSTOMER_37.data.email), async () => {
+                                                Then("I should see my email has been updated", then.textVisible(CUSTOMER_37.data.email, 2000))
                                                 When("I tap Active your Fiit account", when.navigateViaButton("Active account"), async () => {
                                                     Then("I should see Membership claimed", then.textVisible("Membership claimed"))
                                                     Then("I should see that membership is ready", then.textVisible("Your Fiit membership is ready! Fiit will send you an email invitation how to log in and get started. Please check your email account."))
