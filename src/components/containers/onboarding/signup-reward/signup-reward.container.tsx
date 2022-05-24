5c9331e68729bf790c703845c8db4e91eb0b0925
Vitaly Trus <vitaly@yulife.com>
2022-05-24 14:55:07 +0000

    Task: signup reward copy

--- src/components/containers/onboarding/signup-reward/signup-reward.container.tsx
+++ src/components/containers/onboarding/signup-reward/signup-reward.container.tsx
@@ -1,42 +1,25 @@
 import * as React from "react";
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setAuthenticatedRoot } from "@navigation/root";
-import { IReduxState } from "@redux/_core/reducers";
 import { setAuthenticated } from "@redux/app/app.actions";
-import { getCopy } from "@redux/copy/copy.selectors";
 import { getIsOnboarding, getOnboardingReward } from "@redux/onboarding/onboarding.selectors";
 import { SignUpRewardScreen } from "@screens";
 
 // TODO find where these props actually come from in RNN types
-interface IProps {
+interface Props {
   componentId: string;
 }
 
-type ConnectedState = ReturnType<typeof mapStateToProps>;
-type ConnectedDispatch = typeof mapDispatchToProps;
-
-type Props = IProps & ConnectedState & ConnectedDispatch;
-
-const SignUpRewardContainer: React.FC<Props> = (props) => {
-  const { copy, isLoading, yucoinAwarded } = props;
+const SignUpRewardContainer: React.FC<Props> = () => {
+  const dispatch = useDispatch();
+  const yuCoinAwarded = useSelector(getOnboardingReward);
+  const isLoading = useSelector(getIsOnboarding);
 
   const onCollectPress = async () => {
-    await setAuthenticatedRoot(props.setAuthenticated);
+    await setAuthenticatedRoot(() => dispatch(setAuthenticated()));
   };
 
-  return (
-    <SignUpRewardScreen isLoading={isLoading} onCollectPress={onCollectPress} reward={yucoinAwarded} copy={copy} />
-  );
+  return <SignUpRewardScreen isLoading={isLoading} onCollectPress={onCollectPress} yuCoin={yuCoinAwarded} />;
 };
 
-const mapStateToProps = (state: IReduxState) => ({
-  copy: getCopy(state, "signupReward"),
-  yucoinAwarded: getOnboardingReward(state),
-  isLoading: getIsOnboarding(state),
-});
-
-const mapDispatchToProps = {
-  setAuthenticated,
-};
-
-export default connect<ConnectedState, ConnectedDispatch>(mapStateToProps, mapDispatchToProps)(SignUpRewardContainer);
+export default React.memo(SignUpRewardContainer);
--- src/components/screens/onboarding/signup-reward/signup-reward.screen.tsx
+++ src/components/screens/onboarding/signup-reward/signup-reward.screen.tsx
@@ -1,32 +1,43 @@
 import * as React from "react";
-import { FC } from "react";
-import { GetMobileCopy_getMobileCopy_screens_signupReward as SignUpRewardCopy } from "@graphql/_core/schema";
 import { Blurb, Heading, Pad } from "@atoms";
 import { Button } from "@molecules";
 import styles from "./signup-reward.screen.styles";
 import { CoinConfetti, CentredScreen } from "@molecules";
+import { useTranslation } from "@hooks";
 
 interface IProps {
   isLoading: boolean;
   onCollectPress: () => void;
-  reward: number;
-  copy: SignUpRewardCopy;
+  yuCoin: number;
 }
 
-const SignUpRewardScreen: FC<IProps> = ({ isLoading, onCollectPress, reward }) => (
-  <CentredScreen footerImage="forest">
-    <Pad height={74} />
-    <CoinConfetti coins={reward} />
-    <Heading style={styles.heading} bold={true} label={"Sign-up bounty\nunlocked!"} />
-    <Pad height={16} />
-    <Blurb
-      textStyle={styles.message}
-      wrapperStyle={styles.messageWrapper}
-      label={"You can exchange your YuCoin for vouchers from Amazon, Asos, M&S, and many more!"}
-    />
-    <Pad height={40} />
-    <Button label={"Next"} onPress={onCollectPress} isLoading={isLoading} disabled={isLoading} />
-  </CentredScreen>
-);
+const SignUpRewardScreen: React.FC<IProps> = ({ isLoading, onCollectPress, yuCoin }) => {
+  const translations = useTranslation([
+    "screens.signupReward.heading",
+    "screens.signupReward.subheading",
+    "screens.signupReward.ctaLabel",
+  ]);
 
-export default SignUpRewardScreen;
+  return (
+    <CentredScreen footerImage="forest">
+      <Pad height={74} />
+      <CoinConfetti coins={yuCoin} />
+      <Heading style={styles.heading} bold={true} label={translations["screens.signupReward.heading"]} />
+      <Pad height={16} />
+      <Blurb
+        textStyle={styles.message}
+        wrapperStyle={styles.messageWrapper}
+        label={translations["screens.signupReward.subheading"]}
+      />
+      <Pad height={40} />
+      <Button
+        label={translations["screens.signupReward.ctaLabel"]}
+        onPress={onCollectPress}
+        isLoading={isLoading}
+        disabled={isLoading}
+      />
+    </CentredScreen>
+  );
+};
+
+export default React.memo(SignUpRewardScreen);
