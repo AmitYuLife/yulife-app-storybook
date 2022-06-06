0c50c37dcb7a625641b443eed8820ada9266954d
Jeff Filho <jeff@yulife.com>
2022-06-06 11:21:54 +0100

    Bugfix: Storybook aliases

---
+++ .storybook/aliases/react-native-encrypted-storage.js
@@ -0,0 +1,8 @@
+export const StorageErrorCallback = () => {};
+export const StorageValueCallback = () => {};
+export default class EncryptedStorage {
+  setItem() {}
+  getItem() {}
+  removeItem() {}
+  clear() {}
+}
--- .storybook/aliases/react-native-fast-image.js
+++ .storybook/aliases/react-native-fast-image.js
@@ -1,3 +1,6 @@
+import React from "react";
 import { Image } from "react-native";
 
-export const FastImage = () => <Image />;
+const FastImage = () => <Image />;
+
+export default FastImage;
