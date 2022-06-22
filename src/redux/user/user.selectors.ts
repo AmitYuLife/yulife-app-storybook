aab328d183f23bcb8165ac1f1f52d944d3041dfe
Vitaly Tr <vitaly@yulife.com>
2022-06-22 07:41:33 +0100

    Purge: unused user selectors

--- src/redux/user/user.selectors.ts
+++ src/redux/user/user.selectors.ts
@@ -22,38 +22,11 @@ export const getIsUserArchived = createSelector(reducer, isUserArchivedSelector)
 const userConnectionsSelector = (state: State) => state.connections;
 export const getUserConnections = createSelector(reducer, userConnectionsSelector);
 
-const userConsentSelector = (state: State) => state.consent;
-export const getUserConsent = createSelector(reducer, userConsentSelector);
-
 const userFeaturesSelector = (state: State) => state.features;
 export const getUserFeatures = createSelector(reducer, userFeaturesSelector);
 
-const allLeaderboardsSelector = (state: State) => state.leaderboards;
-export const getAllLeaderboards = createSelector(reducer, allLeaderboardsSelector);
-
-const visiblePopupsSelector = (state: State) => state.popupVisibility;
-export const getVisiblePopups = createSelector(reducer, visiblePopupsSelector);
-
-const surgeIntroSelector = (state: State) => state.surgeIntro;
-export const getSurgeIntro = createSelector(reducer, surgeIntroSelector);
-
-const userBusinessSelector = (state: State) => state.business;
-export const getUserBusiness = createSelector(reducer, userBusinessSelector);
-
-const userFirstNameSelector = (state: State) => state.firstName;
-export const getUserFirstName = createSelector(reducer, userFirstNameSelector);
-
-const userLastNameSelector = (state: State) => state.lastName;
-export const getUserLastName = createSelector(reducer, userLastNameSelector);
-
 const userNameSelector = (state: State) => state.firstName + " " + state.lastName;
 export const getUserName = createSelector(reducer, userNameSelector);
-
-const userDateOfBirthSelector = (state: State) => state.dateOfBirth;
-export const getUserDateOfBirth = createSelector(reducer, userDateOfBirthSelector);
-
-const userMembershipTypeSelector = (state: State) => state.membershipType;
-export const getUserMembershipType = createSelector(reducer, userMembershipTypeSelector);
 
 const acceptedLeaderboardsSelector = (state: State) => state.leaderboards.filter((l) => l.hasAccepted);
 export const getAcceptedLeaderboards = createSelector(reducer, acceptedLeaderboardsSelector);
@@ -67,9 +40,6 @@ export const getActiveLeaderboard = createSelector(reducer, activeLeaderboardSel
 
 const currentUserIdSelector = (state: State) => state.id;
 export const getCurrentUserId = createSelector(reducer, currentUserIdSelector);
-
-const businessLeaderboardConsentSelector = (state: State) => !!state.leaderboards.find(checkIsCompanyLeaderbaord);
-export const hasBusinessLeaderboardConsent = createSelector(reducer, businessLeaderboardConsentSelector);
 
 const userSurge = (state: State) => state.surge;
 export const getUserSurge = createSelector(reducer, userSurge);
@@ -108,22 +78,3 @@ export const getUserEventsWithAds = createSelector(getUserActiveEvents, getAdBan
 
 const yuScreenNotification = (state: State) => state.notification?.hasYuScreenNotification;
 export const getYuScreenNotification = createSelector(reducer, yuScreenNotification);
-
-const consentedLeaderboardsSelector = (state: State) =>
-  state.leaderboards.reduce((prev: Leaderboard[], curr) => {
-    if (checkIsCompanyLeaderbaord(curr)) {
-      return [curr, ...prev];
-    }
-
-    if (curr.consent) {
-      return [...prev, curr];
-    }
-
-    return prev;
-  }, []);
-export const getConsentedLeaderboards = createSelector(reducer, consentedLeaderboardsSelector);
-
-// helpers
-const checkIsCompanyLeaderbaord = (board: GetCurrentUser_getCurrentUser_leaderboards) =>
-  // company leaderboard has 32 chars (and it should be first), custom leaderboards have 38
-  !!(board.leaderboardId && board.leaderboardId.length === 32 && board.consent);
