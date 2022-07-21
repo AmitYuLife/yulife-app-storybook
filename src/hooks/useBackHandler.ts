ad8e113b70cf0dea84ef27b07c8d629813f6bf1f
Jeff Filho <jeff@yulife.com>
2022-07-21 17:46:44 +0000

    Bugfix: Failed to execute stack command stack not found

--- src/hooks/useBackHandler.ts
+++ src/hooks/useBackHandler.ts
@@ -1,12 +1,20 @@
 import React from "react";
 import { BackHandler } from "react-native";
+import { usePressedInWithDelay } from "./usePressedInWithDelay";
 
 export function useBackHandler(backHandler: () => boolean) {
+  const { handlePress } = usePressedInWithDelay({ onPress: backHandler });
+
+  const handleBackPress = () => {
+    handlePress();
+    return true;
+  };
+
   React.useEffect(() => {
-    BackHandler.addEventListener("hardwareBackPress", backHandler);
+    BackHandler.addEventListener("hardwareBackPress", handleBackPress);
 
     return () => {
-      BackHandler.removeEventListener("hardwareBackPress", backHandler);
+      BackHandler.removeEventListener("hardwareBackPress", handleBackPress);
     };
   }, [backHandler]);
 }
