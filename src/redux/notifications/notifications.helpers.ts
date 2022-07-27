5c37c4143252f00a14b382fcb1e08c6d7be5dfd4
Vitaly Tr <vitaly@yulife.com>
2022-07-27 20:17:22 +0100

    Bugfix: cancel custom media notifications

--- src/redux/notifications/notifications.helpers.ts
+++ src/redux/notifications/notifications.helpers.ts
@@ -1,16 +1,21 @@
+import { YULIFE_PN_CHANNEL_ID } from "@services/constants";
+import { PushNotificationScheduleObject } from "react-native-push-notification";
+
 export const numericId = (id: string) => id.replace(/\D/g, "").substring(0, 9);
 
-export const defaultNotificationSettings = {
+export const defaultNotificationSettings: Partial<PushNotificationScheduleObject> = {
+  channelId: YULIFE_PN_CHANNEL_ID,
   autoCancel: true, // (optional) default: true
   largeIcon: "ic_launcher", // (optional) default: "ic_launcher"
   ongoing: false, // (optional) set whether this is an "ongoing" notification
-  playSound: false, // (optional) default: true
+  // playSound: false, // (optional) default: true
   smallIcon: "ic_notification", // (optional) default: "ic_notification"
   soundName: "default", // (optional) Sound to play when the notification is shown
   vibrate: true, // (optional) default: true
   vibration: 300, // vibration length in milliseconds, ignored if vibrate=false, default: 1000
 };
 
-export const getNotificationTitleAndMessage = () => {
-  return { title: "Challenge Completed", message: "Time's up! Check how you did on your latest challenge." };
-};
+export const getNotificationTitleAndMessage = () => ({
+  title: "Challenge completed",
+  message: "Time's up! Check how you did on your latest challenge.",
+});
--- src/redux/notifications/sagas/scheduleChallengeNotification.saga.ts
+++ src/redux/notifications/sagas/scheduleChallengeNotification.saga.ts
@@ -2,7 +2,6 @@ import { ApolloQueryResult } from "apollo-client";
 import moment from "moment";
 import PushNotification from "react-native-push-notification";
 import { call } from "redux-saga/effects";
-import { YULIFE_PN_CHANNEL_ID } from "@services/constants";
 import Logger from "@services/logging/logger";
 import { GetUserNotificationsSettings } from "@graphql/_core/schema";
 import { UserNotificationsType } from "@graphql/_core/schema/globalTypes";
@@ -11,10 +10,17 @@ import { challengeStartSuccessAction } from "../../levels/levels.actions";
 import { defaultNotificationSettings, getNotificationTitleAndMessage, numericId } from "../notifications.helpers";
 import { addSecondsToChallengeEndDateTime } from "@utils";
 
-export default function* scheduleChallengeNotificationSaga({
-  payload: { createActiveChallenge },
-}: ReturnType<typeof challengeStartSuccessAction>) {
-  if (!createActiveChallenge.challenge) {
+type Action = ReturnType<typeof challengeStartSuccessAction>;
+
+export default function* scheduleChallengeNotificationSaga({ payload }: Action) {
+  if (!payload?.createActiveChallenge?.challenge) {
+    return;
+  }
+
+  const { createActiveChallenge, videoDuration } = payload;
+
+  if (videoDuration) {
+    // for custom media we dont need a notification
     return;
   }
 
@@ -36,10 +42,7 @@ export default function* scheduleChallengeNotificationSaga({
           PushNotification.localNotificationSchedule({
             ...defaultNotificationSettings,
             date: moment(addSecondsToChallengeEndDateTime(endDateTime)).toDate(),
-            channelId: YULIFE_PN_CHANNEL_ID,
-            group: "Yu Life Challenges", // (optional) add group to message
             id,
-            tag: "challenge_complete", // (optional) add tag to message
             ...details,
           })
         );
