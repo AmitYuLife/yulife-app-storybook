f3d38f296702e81c8ebeefdeb89293b966e882e6
Cris Naranjo <cris@yulife.com>
2022-10-03 14:42:13 +0000

    Chore: Skip encrypted storage errors for ios while on background

--- src/redux/app/sagas/listenToAppState.saga.ts
+++ src/redux/app/sagas/listenToAppState.saga.ts
@@ -3,13 +3,14 @@ import { updateAppStateActive, updateAppState } from "../app.actions";
 import { appStateChannel } from "../app.channels";
 import { getToken } from "@services/storage";
 import { Unpacked } from "@utils";
+import { AppStateStatus } from "react-native";
 
 export default function* listenToAppStateSaga() {
   yield put(updateAppState("active"));
   const stateChannel: ReturnType<typeof appStateChannel> = yield call(appStateChannel);
 
   while (true) {
-    const state: string = yield take(stateChannel);
+    const state: AppStateStatus = yield take(stateChannel);
     yield put(updateAppState(state));
 
     if (state === "active") {
