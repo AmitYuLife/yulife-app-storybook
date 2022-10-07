a1f94582d7ea995a03e4283827a558715a848289
Jeff Filho <jeff@yulife.com>
2022-10-07 12:31:51 +0100

    Bugfix: Video player button on big screens

--- src/components/molecules/button/video-player-button/video-player-button.tsx
+++ src/components/molecules/button/video-player-button/video-player-button.tsx
@@ -13,16 +13,14 @@ interface IProps {
 export const VidePlayerButton = memo(({ onPress, isPaused }: IProps) => {
   return (
     <PressableWithDelay onPress={onPress} style={styles.wrapper}>
-      <Svg width={Style.adjust(40)} height={Style.adjust(40)} fill="none" viewBox="0 0 40 40">
-        {!isPaused ? (
-          <>
-            <Rect x={8.334} y={6.667} width={7.727} height={28.333} rx={3.864} fill={Colours.neutral.white} />
-            <Rect x={23.789} y={6.667} width={7.727} height={28.333} rx={3.864} fill={Colours.neutral.white} />
-          </>
-        ) : (
-          <PlayIcon width={40} height={40} />
-        )}
-      </Svg>
+      {!isPaused ? (
+        <Svg width={Style.adjust(40)} height={Style.adjust(40)} fill="none" viewBox="0 0 40 40">
+          <Rect x={8.334} y={6.667} width={7.727} height={28.333} rx={3.864} fill={Colours.neutral.white} />
+          <Rect x={23.789} y={6.667} width={7.727} height={28.333} rx={3.864} fill={Colours.neutral.white} />
+        </Svg>
+      ) : (
+        <PlayIcon width={40} height={40} />
+      )}
     </PressableWithDelay>
   );
 });
