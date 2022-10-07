c0f74f5ea8d324f952ffc34c26667115b7a53974
Vitaly Trus <vitaly@yulife.com>
2022-10-07 17:05:52 +0000

    Hotfix ENG-2945: meditation crash

--- src/redux/quests/quests.actions.ts
+++ src/redux/quests/quests.actions.ts
@@ -1,10 +1,8 @@
-import { SyncAction } from "../_core/types";
 import {
   CreateQuestMapLevelChallenge_createQuestMapLevelChallenge_yuniversalChest,
   UpdateQuestMapLevelChallenge_updateQuestMapLevelChallenge_challenge as QuestMapActiveChallenge,
 } from "@graphql/_core/schema";
 
-export const CLEAR_CHALLENGE_STATUS_PROMPT = "CLEAR_CHALLENGE_STATUS_PROMPT";
 export const SET_CHALLENGE_STATUS_PROMPT = "SET_CHALLENGE_STATUS_PROMPT";
 
 export const setChallengeStatusPrompt = (payload: {
@@ -20,8 +18,4 @@ export const setChallengeStatusPrompt = (payload: {
 }) => ({
   payload,
   type: SET_CHALLENGE_STATUS_PROMPT,
-});
-
-export const clearChallengeStatusPrompt = (): SyncAction => ({
-  type: CLEAR_CHALLENGE_STATUS_PROMPT,
 });
