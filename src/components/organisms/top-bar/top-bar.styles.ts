bc36e0392bbef81798da1e7cd05f55850861b066
Vitaly Trus <vitaly@yulife.com>
2023-01-30 10:13:47 +0000

    Revert "Merge branch 'bugfix/dynamic-island-top-bar' into 'develop'"
    This reverts merge request !2820

--- src/components/organisms/top-bar/top-bar.styles.ts
+++ src/components/organisms/top-bar/top-bar.styles.ts
@@ -2,8 +2,7 @@ import { StyleSheet, TextStyle, ViewStyle } from "react-native";
 import { Style } from "../../../styles";
 import { TOP_BAR } from "@styles";
 
-export const TOP_BAR_HEIGHT =
-  TOP_BAR.HEIGHT + TOP_BAR.PADDING_TOP + TOP_BAR.PADDING_BOTTOM - (Style.hasDynamicIsland() ? 10 : 0);
+export const TOP_BAR_HEIGHT = TOP_BAR.HEIGHT + TOP_BAR.PADDING_TOP + TOP_BAR.PADDING_BOTTOM;
 
 export default StyleSheet.create({
   wrapper: {
