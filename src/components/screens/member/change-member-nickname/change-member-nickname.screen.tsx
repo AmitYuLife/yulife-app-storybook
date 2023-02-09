db14a66621f59b7e01232e4f43ff66c35a997f6b
Carlos Passos <carlos.passos@yulife.com>
2023-02-09 07:51:00 +0000

    Chore: purge label copies

--- src/components/screens/member/change-member-nickname/change-member-nickname.screen.tsx
+++ src/components/screens/member/change-member-nickname/change-member-nickname.screen.tsx
@@ -8,6 +8,7 @@ import { ROUTES } from "@navigation/constants";
 import { useKeyboardListeners } from "@hooks";
 import { NICKNAME_INPUT, CHANGE_MEMBER_NICK_BUTTON } from "@ids";
 import { GenericHeadingAbsolute, GenericHeadingPad } from "@organisms";
+import { t } from "@locale";
 
 type Props = {
   enableButton: boolean;
@@ -55,7 +56,7 @@ const ChangeMemberNickname: FunctionComponent<Props> = ({ enableButton, isLoadin
             <Button
               disabled={!nickname}
               testID={CHANGE_MEMBER_NICK_BUTTON}
-              label="Save"
+              label={t("labels.cta.save")}
               isLoading={isLoading}
               onPress={() => onPress(nickname)}
             />
--- src/components/screens/member/menu/menu.screen.tsx
+++ src/components/screens/member/menu/menu.screen.tsx
@@ -109,7 +109,7 @@ const ReferralButton = ({ showBadge, loading, uri, onInvitePress }: ReferralButt
     </View>
     <View style={styles.referralButtonWrapper}>
       <Button
-        label="Invite a colleague"
+        label={t("labels.cta.invite")}
         size="Fill"
         onPress={onInvitePress}
         showBadge={showBadge}
@@ -124,25 +124,25 @@ const Links = ({ links }: { links: IProps["links"] }) => (
     {!links
       ? null
       : links.map(({ source, onPress, label, condition }, index) =>
-          !condition ? null : (
-            <View key={`menu-${index}`}>
-              <TouchableOpacityWithDelay
-                style={styles.itemWrapper}
-                onPress={onPress}
-                testID={MENU_ITEM(label)}
-                hitSlop={HIT_SLOP}
-                accessibilityLabel={label}
-              >
-                {!source ? null : (
-                  <View style={styles.iconWrapper}>
-                    <RNImage source={source} />
-                  </View>
-                )}
-                <TextTemplate type="l1">{label}</TextTemplate>
-              </TouchableOpacityWithDelay>
-            </View>
-          )
-        )}
+        !condition ? null : (
+          <View key={`menu-${index}`}>
+            <TouchableOpacityWithDelay
+              style={styles.itemWrapper}
+              onPress={onPress}
+              testID={MENU_ITEM(label)}
+              hitSlop={HIT_SLOP}
+              accessibilityLabel={label}
+            >
+              {!source ? null : (
+                <View style={styles.iconWrapper}>
+                  <RNImage source={source} />
+                </View>
+              )}
+              <TextTemplate type="l1">{label}</TextTemplate>
+            </TouchableOpacityWithDelay>
+          </View>
+        )
+      )}
   </>
 );
 
@@ -155,9 +155,8 @@ const DebugAndVersion = ({ onDebugPress, version }: Pick<IProps, "onDebugPress" 
       hitSlop={HIT_SLOP}
       importantForAccessibility="no"
     >
-      <TextTemplate color={Colours.neutral.n400} type="l3">{`Version ${version}${
-        onDebugPress ? " | " : ""
-      }`}</TextTemplate>
+      <TextTemplate color={Colours.neutral.n400} type="l3">{`Version ${version}${onDebugPress ? " | " : ""
+        }`}</TextTemplate>
       {!onDebugPress ? null : (
         <TextTemplate color={Colours.neutral.n400} type="l3" decoration="underline">
           Debug menu
