7b617c9b5bb27b865602c7254db835521bc38d5a
Carlos Passos <carlos.passos@yulife.com>
2023-02-14 13:42:59 +0000

    Chore: purge challenge progress external apps copies

--- src/components/screens/member/challenges/challenge-progress/subcomponents/external-app-links.tsx
+++ src/components/screens/member/challenges/challenge-progress/subcomponents/external-app-links.tsx
@@ -5,6 +5,7 @@ import { Style } from "@styles";
 import { Platform, StyleSheet, View, ViewStyle } from "react-native";
 import { useSelector } from "react-redux";
 import { getUserFeatures } from "@redux/user/user.selectors";
+import { t } from "@locale";
 
 export interface ExternalLinksProps {
   hideOverlay: () => void;
@@ -15,20 +16,18 @@ export const MeditationExternalLinks = memo(({ hideOverlay }: ExternalLinksProps
   return (
     <>
       <TextTemplate type="h2" textAlign="center">
-        Choose an app to start
+        {t("screens.challenge_progress.external_app_links.heading")}
       </TextTemplate>
       <View style={styles.spaceSmall} />
       <TextTemplate type="b2" textAlign="center">
-        {`You can use any app that syncs mindfulness minutes to ${
-          Platform.OS === "ios" ? "Apple Health" : "Google Fit"
-        }`}
+        {t("screens.challenge_progress.external_app_links.subheading", { syncApp: Platform.select({ ios: t("apple_health"), android: t("google_fit") }) })}
       </TextTemplate>
       {/* //@TODO: GS-340 - This is temporary until we release the new meditation, delete this after */}
       {features.newMediaPlayer ? null : <MeditopiaButton style={styles.spaceMedium} onPressCallback={hideOverlay} />}
       <CalmButton style={styles.spaceSmall} onPressCallback={hideOverlay} />
       <HeadspaceButton style={styles.spaceSmall} onPressCallback={hideOverlay} />
 
-      <LinkButton wrapperStyle={styles.spaceXsmall} label="I'm using a different app" onPress={hideOverlay} />
+      <LinkButton wrapperStyle={styles.spaceXsmall} label={t("screens.challenge_progress.external_app_links.different_app_cta_label")} onPress={hideOverlay} />
     </>
   );
 });
@@ -36,11 +35,11 @@ export const MeditationExternalLinks = memo(({ hideOverlay }: ExternalLinksProps
 export const FiitExternalLinks = memo(({ hideOverlay }: ExternalLinksProps) => (
   <>
     <TextTemplate type="h2" textAlign="center">
-      Get started with Fiit
+      {t("screens.challenge_progress.external_app_links.fiit.heading")}
     </TextTemplate>
     <View style={styles.spaceSmall} />
     <TextTemplate type="b2" textAlign="center">
-      Complete any Fiit class to get rewarded
+      {t("screens.challenge_progress.external_app_links.fiit.subheading")}
     </TextTemplate>
     <FiitButton style={styles.spaceMedium} onPressCallback={hideOverlay} />
   </>
