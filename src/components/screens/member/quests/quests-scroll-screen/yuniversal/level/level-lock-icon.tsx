030191c2ca22256f0eafd9ff5d1a510f8e3273a0
Jeff Filho <jeff@yulife.com>
2023-02-22 08:06:58 +0000

    Bugfix: Fix EOTW icons

--- src/components/screens/member/quests/quests-scroll-screen/yuniversal/level/level-lock-icon.tsx
+++ src/components/screens/member/quests/quests-scroll-screen/yuniversal/level/level-lock-icon.tsx
@@ -7,13 +7,21 @@ export const LevelLockIcon = memo(() => (
       d="M29.5 23.843V21.32c0-2.303-1.889-4.195-4.219-4.195a4.223 4.223 0 0 0-4.218 4.227v2.523"
       stroke="#956AFF"
       strokeMiterlimit={10}
+      fill="none"
     />
-    <Path stroke="#956AFF" strokeLinecap="round" strokeLinejoin="round" d="M18.188 23.813h13.625v9.125H18.188z" />
+    <Path
+      stroke="#956AFF"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M18.188 23.813h13.625v9.125H18.188z"
+      fill="none"
+    />
     <Path
       d="M25 29.5a1.125 1.125 0 1 0 0-2.25 1.125 1.125 0 0 0 0 2.25Z"
       stroke="#956AFF"
       strokeMiterlimit={10}
       strokeLinecap="round"
+      fill="none"
     />
   </>
 ));
