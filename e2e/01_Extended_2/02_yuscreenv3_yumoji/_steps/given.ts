17513ab014a06f65e88f9b9512df78b6abba5fa2
Christian Baker <christian@yulife.com>
2023-03-13 15:51:07 +0000

    Chore - Added Smoke 3 & Extended 3

--- e2e/01_Extended_2/02_yuscreenv3_yumoji/_steps/given.ts
+++
@@ -1,9 +0,0 @@
-import { navigation, completeOnboardingIntro } from "@navigation"
-import { CUSTOMER_1, AUTH_1 } from "@data"
-export { addStepsHistoricalData } from "@socket";
-
-
-export const {
-    logInAndGoToTab,
-    loginToYuScreen
-} = navigation.login
