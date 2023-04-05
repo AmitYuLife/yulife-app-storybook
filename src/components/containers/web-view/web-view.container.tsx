5b5cf8179d60464202226da3d63fdaddf2bf40ed
Ben Tripisciano <ben.tripisciano@yulife.com>
2023-04-05 10:57:09 +0000

    Task XSE-1848: Allow web view on close action

--- src/components/containers/web-view/web-view.container.tsx
+++ src/components/containers/web-view/web-view.container.tsx
@@ -1,27 +1,34 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { WebViewScreen } from "@components/screens/web-view/web-view.screen";
 import { Navigation } from "@navigation/main";
 import { ROUTES } from "@navigation/constants";
 import { WebViewErrorBoundary } from "@components/screens/web-view/web-view.error-boundary";
 import { useBackHandler } from "@hooks";
+import { useDispatch } from "react-redux";
+import { AnyAction, Dispatch } from "redux";
 
 export interface WebViewContainerProps {
   uri: string;
   title: string;
   onBothLinksFail?: () => void;
+  onClose?: (dispatch: Dispatch<AnyAction>) => void;
 }
 
-function handleCloseWebView() {
+function closeWebView() {
   Navigation.dismissModal(ROUTES.webView);
 }
 
 function WebViewContainer(props: WebViewContainerProps) {
-  const { uri, title, onBothLinksFail = () => null } = props;
+  const { uri, title, onBothLinksFail = () => null, onClose = (_) => null } = props;
+  const dispatch = useDispatch();
 
-  useBackHandler(() => {
-    handleCloseWebView();
+  const handleCloseWebView = useCallback(() => {
+    closeWebView();
+    onClose(dispatch);
     return true;
-  });
+  }, [dispatch, onClose]);
+
+  useBackHandler(handleCloseWebView);
 
   return (
     <WebViewErrorBoundary
