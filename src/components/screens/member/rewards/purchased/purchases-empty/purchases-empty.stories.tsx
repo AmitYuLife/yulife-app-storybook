2f8173a4ea18b8b237c4d0443641fee7f7f562f1
Jeff Filho <jeff@yulife.com>
2023-07-11 16:49:17 +0100

    Purge: Old storybooks stories

--- src/components/screens/member/rewards/purchased/purchases-empty/purchases-empty.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react-native";
-import PurchasesEmpty from "./purchases-empty";
-
-function voidFunc(): void {
-  return null;
-}
-
-storiesOf("PurchasesEmpty", module).add("default", () => <PurchasesEmpty onCtaPress={voidFunc} />);
