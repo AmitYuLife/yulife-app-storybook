33d2a2e0dbb89b5cb17b2fdfa593166b4009fbe7
Jeff Filho <jeff@yulife.com>
2023-07-11 17:45:58 +0100

    Chore: Purge left over from old storybook and enable molecules stories for the new one

--- src/components/storybook/withProvider.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { Provider } from "react-redux";
-import { mockStore } from "@redux/_core/store";
-
-export const withProvider = (story: () => React.ReactNode) => <Provider store={mockStore}>{story()}</Provider>;
