4da24e8dcab8772112d649673e0f8fe637eb354b
Yu Lo Chung <yu.lo@yulife.com>
2023-10-30 10:04:33 +0000

    Chore/version check in notification center

--- src/hooks/useNotifications.ts
+++ src/hooks/useNotifications.ts
@@ -3,7 +3,7 @@ import Logger from "@services/logging/logger";
 import moment from "moment";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { useDispatch } from "react-redux";
-import { parseJSON } from "@utils";
+import { parseJSON, appVersionSatisfies } from "@utils";
 
 export const useNotifications = () => {
   const [notifications, setNotifications] = useState<Message[]>([]);
@@ -16,16 +16,19 @@ export const useNotifications = () => {
     }
 
     const inbox = await Logger.leanplum.getInbox();
+    const messages = inbox.allMessages
+      .filter((message) => moment(message.deliveryTimestamp).isAfter(moment().subtract(7, "day").startOf("day")))
+      .filter((message) =>
+        // We're doing version check in client due to the fact that we can't validate client's version in the server since these messages are redirected by third party service and no way to know which version of the app at the receiving end when the initial sending happens
+        message.data?.requiredAppVersion ? appVersionSatisfies(message.data?.requiredAppVersion as string) : true
+      )
+      .sort((a, b) => moment.utc(b.deliveryTimestamp).unix() - moment.utc(a.deliveryTimestamp).unix())
+      .map((message) => ({
+        ...message,
+        imageUrl: (message.data?.inboxMessageImageUrl as string) || message.imageUrl,
+      }));
     setIsInitialized(true);
-    setNotifications(
-      inbox.allMessages
-        .filter((message) => moment(message.deliveryTimestamp).isAfter(moment().subtract(7, "day")))
-        .sort((a, b) => moment.utc(b.deliveryTimestamp).unix() - moment.utc(a.deliveryTimestamp).unix())
-        .map((message) => ({
-          ...message,
-          imageUrl: (message.data?.inboxMessageImageUrl as string) || message.imageUrl,
-        }))
-    );
+    setNotifications(messages);
   }, []);
 
   const onRefresh = useCallback(() => {
--- src/utils/device.ts
+++ src/utils/device.ts
@@ -1,5 +1,8 @@
 import { Platform } from "react-native";
 import DeviceInfo from "react-native-device-info";
+import { DETOX_ENABLED } from "@services/socket";
+import * as semver from "semver";
+const appJson = require("../../package.json");
 
 export const isSamsung = () => {
   if (Platform.OS === "android") {
@@ -7,4 +10,30 @@ export const isSamsung = () => {
   }
 
   return false;
+};
+
+export const getAppVersion = () => {
+  const version = DeviceInfo.getVersion();
+
+  // means is local
+  if (DETOX_ENABLED || version === "1.0") {
+    return appJson.version;
+  }
+
+  return version;
+};
+
+export const appVersionSatisfies = (range: string) => {
+  const appVersion = getAppVersion();
+
+  if (!semver.validRange(range)) {
+    return false;
+  }
+
+  // app development local has always version = 1.0
+  if (!semver.valid(appVersion)) {
+    return false;
+  }
+
+  return semver.satisfies(appVersion, range);
 };
