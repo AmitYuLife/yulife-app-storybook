1da2b320e4b90a5a453be183540d808eeb15b09c
Manolis Vrondakis <manolis.vrondakis@yulife.com>
2023-11-23 10:22:04 +0000

    Task: Red planet webp

--- src/components/containers/member/quests/quest-map/quest-map.config.ts
+++ src/components/containers/member/quests/quest-map/quest-map.config.ts
@@ -1,7 +1,8 @@
 import { IQuestMapConfig } from "./quest-map.interface";
 import { BRIGHT_PLANET_EPISODES } from "./config/bright-planet.config";
 import { ORANGE_PLANET_EPISODES } from "./config/orange-planet.config";
+import { RED_PLANET_EPISODES } from "./config/red-planet.config";
 
 export const QUEST_MAP_CONFIG: IQuestMapConfig = {
-  episodes: { ...BRIGHT_PLANET_EPISODES, ...ORANGE_PLANET_EPISODES },
+  episodes: { ...RED_PLANET_EPISODES, ...BRIGHT_PLANET_EPISODES, ...ORANGE_PLANET_EPISODES },
 };
