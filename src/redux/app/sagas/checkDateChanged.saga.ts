b4a3b63707b7847d92778a2ea863e0a7a83ee11f
Leomar Alejar <leomar@yulife.com>
2023-11-23 15:18:46 +0000

    Bugfix: Exclude update date action on check date saga

--- src/redux/app/sagas/checkDateChanged.saga.ts
+++ src/redux/app/sagas/checkDateChanged.saga.ts
@@ -2,10 +2,15 @@ import { put, select } from "redux-saga/effects";
 import { DATE_FORMAT } from "@utils";
 import moment from "moment";
 
-import { updateCurrentDate } from "../app.actions";
+import { UPDATE_CURRENT_DATE, updateCurrentDate } from "../app.actions";
 import { getCurrentDateState } from "../app.selectors";
+import { Action } from "@reduxjs/toolkit";
 
-export default function* checkDateChanged() {
+export default function* checkDateChanged({ type }: Action) {
+  if (type === UPDATE_CURRENT_DATE) {
+    return;
+  }
+
   const currentReduxDate: string = yield select(getCurrentDateState);
   const date = moment().format(DATE_FORMAT);
 
