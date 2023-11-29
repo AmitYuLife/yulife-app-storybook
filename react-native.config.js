a4a952c58a0c99f3318f3f94c97ef35ef8747beb
Jeff Filho <jeff@yulife.com>
2023-11-29 11:09:53 +0000

    Chore: Remove react-native-video config from react-native-config

--- react-native.config.js
+++ react-native.config.js
@@ -5,13 +5,6 @@ module.exports = {
         android: null,
       },
     },
-    "react-native-video": {
-      platforms: {
-        android: {
-          sourceDir: "../node_modules/react-native-video/android-exoplayer",
-        },
-      },
-    },
     ...(process.env.NO_FLIPPER
       ? { 'react-native-flipper': { platforms: { ios: null } } }
       : {}),
