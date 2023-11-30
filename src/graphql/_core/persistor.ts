81f476140f0651ef3315ab9948fd373b5a984741
Daniel Griffiths <daniel.griffiths@yulife.com>
2023-11-30 15:57:50 +0000

    Task: Update async storage dependency

--- src/graphql/_core/persistor.ts
+++ src/graphql/_core/persistor.ts
@@ -1,5 +1,5 @@
 import { CachePersistor } from "apollo-cache-persist";
-import AsyncStorage from "@react-native-community/async-storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { gqlInMemoryCache } from "./cache";
 
 let persistor: CachePersistor<string>;
