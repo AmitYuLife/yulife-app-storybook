6a7a97bfa7bd4bde5a1fe9d36b6a32d8091ee5eb
Adrian Cilia <adrian@yulife.com>
2023-12-07 14:02:18 +0000

    Bugfix: Get updated coinledger after unity

--- src/redux/levels/sagas/submitUnity.saga.ts
+++ src/redux/levels/sagas/submitUnity.saga.ts
@@ -2,14 +2,14 @@ import submitUnityChallengeWithClient from "@graphql/challenges/submitUnity.gql"
 import getUserSurgeData from "@redux/user/sagas/getUserSurgeData.sagas";
 import Logger from "@services/logging/logger";
 import { call, put, spawn } from "redux-saga/effects";
-import { getUserStart } from "../../user/user.actions";
+import { AppDataType, getUserDataStart } from "../../user/user.actions";
 import { submitUnityAction } from "../levels.actions";
 
 export default function* submitUnitySaga({ payload }: ReturnType<typeof submitUnityAction>) {
   try {
     yield call(submitUnityChallengeWithClient, payload.levelId);
     yield call(getUserSurgeData);
-    yield put(getUserStart());
+    yield put(getUserDataStart([AppDataType.coinLedger]));
   } catch (e) {
     yield spawn(() => {
       Logger.error(e, { event: "submitUnity" });
