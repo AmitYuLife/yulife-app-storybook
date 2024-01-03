fd34179257d8d6e7cdea6dcf01dee49afa143c86
John <john@yulife.com>
2024-01-03 13:57:42 +0000

    Task XSE-2345: Update reward card target from level label to star

--- src/components/atoms/icon/star-icon.tsx
+++ src/components/atoms/icon/star-icon.tsx
@@ -4,10 +4,11 @@ import Svg, { G, Path, Defs, ClipPath } from "react-native-svg";
 
 interface IStarIconProps {
   color?: string;
+  size?: number;
 }
 
-export const StarIcon = memo(({ color = "#FBCC07" }: IStarIconProps) => (
-  <Svg width={Style.adjust(16)} height={Style.adjust(16)} fill="none">
+export const StarIcon = memo(({ color = "#FBCC07", size = Style.adjust(16) }: IStarIconProps) => (
+  <Svg width={size} height={size} viewBox="0 0 16 16" fill="none">
     <G clipPath="url(#prefix__clip0_519:35387)">
       <Path
         d="M8.002 1.002l1.761 5.549 5.738-.013-4.65 3.418 1.786 5.542-4.635-3.439-4.634 3.439 1.786-5.542-4.65-3.418 5.738.013L8.002 1z"
