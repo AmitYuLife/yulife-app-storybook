9bef07e5f1a408a06cdb3e37c520ff1b8bf86918
James Rogers <james.rogers@yulife.com>
2024-02-28 15:32:40 +0000

    detox/usa-fixes-28/02

--- e2e/04_USA/01_YuScreen_And_Products/04_enrollment.spec.ts
+++ e2e/04_USA/01_YuScreen_And_Products/04_enrollment.spec.ts
@@ -14,104 +14,124 @@ import {
   import * as given from "./_steps/given";
   import { AUTH_USA_12, AUTH_USA_13, AUTH_USA_14 } from "../_data/mongo/auths";
   import { BPEEW_USA_12_CAN_PRE, BPEEW_USA_13_CRI, CUSTOMER_USA_12, CUSTOMER_USA_13, CUSTOMER_USA_14 } from "../_data";
-  import * as helper from "./_resources/helpers";
   import * as fixture from "./_resources/fixture";
   import * as id from "@ids";
 
-  Feature("I am able to see the correct enrollment windows on the US YuScreen", async () => {
-    // @flaky - can't find box options on bitrise
-    ScenarioSkip("I can see the correct US pre-enrollment YuScreen",scenario.start, async () => {
-      Given("I login as a user",given.logInAndGoToTab("yu", CUSTOMER_USA_12, AUTH_USA_12, true, "United States"),async () => {
-        //checks the correct products and yucoin power show on the onboarding screen
-        helper.ONBOARDING_YUSCREEN_USA(fixture.CanInsurance);
-        // skips the creation of a YuMoji
-        helper.SKIP_YUMOJI_CREATION(CUSTOMER_USA_12)
-        // confirms the right products are visible on the right side of the YuScreen
-        helper.SLOT_VISIBLE(fixture.CanInsurance)
-        // checks all the components of the YuScreen are correct e.g. name/create YuMoji/ YuCoin power/ the survey at the bottom of the screen
-        helper.YUSCREEN_USA(CUSTOMER_USA_12)
-        // checks the logos are not visible as we are in pre-enrollment
-        helper.SPONSORED_LOGO_NOT_VISIBLE()
-        // checks that the My Wellbeing box is visible as is expected
-        helper.BOX_OPTION_VISIBLE(fixture.MyWellbeingHubBoxUS)
-        // checks explore insurance option is not there as it is pre-enrollment
-        helper.BOX_OPTION_NOT_VISIBLE(fixture.ExploreInsureanceBox)
-        // checks the countdown timer is visible in the correct format and shows the correct information
-        helper.ENROLMENT_VISIBLE(BPEEW_USA_12_CAN_PRE, "pre")
-        // checks the pcp page when clicking to view options from the pre or active enrollment states
-        helper.CHECK_ENROLLMENT_OPTIONS_PRE_ACTIVE(fixture.CancerInsuranceBox)
-        // clicks into the product information and checks page contents
-        helper.CHECK_PCP_PRODUCT(fixture.CancerInsuranceBox, fixture.Guardian_CAN)
-        When(`I tap on the legal information`, when.tapText(fixture.Legal_Information), async () => {
-          Then(`I should see correct legal stuff of cancer insurance`, then.onLegalStuffPage(fixture.Guardian_CAN))
+Feature("I am able to see the correct enrollment windows on the US YuScreen", async () => {
+  // @flaky - can't find box options on bitrise
+  Scenario("I can see the correct US pre-enrollment YuScreen",scenario.start, async () => {
+    Given("I login as a user",given.logInAndGoToTab("yu", CUSTOMER_USA_12, AUTH_USA_12, true, "United States"),async () => {
+      Then("I see the correct onboarding screen info", then.correctUSAOnboardingScreenVisible(fixture.CanInsurance))
+    })
+    When(`When I swipe from ${fixture.protectionPowered}`, when.swipeFromText(fixture.protectionPowered, "up", "slow"), async () => {
+      Then(`I should see ${fixture.buttonText} text`, then.textVisible(fixture.buttonText))
+    })
+    When(`I tap Check out my power`, when.tapText("Check out my power"), async () => {
+      When("I swipe down the screen", when.swipeFromText(fixture.yuMojiBuilder, "up", "slow"), async () => {
+        When("I tap I'll do this later", when.tapText("I'll do this later"), async () => {
+          Then(`I should see the users name`, then.textVisible(`${CUSTOMER_USA_12.data.firstName} ${CUSTOMER_USA_12.data.lastName}`))
+          Then("I can see the correct product slots", then.USAProdSlotVisible(fixture.CanInsurance))
+          Then("I am on the correct US YuScreen", then.onYuScreenUSA(CUSTOMER_USA_12))
+          Then("I can see the sponsored logos", then.sponsoredByVisible(false))
         })
-
-      });
-    });
-
-    // @flaky - can't find box options on bitrise
-    ScenarioSkip("I can see the correct US active enrollment YuScreen",scenario.start, async () => {
-      Given("I login as a user",given.logInAndGoToTab("yu", CUSTOMER_USA_13, AUTH_USA_13, true, "United States"),async () => {
-        //checks the correct products and yucoin power show on the onboarding screen  
-        helper.ONBOARDING_YUSCREEN_USA(fixture.CriInsurance);
-        // skips the creation of a YuMoji
-        helper.SKIP_YUMOJI_CREATION(CUSTOMER_USA_13)
-         // confirms the right products are visible on the right side of the YuScreen
-        helper.SLOT_VISIBLE(fixture.CriInsurance)
-        // checks all the components of the YuScreen are correct e.g. name/create YuMoji/ YuCoin power/ the survey at the bottom of the screen
-        helper.YUSCREEN_USA(CUSTOMER_USA_13)
-        // checks the logos are not visible as we are in active enrollment
-        helper.SPONSORED_LOGO_NOT_VISIBLE()
-        // checks that the My Wellbeing box is visible as is expected
-        When("I scroll to the bottom", when.swipeFromText(fixture.createYumujiCTA, "up", "fast"), async () => {
-          helper.BOX_OPTION_VISIBLE(fixture.MyWellbeingHubBoxUS)
-          // checks explore insurance option is not there as it is active-enrollment
-          helper.BOX_OPTION_NOT_VISIBLE(fixture.ExploreInsureanceBox)
-          // checks the countdown timer is visible in the correct format and shows the correct information
-          helper.ENROLMENT_VISIBLE(BPEEW_USA_13_CRI, "active")
-        })
-        // swiping up page as button doesn't work for automated testing
-        When(`I swipe up the page`, when.scrollUntilTextVisible(id.YUSCREEN_SCROLL_VIEW, `${CUSTOMER_USA_13.data.firstName} ${CUSTOMER_USA_13.data.lastName}`, "up"), async () => {
-          Then(`I should see the top text`, then.textVisible(`${CUSTOMER_USA_13.data.firstName} ${CUSTOMER_USA_13.data.lastName}`))
-        })
-        When("I wait", when.wait(2000), async () => {
-          When(`I tap on slot critical illness`, when.attemptToTapSlot(fixture.Guardian_CRI), async () => {
-              Then(`I should see product card with description critical illness`, then.onMoreProtectionProductsCard(fixture.Guardian_CRI))
-          })
       })
-      When(`I tap on the legal information`, when.tapText(fixture.Legal_Information), async () => {
-        Then(`I should see correct legal stuff of critical illness`, then.onLegalStuffPage(fixture.Guardian_CRI))
+    })
+    // might need to comment out the below due to above flaky reasons
+    When("I scroll to see the wellbeing hub", when.scrollUntilTextVisible(id.YUSCREEN_SCROLL_VIEW, fixture.SurveyText, "down"), async () => {
+    Then("I can see the wellbeing hub box", then.boxOptionVisible(fixture.MyWellbeingHubBoxUS, true))
+    Then("I can't see the explore insurance box", then.boxOptionVisible(fixture.ExploreInsureanceBox, false))
+    })
+    When("I scoll to see the enrollment component", when.scrollUntilTextVisible(id.YUSCREEN_SCROLL_VIEW, fixture.createYumujiCTA, "up"), async () => {
+      Then("I see the correct enrollment component", then.enrollmentComponentVisible(BPEEW_USA_12_CAN_PRE, "pre"))
+    })
+    When(`I tap the button`, when.tapText(fixture.preEnrollmentButtonMessage), async () => {
+      // Then("I'm on the pcp page", then.onPCPPage()) @bug [vbus-179] [GS-863]
+      Then("I cannot see the info panel", then.infoPanelVisible(false))
+      Then("I can see the option for the cancer insurance", then.pcpProductVisible(fixture.CancerInsuranceBox, true))
+      Then("I see the arrow button to go deeper into the product info", then.idVisible(id.ARROW_BUTTON))
+    })
+    When(`I tap the product`, when.tapID(id.BOX_OPTION_TITLE(fixture.CancerInsuranceBox.title)), async () => {
+      Then("I'm on the product page for that product", then.onMoreProtectionProductsCard(fixture.Guardian_CAN))
+    })
+    When(`I tap on the legal information`, when.tapText(fixture.Legal_Information), async () => {
+      Then(`I should see correct legal stuff of cancer insurance`, then.onLegalStuffPage(fixture.Guardian_CAN))
+    })
+
+  });
+
+  // @flaky - can't find box options on bitrise
+  Scenario("I can see the correct US active enrollment YuScreen",scenario.start, async () => {
+    Given("I login as a user",given.logInAndGoToTab("yu", CUSTOMER_USA_13, AUTH_USA_13, true, "United States"),async () => {
+      Then("I see the correct onboarding screen info", then.correctUSAOnboardingScreenVisible(fixture.CriInsurance))
+    })
+    When(`When I swipe from ${fixture.protectionPowered}`, when.swipeFromText(fixture.protectionPowered, "up", "slow"), async () => {
+      Then(`I should see ${fixture.buttonText} text`, then.textVisible(fixture.buttonText))
+    })
+    When(`I tap Check out my power`, when.tapText("Check out my power"), async () => {
+      When("I swipe down the screen", when.swipeFromText(fixture.yuMojiBuilder, "up", "slow"), async () => {
+        When("I tap I'll do this later", when.tapText("I'll do this later"), async () => {
+          Then(`I should see the users name`, then.textVisible(`${CUSTOMER_USA_13.data.firstName} ${CUSTOMER_USA_13.data.lastName}`))
+          Then("I can see the correct product slots", then.USAProdSlotVisible(fixture.CriInsurance))
+          Then("I am on the correct US YuScreen", then.onYuScreenUSA(CUSTOMER_USA_13))
+          Then("I can see the sponsored logos", then.sponsoredByVisible(false))
+        })
       })
-
-    });
-    });
-
-    // @flaky - can't find box options on bitrise
-    ScenarioSkip("I can see the correct US post enrollment YuScreen",scenario.start, async () => {
-      Given("I login as a user",given.logInAndGoToTab("yu", CUSTOMER_USA_14, AUTH_USA_14, true, "United States"),async () => {
-        //checks the correct products and yucoin power show on the onboarding screen (in this case no products should show) 
-        helper.ONBOARDING_YUSCREEN_USA(fixture.VstdInsurance);
-        // skips the creation of a YuMoji
-        helper.SKIP_YUMOJI_CREATION(CUSTOMER_USA_14)
-         // confirms no products are visible on the right side of the YuScreen as none are set to taken up = true
-         When("I wait 1 seconds", when.wait(1000), async () => {
-          Then(`I should NOT see ${fixture.Guardian_VSTD.slotAbreviation} text`, then.textNotVisible(fixture.Guardian_VSTD.slotAbreviation))  
+    })
+    // might need to comment out the below due to above flaky reasons
+    When("I scroll to see the wellbeing hub", when.scrollUntilTextVisible(id.YUSCREEN_SCROLL_VIEW, fixture.SurveyText, "down"), async () => {
+      Then("I can see the wellbeing hub box", then.boxOptionVisible(fixture.MyWellbeingHubBoxUS, true))
+      Then("I can't see the explore insurance box", then.boxOptionVisible(fixture.ExploreInsureanceBox, false))
+    })
+    When("I scoll to see the enrollment component", when.scrollUntilTextVisible(id.YUSCREEN_SCROLL_VIEW, fixture.createYumujiCTA, "up"), async () => {
+      Then("I see the correct enrollment component", then.enrollmentComponentVisible(BPEEW_USA_13_CRI, "active"))
+    })
+    When(`I swipe up the page`, when.scrollUntilTextVisible(id.YUSCREEN_SCROLL_VIEW, `${CUSTOMER_USA_13.data.firstName} ${CUSTOMER_USA_13.data.lastName}`, "up"), async () => {
+      Then(`I should see the top text`, then.textVisible(`${CUSTOMER_USA_13.data.firstName} ${CUSTOMER_USA_13.data.lastName}`))
+    })
+    When("I wait", when.wait(2000), async () => {
+      When(`I tap on slot critical illness`, when.attemptToTapSlot(fixture.Guardian_CRI), async () => {
+          Then(`I should see product card with description critical illness`, then.onMoreProtectionProductsCard(fixture.Guardian_CRI))
+      })
+    })
+    When(`I tap on the legal information`, when.tapText(fixture.Legal_Information), async () => {
+      Then(`I should see correct legal stuff of critical illness`, then.onLegalStuffPage(fixture.Guardian_CRI))
+    })
+  });
+  // @flaky - can't find box options on bitrise
+  Scenario("I can see the correct US post enrollment YuScreen",scenario.start, async () => {
+    Given("I login as a user",given.logInAndGoToTab("yu", CUSTOMER_USA_14, AUTH_USA_14, true, "United States"),async () => {
+      Then("I see the correct onboarding screen info", then.correctUSAOnboardingScreenVisible(fixture.VstdInsurance))
+    })
+    When(`When I swipe from ${fixture.protectionPowered}`, when.swipeFromText(fixture.protectionPowered, "up", "slow"), async () => {
+      Then(`I should see ${fixture.buttonText} text`, then.textVisible(fixture.buttonText))
+    })
+    When(`I tap Check out my power`, when.tapText("Check out my power"), async () => {
+      When("I swipe down the screen", when.swipeFromText(fixture.yuMojiBuilder, "up", "slow"), async () => {
+        When("I tap I'll do this later", when.tapText("I'll do this later"), async () => {
+          Then(`I should see the users name`, then.textVisible(`${CUSTOMER_USA_14.data.firstName} ${CUSTOMER_USA_14.data.lastName}`))
+          Then(`I should NOT see ${fixture.Guardian_VSTD.slotAbreviation} text`, then.textNotVisible(fixture.Guardian_VSTD.slotAbreviation))
+          Then("I am on the correct US YuScreen", then.onYuScreenUSA(CUSTOMER_USA_14))
+          Then("I can see the sponsored logos", then.sponsoredByVisible(true))
         })
-        // checks all the components of the YuScreen are correct e.g. name/create YuMoji/ YuCoin power/ the survey at the bottom of the screen
-        helper.YUSCREEN_USA(CUSTOMER_USA_14)
-        // checks that the sponsored logos appear now we're in post enrollment
-        helper.SPONSORED_LOGO_VISIBLE()
-        // checks that the My Wellbeing box is visible as is expected
-        helper.BOX_OPTION_VISIBLE(fixture.MyWellbeingHubBoxUS)
-        // checks explore insurance option is there as it is post-enrollment
-        helper.BOX_OPTION_VISIBLE(fixture.ExploreInsureanceBox)
-        // checks the countdown timer is not visible as we are in post-enrollment
-        When(`I swipe down the page`, when.scrollUntilTextVisible(id.YUSCREEN_SCROLL_VIEW, fixture.SurveyLabel, "down"), async () => {
-          Then(`I should NOT see the enrollment section`, then.idNotVisible(id.COUNTDOWN_COMPONENT))
-        })
-        helper.CHECK_EXPLORE_INSURANCE(fixture.ShortTermDisibilityInsurance)
-
-      });
-    });
-    
+      })
+    })
+    // might need to comment out the below due to above flaky reasons
+    When("I swipe to the bottom of the screen", when.swipeFromText(`${CUSTOMER_USA_14.data.firstName} ${CUSTOMER_USA_14.data.lastName}`, "up", "fast"), async () => {
+      Then("I can see the wellbeing hub box", then.boxOptionVisible(fixture.MyWellbeingHubBoxUS, true))
+      Then("I can see the explore insurance box", then.boxOptionVisible(fixture.ExploreInsureanceBox, true))
+    })
+    When(`I swipe down the page`, when.scrollUntilTextVisible(id.YUSCREEN_SCROLL_VIEW, fixture.SurveyLabel, "down"), async () => {
+      Then(`I should NOT see the enrollment section`, then.idNotVisible(id.COUNTDOWN_COMPONENT))
+    })
+    When(`I tap explore insurance`, when.tapText(fixture.ExploreInsureanceBox.description), async () => {
+      When("I wait", when.wait(2000), async () => {
+          // Then("I'm on the pcp page", then.onPCPPage()) @bug [vbus-179] [GS-863]
+          Then("I can see the info panel", then.infoPanelVisible(true))
+          Then("I can see the vision insurance box", then.boxOptionVisible(fixture.ShortTermDisibilityInsurance, true))
+          Then("I can NOT see the arrow button to go deeper into the product info", then.idNotVisible(id.ARROW_BUTTON))
+      })
+    })
+      
   })
+})
+    
--- e2e/04_USA/01_YuScreen_And_Products/_steps/then.ts
+++ e2e/04_USA/01_YuScreen_And_Products/_steps/then.ts
@@ -12,6 +12,12 @@ import {
   BOX_OPTION_DESCRIPTION,
   YUSCREEN_SCROLL_VIEW,
   COUNTDOWN_COMPONENT,
+  LEFT_SIDE_BACKGROUD_IMAGE_SLOT,
+  LEFT_SIDE_TEXT_SLOT_POWER,
+  CAROUSEL_CARD,
+  SPONSOR_LOGO_IMAGE,
+  WELLBEING_HUB_SCREEN,
+  TEXT_TEMPLATE,
 } from "@ids";
 import moment = require("moment");
 import * as text from "../_resources/fixture";
@@ -21,8 +27,9 @@ import {
   BPEEW_USA_3_ACC,
   BUSINESS_ACCOUNT_USA_1,
 } from "04_USA/_data";
-import { BoxOption, USProductData } from "../_resources/types";
+import { BoxOption, USProductData, YuScreenInfo } from "../_resources/types";
 import { expect } from 'detox'
+import { BPEW_GDent_10 } from "@data";
 
 export const {
   idVisible,
@@ -417,4 +424,109 @@ export const exploreInsuranceBoxVisible = async () => {
   await idVisible(BOX_OPTION_DESCRIPTION(text.ExploreInsureanceBox.description))()
 }
 
+export const correctUSAOnboardingScreenVisible = (product:YuScreenInfo) => async () => {
+  await textVisibleAtIndex(text.yuCoinText, 0)()
+  await textVisible(text.powerText)()
+  await textVisible(product.mainYuCoinPower)()
+  product.SlotProductTitle && await textVisible(product.SlotProductTitle)()
+  product.SlotLeftBackgroundImgSrc && await idVisibleAtIndex(LEFT_SIDE_BACKGROUD_IMAGE_SLOT(product.SlotLeftBackgroundImgSrc), 0)()
+  product.SlotYuCoinPowerText && await idVisibleAtIndex(LEFT_SIDE_TEXT_SLOT_POWER(product.SlotYuCoinPowerText), 0)()
+  //if product data have info for second slot will run below
+  product.SecondSlotProductTitle && await textVisible(product.SecondSlotProductTitle)()
+  product.SecondSlotLeftBackgroundImgSrc && await idVisibleAtIndex(LEFT_SIDE_BACKGROUD_IMAGE_SLOT(product.SecondSlotLeftBackgroundImgSrc), 1)()
+  product.SlotYuCoinPowerText && await idVisibleAtIndex(LEFT_SIDE_TEXT_SLOT_POWER(product.SlotYuCoinPowerText), 1)()
+  await idVisible(ONBOARDING_SCREEN)()
+  await textVisible(text.protectionPowered)()
+  await textVisible(text.earnRewardsCopy)()
+}
 
+export const USAProdSlotVisible = (prod:YuScreenInfo) => async () => {
+  await textVisible(prod.mainYuCoinPower)()
+  await textVisible(prod.SlotProductTitle)()
+  prod.SlotLeftBackgroundImgSrc && await idVisibleAtIndex(LEFT_SIDE_BACKGROUD_IMAGE_SLOT(prod.SlotLeftBackgroundImgSrc), 0)()
+  prod.SlotYuCoinPowerText && await idVisibleAtIndex(LEFT_SIDE_TEXT_SLOT_POWER(prod.SlotYuCoinPowerText), 0)()
+        
+  prod.SecondSlotProductTitle && await textVisible(prod.SecondSlotProductTitle)()
+  prod.SecondSlotLeftBackgroundImgSrc && await idVisibleAtIndex(LEFT_SIDE_BACKGROUD_IMAGE_SLOT(prod.SecondSlotLeftBackgroundImgSrc), 1)()
+  prod.SlotYuCoinPowerText && await idVisibleAtIndex(LEFT_SIDE_TEXT_SLOT_POWER(prod.SlotYuCoinPowerText), 1)()
+}
+
+export const onYuScreenUSA = (customer: any) => async () => {
+  const firstName = customer.data.firstName;
+  const lastName = customer.data.lastName;
+
+  await textVisible(`${firstName} ${lastName}`)()
+  await idVisible(V4_YUSCREEN)()
+  await textVisible(`${text.createYumujiHeading}`)()
+  await textVisible(`${text.createYumujiText}`)()
+  await textVisible(`${text.createYumujiCTA}`)()
+  await textVisibleAtIndex(`${text.yuCoinText}`, 0)() // @update - can't find YuCoin text on yuscreen it is there
+  await textVisible(`${text.powerText}`)()
+  await scrollUntilTextVisible(YUSCREEN_SCROLL_VIEW, text.SurveyLabel, "down")()
+  await textVisible(`${text.SurveyLabel}`)()
+  await textVisible(`${text.SurveyText}`)()
+  await idNotVisible(CAROUSEL_CARD)()
+  await scrollUntilTextVisible(YUSCREEN_SCROLL_VIEW, `${firstName} ${lastName}`, "up")()
+
+}
+
+export const sponsoredByVisible = (visible: boolean) => async () => {
+  if(visible){
+    await textVisible(text.SponsoredBy)()
+    await idVisible(SPONSOR_LOGO_IMAGE(text.Guardian_Sponsor))()
+    await idVisible(SPONSOR_LOGO_IMAGE(text.Yulife_Sponsor))()
+    await idVisible(SPONSOR_LOGO_IMAGE(text.Transamerica_Sponsor))()
+  } else {
+    await textNotVisible(text.SponsoredBy)()
+    await idNotVisible(SPONSOR_LOGO_IMAGE(text.Guardian_Sponsor))()
+    await idNotVisible(SPONSOR_LOGO_IMAGE(text.Yulife_Sponsor))()
+    await idNotVisible(SPONSOR_LOGO_IMAGE(text.Transamerica_Sponsor))()
+  }
+}
+
+export const boxOptionVisible = (box:BoxOption, visible: boolean) => async () => {
+  if(visible){
+    await idVisible(RIGHT_SIDE_IMAGE_BOX_OPTION(box.imageUrl))()
+    await idVisible(BOX_OPTION_TITLE(box.title))()
+    await idVisible(BOX_OPTION_DESCRIPTION(box.description))()
+  } else {
+    await idNotVisible(RIGHT_SIDE_IMAGE_BOX_OPTION(box.imageUrl))()
+    await idNotVisible(BOX_OPTION_TITLE(box.title))()
+    await idNotVisible(BOX_OPTION_DESCRIPTION(box.description))()
+  }
+}
+
+export const enrollmentComponentVisible = (date:typeof BPEW_GDent_10, state = "pre" || "active") => async () => {
+  const targetDate = date.data.enrolment_end_date
+  const countdownDate = state === "pre" ? moment(targetDate).subtract(1, "day").format("YYYY-MM-DD") : targetDate
+  const buttonMessage = state === "pre" ? text.preEnrollmentButtonMessage : text.activeEnrollmentButtonMessage
+
+  await enrollmentMessageVisible(targetDate, state)()
+  await enrolmentEndsIn(moment(countdownDate).format("MM/DD/YYYY"))()
+  await textVisible(buttonMessage)()
+}
+
+const enrollmentMessageVisible = (date: string, state = "pre" || "active") => async () => {
+  if(state === "active"){
+    try {
+      await textVisible(`${text.enrolmentMessage} ${moment(date).format("MM/DD/YYYY")}`)
+    } catch {
+      await textVisible(`${text.enrollmentMessage} ${moment(date).format("MM/DD/YYYY")}`)
+    }
+  } else {
+    try {
+      await textVisible(`${text.preEnrolmentMessage} ${moment(date).format("MM/DD/YYYY")}`)
+    } catch {
+      await textVisible(`${text.preEnrollmentMessage} ${moment(date).format("MM/DD/YYYY")}`)
+    }
+  }
+}
+
+export const wellbeingHubScreenVisible = (customer: any) => async () => {
+  await idVisible(WELLBEING_HUB_SCREEN)()
+  await textVisible(`Hi ${customer.data.firstName} ${customer.data.lastName}`)()
+  await textVisible(text.wellbeingHubDescription)()
+  await textVisible("All")()
+  await idVisible(TEXT_TEMPLATE("Smart Health"))()
+  await textVisible("Immediate access to a GP by phone or video")()
+}
