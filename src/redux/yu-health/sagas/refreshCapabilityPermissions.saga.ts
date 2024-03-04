7624d7fb9abe1d16204b537a9ca7df19613e0091
Manolis Vrondakis <manolis.vrondakis@yulife.com>
2024-03-04 16:14:57 +0000

    Task: Unsupported capability fixes

--- src/redux/yu-health/sagas/refreshCapabilityPermissions.saga.ts
+++ src/redux/yu-health/sagas/refreshCapabilityPermissions.saga.ts
@@ -3,19 +3,24 @@ import { put, select, take } from "redux-saga/effects";
 import { YU_HEALTH_SET_ACTIVE_PROVIDER, updateCapabilityStatuses } from "../yu-health.actions";
 import { getActiveProviderSelector } from "../yu-health.selectors";
 import { PayloadAction } from "@reduxjs/toolkit";
+import Logger from "@services/logging/logger";
 
 export default function* refreshCapabilityPermissionsSaga() {
-  let activeProvider: HealthProvider = yield select(getActiveProviderSelector);
-  if (!activeProvider) {
-    // Wait until an active provider has been set
-    const setAction: PayloadAction<HealthProvider> = yield take(YU_HEALTH_SET_ACTIVE_PROVIDER);
-    activeProvider = setAction.payload;
+  try {
+    let activeProvider: HealthProvider = yield select(getActiveProviderSelector);
+    if (!activeProvider) {
+      // Wait until an active provider has been set
+      const setAction: PayloadAction<HealthProvider> = yield take(YU_HEALTH_SET_ACTIVE_PROVIDER);
+      activeProvider = setAction.payload;
+    }
+
+    const status: Awaited<ReturnType<typeof hasPermissions>> = yield hasPermissions(
+      Object.values(HealthProviderCapability),
+      activeProvider
+    );
+
+    yield put(updateCapabilityStatuses(status));
+  } catch (e) {
+    Logger.error(e, { file: "refreshCapabilityPermissions.saga" });
   }
-
-  const status: Awaited<ReturnType<typeof hasPermissions>> = yield hasPermissions(
-    Object.values(HealthProviderCapability),
-    activeProvider
-  );
-
-  yield put(updateCapabilityStatuses(status));
 }
