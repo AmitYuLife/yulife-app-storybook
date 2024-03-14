b52d10d1dad7c38b780565746969a6d1c7b22f51
Manolis Vrondakis <manolis.vrondakis@yulife.com>
2024-03-14 12:24:19 +0000

    Task: Add YuWatch library

---
+++ src/redux/yu-watch/sagas/index.ts
@@ -0,0 +1,39 @@
+import { select, takeLeading } from "redux-saga/effects";
+import {
+  CHALLENGE_END_FAIL,
+  CHALLENGE_END_SUCCESS,
+  CHALLENGE_RESET_FAIL,
+  CHALLENGE_RESET_SUCCESS,
+  CHALLENGE_START_SUCCESS,
+} from "@redux/levels/levels.actions";
+import yuWatchRefetchActiveChallengeSaga from "./yuWatchRefetchActiveChallenge.saga";
+import { IFeature } from "@redux/user/user.types";
+import { getUserFeatures } from "@redux/user/user.selectors";
+import { isAndroid } from "@utils";
+
+export default [
+  takeLeading(
+    [
+      CHALLENGE_START_SUCCESS,
+      CHALLENGE_RESET_FAIL,
+      CHALLENGE_RESET_SUCCESS,
+      CHALLENGE_END_FAIL,
+      CHALLENGE_END_SUCCESS,
+      CHALLENGE_END_FAIL,
+    ],
+    yuWatchFeatureGuard(yuWatchRefetchActiveChallengeSaga)
+  ),
+];
+
+// Allows use of 'Function':
+// eslint-disable-next-line @typescript-eslint/ban-types
+function yuWatchFeatureGuard<T extends Function>(saga: T) {
+  return function* yuWatchFeatureGuardSaga(...args: T extends (...args: infer A) => unknown ? A : never) {
+    const features: IFeature = yield select(getUserFeatures);
+    if (!features.tempGameEnableYuWatch || isAndroid()) {
+      return;
+    }
+
+    yield* saga(...args);
+  };
+}
