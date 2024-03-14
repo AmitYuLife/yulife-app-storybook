5b09bff2001f88eabff327bdcb642aa7c5706b34
Jeff Filho <jeff@yulife.com>
2024-03-14 11:48:11 +0000

    Task: Add BottomShadow stories

--- src/components/atoms/bottom-shadow/bottom-shadow.tsx
+++ src/components/atoms/bottom-shadow/bottom-shadow.tsx
@@ -1,22 +1,23 @@
-import { memo, useMemo } from "react";
+import React, { memo, useMemo } from "react";
 import { StyleSheet } from "react-native";
 import { Style } from "@styles";
-import { LinearGradient } from "react-native-linear-gradient";
+import LinearGradient from "react-native-linear-gradient";
 
-export const BottomShadow = () => {
-  const { start, end, colors } = useMemo(() => {
-    return {
+const BottomShadow = () => {
+  const { start, end, colors } = useMemo(
+    () => ({
       start: { x: 0, y: 0 },
       end: { x: 0, y: 1 },
       colors: ["#D9D9D933", "#D9D9D900"],
-    };
-  }, []);
+    }),
+    []
+  );
 
   return <LinearGradient colors={colors} start={start} end={end} style={styles.shadow} />;
 };
 
-export default memo(BottomShadow);
-
 const styles = StyleSheet.create({
   shadow: { height: Style.adjust(7), width: "100%", position: "absolute", bottom: -7 },
 });
+
+export default memo(BottomShadow);
