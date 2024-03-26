98b9de17c13f4faed1cf99721076987cb865e4ec
Manolis Vrondakis <manolis.vrondakis@yulife.com>
2024-03-26 09:29:52 +0000

    Task: Watch milestone star animation

--- ios/YuWatch/views/components/challenge-progress-bar/StarView.swift
+++ ios/YuWatch/views/components/challenge-progress-bar/StarView.swift
@@ -1,22 +1,50 @@
 import SwiftUI
 
-
 struct StarView: View {
-  let progress: Double;
+  let progress: Double
+  var isComplete: Bool { return progress >= 1 }
   
-  var isComplete: Bool { return progress >= 1; }
+  @State private var shouldAnimate: Bool = false
   
   var body: some View {
     VStack {
-      VStack {
-        Image(systemName: "star.fill")
-          .resizable()
-          .scaledToFit()
-          .foregroundColor(isComplete ? Color.yellow : Color("ProgressStarInActive"))
+      Image(systemName: "star.fill")
+        .resizable()
+        .scaledToFit()
+        .foregroundColor(isComplete ? Color.yellow : Color("ProgressStarInActive"))
+    }
+    .padding(6 * ADJUST)
+    .background(Color(isComplete ? "ProgressStarActiveBackground" : "ProgressSegmentDisabled"))
+    .cornerRadius(100)
+    .overlay(
+      Group {
+        if isComplete {
+          Circle()
+            .fill(Color.clear)
+            .overlay(
+              Image(systemName: "star.fill")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(Color.yellow)
+              
+            )
+            .scaleEffect(shouldAnimate ? 5 : 0)
+            .padding(6 * ADJUST)
+            .opacity(shouldAnimate ? 0 : 1)
+            .animation(.easeOut(duration: 0.7), value: shouldAnimate)
+            .onAppear {
+              shouldAnimate = true
+            }
+        }
       }
-      .padding(6 * ADJUST)
-      .background(Color(isComplete ? "ProgressStarActiveBackground" : "ProgressSegmentDisabled"))
-      .cornerRadius(100)
+        .fixedSize()
+    )
+    .onChange(of: isComplete) { newValue in
+      if newValue {
+        withAnimation {
+          shouldAnimate = true
+        }
+      }
     }
   }
 }
