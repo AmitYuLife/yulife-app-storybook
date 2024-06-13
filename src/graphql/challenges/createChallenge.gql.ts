ff12e1d39bdc0567e968a9906db5c6a431200813
Leomar Alejar <leomar@yulife.com>
2024-06-13 10:40:25 +0000

    Bugfix: Use create challenge error from API

--- src/graphql/challenges/createChallenge.gql.ts
+++ src/graphql/challenges/createChallenge.gql.ts
@@ -28,7 +28,6 @@ export const createChallengeToggle = ({
         ...createQuestMapLevelChallengeVariables,
         createdBySource: ActiveChallengeSourceType.Phone,
       },
-      errorPolicy: "ignore",
     });
   }
 
@@ -38,7 +37,6 @@ export const createChallengeToggle = ({
       ...createMobileQuestLevelChallengeVariables,
       createdBySource: ActiveChallengeSourceType.Phone,
     },
-    errorPolicy: "ignore",
   });
 };
 
