32ecd85fa5eca783ee08c1db73b73fd94740df6a
Manolis Vrondakis <manolis.vrondakis@yulife.com>
2024-06-18 15:17:17 +0000

    Task: Convert challenge list to function component

--- src/components/screens/member/challenges/challenges-list/challenges-list.screen.tsx
+++ src/components/screens/member/challenges/challenges-list/challenges-list.screen.tsx
@@ -1,15 +1,16 @@
-import React, { memo, Component } from "react";
+import React, { memo, useState, useMemo, useCallback } from "react";
 import { CHALLENGE_SCREEN } from "@ids";
 import { getTheme } from "@theme";
 import { t } from "@locale";
 import { ChallengeBackground } from "@atoms";
 import { ChallengesList, IChallengesListProps } from "@molecules/index";
-import { View, BackHandler } from "react-native";
+import { View } from "react-native";
 import styles from "./challenges-list.screen.styles";
 import { GenericHeadingPad, TopBarAbsolute } from "@components/organisms";
 import { LeftIcon } from "@organisms/top-bar/subcomponents/left";
+import { useBackHandler } from "@hooks";
 
-interface IProps extends IChallengesListProps {
+interface IChallengeListScreenProps extends IChallengesListProps {
   currentLevel?: number;
   yuniversalMap?: number;
   name: string;
@@ -32,87 +33,59 @@ interface IFormattedChallenge {
   hasBonus?: boolean;
 }
 
-interface IState {
-  hideChallengeTiles: boolean;
-}
+const ChallengesListScreen = ({
+  challenges,
+  currentLevel,
+  yuniversalMap,
+  onPressLeftIcon,
+  name,
+  onLayout,
+}: IChallengeListScreenProps) => {
+  const [hideChallengeTiles, setHideChallengeTiles] = useState<boolean>(true);
+  const challengeListScreen = useMemo(() => {
+    return getTheme(currentLevel, yuniversalMap)?.challengeListScreen;
+  }, [currentLevel, yuniversalMap]);
 
-class ChallengesListScreen extends Component<IProps, IState> {
-  public timeout: NodeJS.Timer = null;
-  public state = {
-    hideChallengeTiles: true,
-  };
-
-  constructor(props: IProps) {
-    super(props);
-    this.handleBackPress = this.handleBackPress.bind(this);
-  }
-
-  private handleBackPress() {
-    this.props.onPressLeftIcon();
-    return true;
-  }
-
-  public componentDidMount() {
-    BackHandler.addEventListener("hardwareBackPress", this.handleBackPress);
-  }
-
-  public shouldComponentUpdate(nextProps: IProps, nextState: IState) {
-    return (
-      this.props.currentLevel !== nextProps.currentLevel ||
-      this.props.yuniversalMap !== nextProps.yuniversalMap ||
-      this.state.hideChallengeTiles !== nextState.hideChallengeTiles ||
-      (!!(this.props.challenges[0] && nextProps.challenges[0]) &&
-        this.props.challenges[0].id !== nextProps.challenges[0].id)
-    );
-  }
-
-  public componentWillUnmount() {
-    if (this.timeout) {
-      global.clearTimeout(this.timeout);
-    }
-
-    BackHandler.removeEventListener("hardwareBackPress", this.handleBackPress);
-  }
-
-  public render() {
-    const { challenges, currentLevel, yuniversalMap, onPressLeftIcon, name, onLayout } = this.props;
-    const { challengeListScreen } = getTheme(currentLevel, yuniversalMap);
-    return (
-      <View style={styles.wrapper} testID={CHALLENGE_SCREEN} onLayout={onLayout}>
-        <GenericHeadingPad />
-        <ChallengeBackground
-          onLayout={this.showChallengeTiles}
-          source={challengeListScreen.backgroundImage}
-          style={challengeListScreen.style}
-          backgroundColor={challengeListScreen.style.backgroundColor}
-        />
-        <View style={styles.challengeSetWrapper}>
-          {this.state.hideChallengeTiles ? null : (
-            <ChallengesList
-              challenges={challenges}
-              tileColour={challengeListScreen.tileBackgroundColour}
-              durationColour={challengeListScreen.durationBackgroundColour}
-              durationTextColour={challengeListScreen.durationTextColour}
-            />
-          )}
-        </View>
-        <TopBarAbsolute
-          type={challengeListScreen.topBarType}
-          leftIcon={LeftIcon.BACK}
-          menuLabel={t("labels.quest_map")}
-          name={name}
-          onPressLeftIcon={onPressLeftIcon}
-        />
-      </View>
-    );
-  }
-
-  private showChallengeTiles = () => {
-    this.timeout = global.setTimeout(() => {
-      this.setState({ hideChallengeTiles: false });
-      this.timeout = null;
+  // No idea why this timeout is needed... it's likely not
+  const showChallengeTiles = useCallback(() => {
+    setTimeout(() => {
+      setHideChallengeTiles(false);
     }, 120);
-  };
-}
+  }, []);
+
+  useBackHandler(() => {
+    onPressLeftIcon();
+    return true;
+  });
+
+  return (
+    <View style={styles.wrapper} testID={CHALLENGE_SCREEN} onLayout={onLayout}>
+      <GenericHeadingPad />
+      <ChallengeBackground
+        source={challengeListScreen.backgroundImage}
+        style={challengeListScreen.style}
+        backgroundColor={challengeListScreen.style.backgroundColor}
+        onLayout={showChallengeTiles}
+      />
+      <View style={styles.challengeSetWrapper}>
+        {hideChallengeTiles ? null : (
+          <ChallengesList
+            challenges={challenges}
+            tileColour={challengeListScreen.tileBackgroundColour}
+            durationColour={challengeListScreen.durationBackgroundColour}
+            durationTextColour={challengeListScreen.durationTextColour}
+          />
+        )}
+      </View>
+      <TopBarAbsolute
+        type={challengeListScreen.topBarType}
+        leftIcon={LeftIcon.BACK}
+        menuLabel={t("labels.quest_map")}
+        name={name}
+        onPressLeftIcon={onPressLeftIcon}
+      />
+    </View>
+  );
+};
 
 export default memo(ChallengesListScreen);
