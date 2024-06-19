a40e4b363cf414c90fa972095f9e2bba6c1074c5
Manolis Vrondakis <manolis.vrondakis@yulife.com>
2024-06-19 12:42:37 +0000

    Task: Fix storybook build

--- .storybook/main.js
+++ .storybook/main.js
@@ -44,7 +44,7 @@ const CUSTOM_CONFIG = {
         },
       },
       {
-        test: /\.(png|jpe?g|woff|woff2|eot|otf|ttf|svg|webp)$/,
+        test: /\.(png|jpe?g|woff|woff2|eot|otf|ttf|svg|webp|lottie)$/,
         loader: "file-loader",
         options: {
           name: "[name].[hash:8].[ext]",
