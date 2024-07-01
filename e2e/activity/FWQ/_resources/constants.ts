fe3f9da217da008389743a78bb7b61feabaff332
Osama Rahman <osama@yulife.com>
2024-07-01 15:47:21 +0000

    Detox: INTL-433 money mastery purge

--- e2e/activity/FWQ/_resources/constants.ts
+++
@@ -1,8 +0,0 @@
-// Financial Wellness Quiz
-
-export const quizCompletedTitle = (event: string) => `${event} event finished`
-export const quizCompletedHeader = "Great job!"
-export const quizCompletedAnimatedCircle = "#40C057"
-export const quizCompletedYuCoin = "YuCoin"
-export const quizCompletedClaimed = "Claimed"
-export const quizCompletedButton = "Great!" 
