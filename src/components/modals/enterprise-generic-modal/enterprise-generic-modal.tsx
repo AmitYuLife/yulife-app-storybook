3cdfa2e1ffe17847ae28a489531fd8709b0506dc
Jeff Filho <jeff@yulife.com>
2024-07-02 15:51:35 +0100

    Task: Create EnterpriseGenericModal component

---
+++ src/components/modals/enterprise-generic-modal/enterprise-generic-modal.tsx
@@ -0,0 +1,31 @@
+import React, { memo } from "react";
+import { StyleSheet, View } from "react-native";
+import { TextTemplate } from "@atoms";
+import { Style } from "@styles";
+
+interface IProps {
+  title: string;
+  description: string;
+}
+
+const EnterpriseGenericModal = ({ title, description }: IProps) => (
+  <View>
+    <TextTemplate textAlign="center" type="h1">
+      {title}
+    </TextTemplate>
+    <View style={styles.description}>
+      <TextTemplate textAlign="center" type="b1">
+        {description}
+      </TextTemplate>
+    </View>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  description: {
+    marginHorizontal: Style.adjust(30),
+    marginTop: Style.adjust(20),
+  },
+});
+
+export default memo(EnterpriseGenericModal);
