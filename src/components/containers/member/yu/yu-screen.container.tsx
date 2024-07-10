096b68160b314da80b56097776046117609ea99b
Adrian Cilia <adrian@yulife.com>
2024-07-10 09:28:21 +0000

    Chore: default to yuscreen v5 with toggle

--- src/components/containers/member/yu/yu-screen.container.tsx
+++ src/components/containers/member/yu/yu-screen.container.tsx
@@ -1,22 +1,28 @@
-import React, { memo, useMemo, useState } from "react";
+import React, { memo, useEffect, useMemo, useState } from "react";
 import { YuScreen as YuScreenV4 } from "./subcomponents/yu-screen/yu-screen";
 import { YuScreen as YuScreenV5 } from "./subcomponents/yu-screen-v5/yu-screen";
 import { useSelector } from "react-redux";
 import { useTapBackTwiceToExit } from "@hooks";
 import { IMainTabsProps as Props } from "@navigation/root";
 import { YuScreenContext } from "./context/yu-screen.context";
-import { getUserAvatar, getUserEarnRate } from "@redux/user/user.selectors";
+import { getUserAvatar, getUserEarnRate, getUserFeatures } from "@redux/user/user.selectors";
 import { DevVersionSelector } from "@organisms";
 
 const VERSIONS_AVAILABLE = ["4", "5"];
+const YU_SCREEN_V5_TOGGLE = "tempEnableYuScreenV5";
 
 const YuScreenContainer = memo(({ componentId }: Props) => {
   useTapBackTwiceToExit(componentId);
   const avatar = useSelector(getUserAvatar);
   const earnRate = useSelector(getUserEarnRate);
+  const features = useSelector(getUserFeatures);
   const [version, setVersion] = useState("4");
 
   const yumojiRemoteUrl = avatar.avatarRemoteFiles?.pngFull;
+
+  useEffect(() => {
+    setVersion(features[YU_SCREEN_V5_TOGGLE] ? "5" : "4");
+  }, [features]);
 
   const YuScreen = useMemo(() => {
     switch (version) {
@@ -34,7 +40,7 @@ const YuScreenContainer = memo(({ componentId }: Props) => {
         <YuScreen componentId={componentId} />
       </YuScreenContext.Provider>
       <DevVersionSelector
-        feature="tempEnableYuScreenV5"
+        feature={YU_SCREEN_V5_TOGGLE}
         marginTop={300}
         version={version}
         setVersion={setVersion}
