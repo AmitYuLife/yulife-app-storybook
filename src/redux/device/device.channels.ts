2619ab36c4b86f20b4c3b3b01dc89ae79afc9767
Vitaly Tr <vitaly@yulife.com>
2024-07-25 09:12:19 +0100

    Chore: push notification catch

--- src/redux/device/device.channels.ts
+++ src/redux/device/device.channels.ts
@@ -3,6 +3,7 @@ import Logger from "@services/logging/logger";
 
 import * as ExpoNotification from "expo-notifications";
 import { YULIFE_PN_CHANNEL_NAME, YULIFE_PN_CHANNEL_ID, YULIFE_PN_CHANNEL_DESCRIPTION } from "@services/constants";
+import { isAndroid } from "@utils";
 
 export async function createPushNotificationsChannel() {
   return eventChannel((emitter) => {
@@ -21,21 +22,25 @@ export async function createPushNotificationsChannel() {
       },
     });
 
-    ExpoNotification.setNotificationChannelAsync(YULIFE_PN_CHANNEL_ID, {
-      enableVibrate: true,
-      enableLights: true,
-      name: YULIFE_PN_CHANNEL_NAME,
-      description: YULIFE_PN_CHANNEL_DESCRIPTION,
-      importance: ExpoNotification.AndroidImportance.DEFAULT,
-      lockscreenVisibility: ExpoNotification.AndroidNotificationVisibility.PUBLIC,
-    });
+    if (isAndroid()) {
+      ExpoNotification.setNotificationChannelAsync(YULIFE_PN_CHANNEL_ID, {
+        enableVibrate: true,
+        enableLights: true,
+        name: YULIFE_PN_CHANNEL_NAME,
+        description: YULIFE_PN_CHANNEL_DESCRIPTION,
+        importance: ExpoNotification.AndroidImportance.DEFAULT,
+        lockscreenVisibility: ExpoNotification.AndroidNotificationVisibility.PUBLIC,
+      });
+    }
 
-    ExpoNotification.getDevicePushTokenAsync().then((value) =>
-      emitter({
-        os: value.type,
-        token: value.data,
-      })
-    );
+    ExpoNotification.getDevicePushTokenAsync()
+      .then((value) =>
+        emitter({
+          os: value.type,
+          token: value.data,
+        })
+      )
+      .catch((error) => Logger.error(error, { event: "pushNotification" }));
 
     return () => null;
   });
@@ -44,13 +49,15 @@ export async function createPushNotificationsChannel() {
 /** should rename this into check iOS permissions */
 export function createPushPermissionsChannel() {
   return eventChannel((emitter) => {
-    ExpoNotification.getPermissionsAsync().then((val) =>
-      emitter({
-        alert: val.ios.allowsAlert,
-        badge: val.ios.allowsBadge,
-        sound: val.ios.allowsSound,
-      })
-    );
+    ExpoNotification.getPermissionsAsync()
+      .then((val) =>
+        emitter({
+          alert: val.ios.allowsAlert,
+          badge: val.ios.allowsBadge,
+          sound: val.ios.allowsSound,
+        })
+      )
+      .catch((error) => Logger.error(error, { event: "pushNotification" }));
 
     return () => null;
   });
