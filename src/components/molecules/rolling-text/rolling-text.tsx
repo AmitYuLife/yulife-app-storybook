5d3a2802f9fffd85dc48f6bc366a50cfe50534b9
Manolis Vrondakis <manolis.vrondakis@yulife.com>
2024-07-29 10:27:55 +0000

    Task: Rolling text component

---
+++ src/components/molecules/rolling-text/rolling-text.tsx
@@ -0,0 +1,32 @@
+import { RollingTextItem, Stack } from "@atoms";
+import { Style } from "@styles";
+import React, { memo, useMemo } from "react";
+import { StyleSheet } from "react-native";
+
+interface IRollingTextSpinProps {
+  previousValue: string | number;
+  newValue: string | number;
+}
+
+const RollingTextSpin = ({ previousValue, newValue }: IRollingTextSpinProps) => {
+  const elements = useMemo(() => {
+    const nodes = `${newValue}`.split("");
+    const previousNodes = `${previousValue}`.split("");
+
+    return nodes.map((node, index) => {
+      return <RollingTextItem oldValue={previousNodes[index]} value={node} key={index} />;
+    });
+  }, [previousValue, newValue]);
+
+  return (
+    <Stack style={styles.container} gap={Style.adjust(2)} direction="row" justifyContent={"center"}>
+      {elements}
+    </Stack>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { overflow: "hidden", height: Style.adjust(52) },
+});
+
+export default memo(RollingTextSpin);
