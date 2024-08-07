2ba1360a78c3ce7d3af29f1c91fc0fa595c0eeb3
Adrian Cilia <adrian@yulife.com>
2024-08-07 10:59:53 +0000

    Bugfix: undo change to battle-pass start index for auto scroll

--- src/components/organisms/battle-pass-list/battle-pass-list.tsx
+++ src/components/organisms/battle-pass-list/battle-pass-list.tsx
@@ -42,7 +42,7 @@ const BattlePassList = forwardRef(
 
         if (startIndex >= 0) {
           listRef.current?.scrollToIndex({
-            index: focusedIndex,
+            index: startIndex,
             animated: false,
             viewOffset: Style.adjust(7) + animationOffset,
           });
