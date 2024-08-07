bfe81300b1b536c60b3daab8b464cbc3772f62fa
Boris <boris@yulife.com>
2024-08-07 20:39:22 +0300

    Task: Coins reducer toolkit

--- src/redux/coins/coins.actions.ts
+++ src/redux/coins/coins.actions.ts
@@ -2,6 +2,6 @@ import { createAction } from "@reduxjs/toolkit";
 export const UPDATE_TOTAL_COINS = "UPDATE_TOTAL_COINS";
 export const REFRESH_TOTAL_COINS = "REFRESH_TOTAL_COINS";
 
-export const totalCoinsUpdated = createAction<number>(UPDATE_TOTAL_COINS);
+export const totalCoinsUpdated = createAction<number, typeof UPDATE_TOTAL_COINS>(UPDATE_TOTAL_COINS);
 
 export const refreshTotalCoins = createAction(REFRESH_TOTAL_COINS);
