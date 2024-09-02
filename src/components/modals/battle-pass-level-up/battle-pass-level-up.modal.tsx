738bb59722b7465d24a139f358a98d41f94a2803
Manolis Vrondakis <manolis.vrondakis@yulife.com>
2024-09-02 11:21:27 +0000

    Task: Level up modal changes

--- src/components/modals/battle-pass-level-up/battle-pass-level-up.modal.tsx
+++ src/components/modals/battle-pass-level-up/battle-pass-level-up.modal.tsx
@@ -4,11 +4,13 @@ import { t } from "@locale";
 import PodiumRays from "@organisms/podium/podium-rays";
 import { Colours, Style } from "@styles";
 import LottieViewRef from "lottie-react-native";
-import { memo, useEffect, useRef } from "react";
-import { StyleSheet, View } from "react-native";
+import { memo, useEffect, useMemo, useRef } from "react";
+import { ScrollView, StyleSheet, View, ViewStyle } from "react-native";
 import Animated, { FadeIn, FadeInDown } from "react-native-reanimated";
 import { RollingText } from "@organisms";
 import { GetMobileGameBattlePassQuery } from "@graphql/__generated";
+import { useSafeAreaViewOffset } from "@hooks";
+import BlurredOverlay from "../blurred-overlay/blurred-overlay";
 
 interface IBattlePassLevelUpModalProps {
   onClose: () => void;
@@ -26,40 +28,59 @@ const BattlePassLevelUpModal = ({ onClose, reward }: IBattlePassLevelUpModalProp
     return () => clearTimeout(timeout);
   }, []);
 
+  const offset = useSafeAreaViewOffset();
+
+  const scrollStyle = useMemo(
+    (): ViewStyle => ({
+      width: "100%",
+      minHeight: Style.DEVICE_HEIGHT - offset.safeAreaViewOffset.y - Style.adjust(40),
+    }),
+    [offset.safeAreaViewOffset]
+  );
+
   if (!reward) {
     return null;
   }
 
   return (
-    <Animated.View entering={FadeIn.duration(200)} style={styles.wrapper}>
-      <View style={styles.raysWrapper}>
-        <Animated.View style={styles.rays} entering={FadeIn.delay(300).duration(800)}>
-          <PodiumRays backgroundColor={"transparent"} style="alternate" />
-        </Animated.View>
-      </View>
-      <Animated.View entering={FadeInDown.delay(200).duration(500)} style={styles.levelUpText}>
-        <TextTemplate type="h1" color={Colours.neutral.white} textAlign="center">
-          {t("screens.battle_pass.level_up.title")}
-        </TextTemplate>
-        <RollingText previousValue={Math.max(reward.position - 1, 0)} newValue={reward.position} />
+    <BlurredOverlay withBlurBackground={true} blurAmount={3} blurType="dark" backgroundColor="rgba(0,0,0,.1)">
+      <Animated.View entering={FadeIn.duration(200)} style={styles.wrapper}>
+        <View style={styles.raysWrapper}>
+          <Animated.View style={styles.rays} entering={FadeIn.delay(300).duration(800)}>
+            <PodiumRays backgroundColor={"transparent"} style="alternate" />
+          </Animated.View>
+        </View>
+        <ScrollView showsVerticalScrollIndicator={false} bounces={false} contentContainerStyle={scrollStyle}>
+          <View style={styles.contentContainer}>
+            <Animated.View entering={FadeInDown.delay(200).duration(500)} style={styles.levelUpText}>
+              <TextTemplate type="h2" color={Colours.neutral.white} textAlign="center">
+                {t("screens.battle_pass.level_up.title")}
+              </TextTemplate>
+              <RollingText
+                previousValue={`${Math.max(reward.position - 1, 0)}`.padStart(2, "0")}
+                newValue={`${reward.position}`.padStart(2, "0")}
+              />
+            </Animated.View>
+
+            <View style={styles.imageWrapper}>
+              <Animated.View entering={FadeInDown.delay(700).duration(600)} style={styles.animatedImageWrapper}>
+                <LottieView
+                  ref={lottieRef}
+                  source={require("./enterprise-glow.json")}
+                  style={{ width: Style.adjust(230), height: Style.adjust(250) }}
+                  loop={true}
+                />
+                <Image style={styles.rewardOverlayIcon} width={Style.adjust(200)} source={reward.overlayIcon} />
+              </Animated.View>
+            </View>
+          </View>
+
+          <Stack style={styles.buttonsWrapper} gap={5}>
+            <Button translationKey={"labels.cta.continue"} onPress={onClose} />
+          </Stack>
+        </ScrollView>
       </Animated.View>
-
-      <View style={styles.imageWrapper}>
-        <Animated.View entering={FadeInDown.delay(700).duration(600)} style={styles.animatedImageWrapper}>
-          <LottieView
-            ref={lottieRef}
-            source={require("./enterprise-glow.json")}
-            style={{ width: Style.adjust(230), height: Style.adjust(250) }}
-            loop={true}
-          />
-          <Image style={styles.rewardOverlayIcon} width={Style.adjust(200)} source={reward.overlayIcon} />
-        </Animated.View>
-      </View>
-
-      <Stack style={styles.buttonsWrapper} gap={5}>
-        <Button translationKey={"labels.cta.continue"} onPress={onClose} />
-      </Stack>
-    </Animated.View>
+    </BlurredOverlay>
   );
 };
 
@@ -71,15 +92,15 @@ const styles = StyleSheet.create({
     height: "100%",
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: `rgba(0,0,0,0.4)`,
+  },
+  contentContainer: {
+    paddingTop: Style.adjust(40),
   },
   buttonsWrapper: {
     flex: 1,
-    marginTop: Style.adjust(100),
     justifyContent: "flex-end",
-    paddingBottom: Style.adjust(45),
   },
-  levelUpText: { position: "absolute", top: 100 },
+  levelUpText: { gap: Style.adjust(14) },
   rewardOverlayIcon: { position: "absolute", top: Style.adjust(25), left: Style.adjust(25) },
   rays: { width: "100%", height: "100%", position: "absolute", top: Style.adjust(-130) },
   raysWrapper: { width: "100%", height: "100%", position: "absolute", opacity: 0.4 },
@@ -89,7 +110,15 @@ const styles = StyleSheet.create({
     width: Style.adjust(250),
     height: Style.adjust(250),
   },
-  imageWrapper: { marginBottom: Style.adjust(50), paddingTop: Style.adjust(160), marginTop: Style.adjust(100) },
+  imageWrapper: {
+    paddingTop: Style.adjust(10),
+    marginTop: Style.adjust(100),
+    position: "absolute",
+    top: Style.adjust(60),
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default memo(BattlePassLevelUpModal);
