abe68d6f53cbd767ee1887cc0e014425ffbee8ab
Boris <boris@yulife.com>
2024-09-11 13:00:21 +0300

    Bugfix: Use separate effects for daily passive

--- src/redux/dailyPassiveActivity/sagas/index.ts
+++ src/redux/dailyPassiveActivity/sagas/index.ts
@@ -5,5 +5,6 @@ import { UPDATE_APP_STATE } from "@redux/app/app.actions";
 import { UPDATE_IN_APP_MEDITATION } from "@redux/daily-meditation/daily-meditation.actions";
 
 export default [
-  takeLatest([UPDATE_APP_STATE, UPDATE_IN_APP_MEDITATION, GET_USER_TODAY_ACTIVITY_SUCCESS], getDailyPassiveActivity),
+  takeLatest([UPDATE_APP_STATE, UPDATE_IN_APP_MEDITATION], getDailyPassiveActivity),
+  takeLatest([GET_USER_TODAY_ACTIVITY_SUCCESS], getDailyPassiveActivity),
 ];
