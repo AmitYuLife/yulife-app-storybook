89e71016beb8f5cf2b4c5f2455c04b29689b6b7f
Manolis Vrondakis <manolis.vrondakis@yulife.com>
2024-10-08 09:24:49 +0000

    Task: Add react-native-safe-area-context

--- src/navigation/withProvider.tsx
+++ src/navigation/withProvider.tsx
@@ -5,6 +5,7 @@ import { ComponentClass } from "react";
 import React from "react";
 import { generateOnLeftMenuPress } from "./root";
 import { NavigationContext } from "./navigation.context";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const withProvider =
   (WrappedComponent: ComponentClass, client: ApolloClient<Record<string, unknown>>, hasMenu = false) =>
@@ -12,14 +13,16 @@ const withProvider =
     return (
       <Provider store={store}>
         <ApolloProvider client={client}>
-          <NavigationContext.Provider
-            value={{
-              componentId: props.componentId,
-              onLeftMenuPress: hasMenu ? generateOnLeftMenuPress(props.componentId) : null,
-            }}
-          >
-            <WrappedComponent {...props} />
-          </NavigationContext.Provider>
+          <SafeAreaProvider>
+            <NavigationContext.Provider
+              value={{
+                componentId: props.componentId,
+                onLeftMenuPress: hasMenu ? generateOnLeftMenuPress(props.componentId) : null,
+              }}
+            >
+              <WrappedComponent {...props} />
+            </NavigationContext.Provider>
+          </SafeAreaProvider>
         </ApolloProvider>
       </Provider>
     );
