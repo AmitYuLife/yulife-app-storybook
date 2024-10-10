0f8523d074f8a252edc2dd4d8b42697a3321af24
Manolis Vrondakis <manolis.vrondakis@yulife.com>
2024-10-10 14:48:39 +0000

    Task: Convert app.json to app.config.ts

---
+++ app.config.ts
@@ -0,0 +1,11 @@
+export default () => ({
+  name: "YuLife",
+  displayName: "yulife",
+  android: {
+    package: "com.yulife.debug",
+  },
+  ios: {
+    bundleIdentifier: "com.yulife.debug",
+  },
+  icon: "./assets/native/app-icon.png",
+});
