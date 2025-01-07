a317a63a1239acf6eb4d77e1496c97be731ed2e2
Jeff Filho <jeff@yulife.com>
2025-01-07 16:24:46 +0000

    Purge GS-1122: audio-player component
    See merge request yu-life/yulife-rn-client!5530

--- src/components/screens/member/media/media-player/media-player.screen.tsx
+++ src/components/screens/member/media/media-player/media-player.screen.tsx
@@ -1,8 +1,7 @@
 import React, { memo, useMemo } from "react";
-import { AudioPlayer, VideoPlayer } from "@organisms";
+import { VideoPlayer } from "@organisms";
 import { IVideoPlayerProps } from "@organisms/video-player/video-player";
 import { t } from "@locale";
-import { useUserFeatures } from "@hooks";
 import { MediaFragment } from "@graphql/__generated";
 
 interface IVideo extends MediaFragment {
@@ -44,73 +43,38 @@ const MediaPlayerScreen = ({
   startChallengeButtonLabel,
   autoPlay,
 }: IMediaPlayerScreenProps) => {
-  const { enableAudioPlayer } = useUserFeatures();
   const subtitle = useMemo(() => t(`screens.video_player.video_type.${eventType}`), [eventType]);
 
   return (
-    <>
-      {video.sourceType === "mp3" && enableAudioPlayer ? (
-        <AudioPlayer
-          autoPlay={autoPlay}
-          startTimeInSeconds={startTimeInSeconds}
-          source={video.media.uri}
-          poster={video.cover.uri}
-          audioDuration={video.duration}
-          tag={video.tag}
-          title={video.title}
-          subtitle={subtitle}
-          thumbnail={video.thumbnail.uri}
-          logo={video.logo.uri}
-          onProgress={onProgress}
-          audioLogo={video?.videoLogo?.uri}
-          description={video.description}
-          shortDescription={video.shortDescription}
-          theme={video.theme as "light" | "dark"}
-          yuCoin={video.reward}
-          stars={video.stars}
-          onLeftIconPress={onLeftIconPress}
-          onRightIconPress={onRightIconPress}
-          onStart={onStart}
-          onEnd={onEnd}
-          onError={onError}
-          startErrorMessage={startErrorMessage}
-          lottie={video?.lottie}
-          eventType={eventType}
-          startChallengeButtonLabel={startChallengeButtonLabel}
-          audioSourceType={video.sourceType}
-        />
-      ) : (
-        <VideoPlayer
-          autoPlay={autoPlay}
-          startTimeInSeconds={startTimeInSeconds}
-          source={video.media.uri}
-          poster={video.cover.uri}
-          tag={video.tag}
-          title={video.title}
-          subtitle={subtitle}
-          thumbnail={video.thumbnail.uri}
-          logo={video.logo.uri}
-          onProgress={onProgress}
-          videoLogo={video?.videoLogo?.uri}
-          description={video.description}
-          shortDescription={video.shortDescription}
-          theme={video.theme as "light" | "dark"}
-          yuCoin={video.reward}
-          stars={video.stars}
-          onLeftIconPress={onLeftIconPress}
-          onRightIconPress={onRightIconPress}
-          onStart={onStart}
-          onEnd={onEnd}
-          onError={onError}
-          startErrorMessage={startErrorMessage}
-          lottie={video?.lottie}
-          eventType={eventType}
-          orientation={orientation}
-          startChallengeButtonLabel={startChallengeButtonLabel}
-          videoSourceType={video.sourceType}
-        />
-      )}
-    </>
+    <VideoPlayer
+      autoPlay={autoPlay}
+      startTimeInSeconds={startTimeInSeconds}
+      source={video.media.uri}
+      poster={video.cover.uri}
+      tag={video.tag}
+      title={video.title}
+      subtitle={subtitle}
+      thumbnail={video.thumbnail.uri}
+      logo={video.logo.uri}
+      onProgress={onProgress}
+      videoLogo={video?.videoLogo?.uri}
+      description={video.description}
+      shortDescription={video.shortDescription}
+      theme={video.theme as "light" | "dark"}
+      yuCoin={video.reward}
+      stars={video.stars}
+      onLeftIconPress={onLeftIconPress}
+      onRightIconPress={onRightIconPress}
+      onStart={onStart}
+      onEnd={onEnd}
+      onError={onError}
+      startErrorMessage={startErrorMessage}
+      lottie={video?.lottie}
+      eventType={eventType}
+      orientation={orientation}
+      startChallengeButtonLabel={startChallengeButtonLabel}
+      videoSourceType={video.sourceType}
+    />
   );
 };
 
