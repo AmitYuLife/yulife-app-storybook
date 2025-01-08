7eacd5e2beabe8cdf69f2adedba64e10e6b51851
Ivan Varga <ivan@yulife.com>
2025-01-08 14:22:29 +0000

    Task GS-1113: Auto Apply Streak Savers
    See merge request yu-life/yulife-rn-client!5505

--- src/redux/streaks/sagas/showStreakSavedModal.saga.tsx
+++ src/redux/streaks/sagas/showStreakSavedModal.saga.tsx
@@ -1,19 +1,31 @@
-import { select } from "redux-saga/effects";
+import { gql } from "@graphql/__generated";
+import client from "@graphql/_core/client";
 import StreakSavedModal from "@modals/streak-saved/streak-saved.modal";
+// import { MODALS } from "@navigation/constants";
 import { Navigation } from "@navigation/main";
-import { getUserFeatures } from "@redux/user/user.selectors";
 import { getUserActiveStreakSuccess } from "@redux/user/user.actions";
+import { getUserFeatures } from "@redux/user/user.selectors";
+import { call, select } from "redux-saga/effects";
 
 export default function* showStreakSavedModalSaga(dataPayload: ReturnType<typeof getUserActiveStreakSuccess>) {
   const { payload } = dataPayload ?? {};
-
   const features: ReturnType<typeof getUserFeatures> = yield select(getUserFeatures);
   if (!features.tempGameEnableStreakSaver) {
     return;
   }
 
   if (payload.activeStreak.canUseStreakSaver) {
-    const frameModal = <StreakSavedModal onClose={() => Navigation.dismissOverlayWithChild()} />;
-    Navigation.showOverlayWithChild(frameModal, false);
+    const frameModal = (
+      <StreakSavedModal
+        onClose={() => Navigation.dismissOverlayWithChild()}
+        streakSaverCount={payload.activeStreak.availableStreakSavers}
+      />
+    );
+    yield call(() =>
+      client().mutate({
+        mutation: gql("RestoreStreakDocument"),
+      })
+    );
+    yield call(() => Navigation.showOverlayWithChild(frameModal, false));
   }
 }
