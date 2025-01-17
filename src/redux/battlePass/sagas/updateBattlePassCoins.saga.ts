8a5761fe3ea9b5d9aa43d3ce732f54d2cde81ea4
Ivan Varga <ivan@yulife.com>
2025-01-17 12:54:06 +0000

    ENG-4160: Update Battle Pass balance on challenge
    See merge request yu-life/yulife-rn-client!5576

---
+++ src/redux/battlePass/sagas/updateBattlePassCoins.saga.ts
@@ -0,0 +1,42 @@
+import { totalCoinsUpdated } from "@redux/coins/coins.actions";
+import Logger from "@services/logging/logger";
+import { spawn } from "redux-saga/effects";
+import client from "@graphql/_core/client";
+import {
+  MobileGameBattlePassProgressInfo,
+  MobileGameBattlePassProgressInfoFragmentDoc,
+} from "@graphql/__generated/graphql";
+import { getUserCoinLedgerSuccess } from "@redux/user/user.actions";
+
+export default function* updateBattlePassCoinsSaga(
+  dataPayload: ReturnType<typeof totalCoinsUpdated> | ReturnType<typeof getUserCoinLedgerSuccess>
+) {
+  try {
+    let coinledgerBalance = 0;
+    if (dataPayload.type === "GET_USER_COIN_LEDGER_SUCCESS") {
+      coinledgerBalance = dataPayload.payload.total;
+    } else if (dataPayload.type === "UPDATE_TOTAL_COINS") {
+      coinledgerBalance = dataPayload.payload;
+    } else {
+      return;
+    }
+
+    const apolloClient = client();
+    Object.entries(apolloClient.cache.extract())
+      .filter(([key]) => key.startsWith(`MobileGameBattlePassProgressInfo:`))
+      .forEach(([key, value]) => {
+        apolloClient.cache.writeFragment({
+          id: key,
+          fragment: MobileGameBattlePassProgressInfoFragmentDoc,
+          data: {
+            ...(value as unknown as MobileGameBattlePassProgressInfo),
+            currentBalance: coinledgerBalance,
+          },
+        });
+      });
+  } catch (e) {
+    yield spawn(() => {
+      Logger.error(e, { event: "updateBattlePassCoins" });
+    });
+  }
+}
