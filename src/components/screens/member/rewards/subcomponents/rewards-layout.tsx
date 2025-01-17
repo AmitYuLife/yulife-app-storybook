4aee318a008398e5b304778dbe956d8d2d025f49
Jeff Filho <jeff@yulife.com>
2025-01-17 14:07:48 +0000

    Task GS-1176: Create coupons tabs
    See merge request yu-life/yulife-rn-client!5582

--- src/components/screens/member/rewards/subcomponents/rewards-layout.tsx
+++ src/components/screens/member/rewards/subcomponents/rewards-layout.tsx
@@ -1,28 +1,60 @@
-import React from "react";
+import React, { ReactNode, useState } from "react";
 import { StyleSheet, ViewStyle, View, Platform } from "react-native";
 import { REWARDS_SCREEN } from "@ids";
 import { LeftIcon } from "@organisms/top-bar/subcomponents/left";
 import { GenericHeadingPad, NavBar, TopBarAbsolute } from "@organisms";
+import { Box, TextTemplate } from "@atoms";
+import { TouchableOpacityWithDelay } from "@molecules";
+import { Colours, Style } from "@styles";
+import { useUserFeatures } from "@hooks";
+import { t } from "@locale";
 
 const WRAPPER_MARGIN_TOP = Platform.select({
   ios: 0,
   android: -20,
 });
 interface Props {
-  children: React.ReactNode;
+  children: ReactNode;
   onLeftMenuPress: () => void;
   hasBackButton?: boolean;
-  Overlay?: React.ReactNode | null;
+  Overlay?: ReactNode | null;
   showNavbar?: boolean;
   showTopBar?: boolean;
 }
 
 export function RewardsListLayout({ showNavbar = true, showTopBar, ...props }: Props) {
+  const [activeTab, setActiveTab] = useState(1);
   const { children, onLeftMenuPress, hasBackButton, Overlay } = props;
+  const { tempGameShowCouponsTab } = useUserFeatures();
 
   return (
     <View style={styles.wrapper} testID={REWARDS_SCREEN}>
-      {!showTopBar ? null : <GenericHeadingPad />}
+      {!showTopBar ? null : (
+        <>
+          <GenericHeadingPad />
+          {!tempGameShowCouponsTab ? null : (
+            <>
+              {/*@TODO: Another RewardsTab, ChipList, Tabs... this will need to be merged into a single component in the future!*/}
+              <Box flexDirection="row" ml={15} gap={10}>
+                {MENUS().map(({ id, label }) => {
+                  const isActive = activeTab === id;
+                  return (
+                    <TouchableOpacityWithDelay
+                      onPress={() => setActiveTab(id)}
+                      key={id}
+                      style={[styles.button, isActive ? styles.buttonActive : styles.buttonInactive]}
+                    >
+                      <TextTemplate type="b2b" color={isActive ? "white" : "#464647"}>
+                        {label}
+                      </TextTemplate>
+                    </TouchableOpacityWithDelay>
+                  );
+                })}
+              </Box>
+            </>
+          )}
+        </>
+      )}
       <View style={styles.listWrapper}>{children}</View>
       {!showTopBar ? null : (
         <TopBarAbsolute leftIcon={hasBackButton ? LeftIcon.BACK : LeftIcon.MENU} onPressLeftIcon={onLeftMenuPress} />
@@ -32,6 +64,11 @@ export function RewardsListLayout({ showNavbar = true, showTopBar, ...props }: P
     </View>
   );
 }
+
+const MENUS = () => [
+  { id: 1, label: t("history") },
+  { id: 2, label: t("coupons") },
+];
 
 const styles = StyleSheet.create({
   wrapper: {
@@ -45,4 +82,18 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginTop: WRAPPER_MARGIN_TOP,
   } as ViewStyle,
+  button: {
+    paddingHorizontal: Style.adjust(10),
+    paddingVertical: Style.adjust(3),
+    borderRadius: 100,
+    borderWidth: 1,
+  },
+  buttonActive: {
+    borderColor: "#F43E8E",
+    backgroundColor: "#CC0D6E",
+  },
+  buttonInactive: {
+    borderColor: "#E3E3E1",
+    backgroundColor: Colours.neutral.white,
+  },
 });
