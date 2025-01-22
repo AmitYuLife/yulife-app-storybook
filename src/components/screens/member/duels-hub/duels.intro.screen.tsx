e59bc065d4070bcc5380ca6b7ca885094d23b7c7
Boris <boris@yulife.com>
2025-01-22 11:45:16 +0000

    Task: Duel intro cleanup
    See merge request yu-life/yulife-rn-client!5620

--- src/components/screens/member/duels-hub/duels.intro.screen.tsx
+++ src/components/screens/member/duels-hub/duels.intro.screen.tsx
@@ -1,6 +1,6 @@
-import React, { useMemo } from "react";
-import { Image, View, ListRenderItemInfo, StyleSheet, TextStyle, ViewStyle, ImageStyle } from "react-native";
-import { Text } from "@atoms";
+import { useMemo, memo } from "react";
+import { Image, ListRenderItemInfo, StyleSheet, ImageStyle } from "react-native";
+import { TextTemplate, Box } from "@atoms";
 import { OnboardingSwiper, OnboardingSwiperData } from "@organisms";
 import { Style, Colours } from "@styles";
 import { t } from "@locale";
@@ -36,60 +36,36 @@ interface Props {
   setOnboardingShown: () => void;
 }
 
-function _DuelsIntroScreen(props: Props) {
+function DuelsIntroScreen(props: Props) {
   const data = useMemo(() => buildOnboardingData(), []);
   return <OnboardingSwiper data={data} renderItem={renderItem} onClose={props.setOnboardingShown} type="duels" />;
 }
 
-const DuelsIntroScreen = React.memo(_DuelsIntroScreen);
-
-export default DuelsIntroScreen;
+export default memo(DuelsIntroScreen);
 
 function renderItem({ item, index }: ListRenderItemInfo<OnboardingSwiperData>) {
   return (
-    <View style={styles.wrapper}>
-      <View style={styles.imageWrapper}>
+    <Box flex={1} width={Style.DEVICE_WIDTH} justifyContent="center">
+      <Box height={320} width={320} alignSelf="center">
         <Image style={styles.image} source={images[index]} />
-      </View>
-      <Text bold={true} style={styles.title}>
-        {item.title}
-      </Text>
-      <Text style={styles.subTitle}>{item.subtitle}</Text>
-    </View>
+      </Box>
+      <Box alignSelf="center" mt={Style.isShortToMediumAndroid() ? 17 : 34}>
+        <TextTemplate type="h3" color={Colours.neutral.n800}>
+          {item.title}
+        </TextTemplate>
+      </Box>
+      <Box alignSelf="center" alignContent="center" mt={16} w={280}>
+        <TextTemplate type="b2" textAlign="center" color={Colours.neutral.n800}>
+          {item.subtitle}
+        </TextTemplate>
+      </Box>
+    </Box>
   );
 }
 
 const styles = StyleSheet.create({
-  imageWrapper: {
-    height: Style.adjust(320),
-    width: Style.adjust(320),
-    alignSelf: "center",
-  } as ViewStyle,
-  wrapper: {
-    flex: 1,
-    width: Style.DEVICE_WIDTH,
-    justifyContent: "center",
-  } as ViewStyle,
   image: {
     height: Style.adjust(320),
     width: Style.adjust(320),
   } as ImageStyle,
-  title: {
-    alignSelf: "center",
-    marginTop: Style.adjust(Style.isShortToMediumAndroid() ? 17 : 34),
-    fontSize: Style.adjust(24),
-    letterSpacing: 0.8,
-    color: Colours.neutral.n800,
-  } as TextStyle,
-  subTitle: {
-    alignSelf: "center",
-    alignContent: "center",
-    textAlign: "center",
-    marginTop: Style.adjust(18),
-    fontSize: Style.adjust(16),
-    lineHeight: Style.adjust(24),
-    width: 280,
-    letterSpacing: 0.8,
-    color: Colours.neutral.n800,
-  } as TextStyle,
 });
