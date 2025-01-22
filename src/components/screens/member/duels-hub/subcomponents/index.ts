7475f35db028dac9db43048bc4aea6ee53e22764
Boris <boris@yulife.com>
2025-01-22 11:59:59 +0000

    Task: Remove duel empty
    See merge request yu-life/yulife-rn-client!5618

--- src/components/screens/member/duels-hub/subcomponents/index.ts
+++ src/components/screens/member/duels-hub/subcomponents/index.ts
@@ -1,4 +1,3 @@
 export { default as DuelEntry } from "./duel-entry/duel-entry";
-export { default as DuelEmpty } from "./duel-empty/duel-empty";
 export { default as DuelImage } from "./duel-image/duel-image";
 export { default as DuelTabs } from "./duel-tabs/duels-tabs";
