98ceb0349da924782972fd006815ed1a50c85d20
Vitaly Trus <vitaly@yulife.com>
2025-01-24 10:28:29 +0000

    Task: add android deep links
    See merge request yu-life/yulife-rn-client!5645

--- plugins/yulife/with-yulife-android-manifest.plugin.js
+++ plugins/yulife/with-yulife-android-manifest.plugin.js
@@ -4,6 +4,31 @@ module.exports = (app) => {
   return withAndroidManifest(app, async (config) => {
     let androidManifest = config.modResults.manifest;
 
+    // ignore intents in queries
+    androidManifest.queries = androidManifest.queries.filter((q) => (q?.intent || []).length === 0);
+
+    // change the launchMode from singleTask to singleInstance
+    androidManifest.application[0].activity[0].$["android:launchMode"] = "singleInstance";
+
+    androidManifest.application[0].activity[0]["intent-filter"] = androidManifest.application[0].activity[0][
+      "intent-filter"
+    ].map((intentFilter) => {
+      if (intentFilter.action[0].$["android:name"] === "android.intent.action.VIEW") {
+        // attach the app name to the deep link
+        intentFilter.$ = { "android:label": process.env.ANDROID_DEEP_LINK_APP_NAME };
+        intentFilter.data = intentFilter.data.map((d) => {
+          // attach the host to the yulifeapp scheme
+          if (d.$["android:scheme"] === "yulifeapp") {
+            d.$["android:host"] = "yulife";
+          }
+
+          return d;
+        });
+      }
+
+      return intentFilter;
+    });
+
     androidManifest.application[0].$["android:largeHeap"] = "true";
 
     androidManifest.application[0]["meta-data"].push(
