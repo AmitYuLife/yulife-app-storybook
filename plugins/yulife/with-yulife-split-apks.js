501d56c8f0017d304c60f1c37ef25eb956286361
Vitaly Trus <vitaly@yulife.com>
2025-01-24 12:11:03 +0000

    Task: override version code for android splits
    See merge request yu-life/yulife-rn-client!5646

--- plugins/yulife/with-yulife-split-apks.js
+++ plugins/yulife/with-yulife-split-apks.js
@@ -8,14 +8,36 @@ module.exports = (config) => {
     splitContents.splice(
       androidLine + 1,
       0,
-      `splits {
+      `    splits {
         abi {
             reset()
             enable true
             universalApk false
             include "armeabi-v7a", "arm64-v8a", "x86", "x86_64"
         }
-    }`
+    }\n`
+    );
+
+    splitContents.splice(
+      androidLine - 1,
+      0,
+      `\next.abiCodes = [
+    "armeabi-v7a": 1,
+    "x86"       : 2,
+    "arm64-v8a" : 3,
+    "x86_64"    : 4
+]
+
+android.applicationVariants.all { variant ->
+    variant.outputs.each { output ->
+        def abi = output.getFilter(com.android.build.OutputFile.ABI)
+        if (abi != null) { // Only override for ABI-specific APKs
+            def baseAbiVersionCode = project.ext.abiCodes.get(abi)
+            // check if we can use 'variant.versionCode' instead of 'project.android.defaultConfig.versionCode'
+            output.versionCodeOverride = project.android.defaultConfig.versionCode * 1000 + baseAbiVersionCode
+        }
+    }
+}`
     );
 
     app.modResults.contents = splitContents.join(`\n`);
