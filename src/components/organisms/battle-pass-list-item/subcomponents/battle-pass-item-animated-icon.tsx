7887bf798dfbc77610d825170b0c76231805c838
Manolis Vrondakis <manolis.vrondakis@yulife.com>
2025-02-04 12:42:31 +0000

    Bugfix GS-1236: Android reward item screen transition bug
    See merge request yu-life/yulife-rn-client!5695

--- src/components/organisms/battle-pass-list-item/subcomponents/battle-pass-item-animated-icon.tsx
+++ src/components/organisms/battle-pass-list-item/subcomponents/battle-pass-item-animated-icon.tsx
@@ -1,18 +1,16 @@
-import { Box, Source } from "@atoms";
+import { Source } from "@atoms";
 import MaskedView from "@react-native-masked-view/masked-view";
 import { Image } from "expo-image";
 import { memo, useEffect, useMemo, useState } from "react";
 import { ImageResizeMode, View, ViewStyle } from "react-native";
-import Animated, { Easing, FadeOut, ZoomIn } from "react-native-reanimated";
+import Animated from "react-native-reanimated";
+import BattlePassListItemTeaserAnimation from "./battle-pass-list-item-teaser-animation";
 
 interface BattlePassItemAnimatedIconProps {
   images: Source[];
   radius?: number;
   style?: ViewStyle;
 }
-
-const EXIT_DURATION = 500;
-const ENTER_DURATION = 1000;
 
 const AnimatedImage = Animated.createAnimatedComponent(Image);
 
@@ -35,23 +33,19 @@ const BattlePassItemAnimatedIcon = ({ radius = 24, images, style }: BattlePassIt
 
   const imageStyle = useMemo(() => {
     return {
-      height: radius,
-      width: radius,
-      resizeMode: "contain" as ImageResizeMode,
       flex: 1,
+      width: radius,
+      height: radius,
       background: "transparent",
+      resizeMode: "contain" as ImageResizeMode,
     };
   }, [radius]);
 
   return (
     <MaskedView style={style} maskElement={<View style={wrapperStyle} />}>
-      <Box
-        entering={ZoomIn.easing(Easing.elastic(1.1)).duration(ENTER_DURATION)}
-        exiting={FadeOut.duration(EXIT_DURATION)}
-        key={activeImage}
-      >
+      <BattlePassListItemTeaserAnimation key={activeImage}>
         <AnimatedImage source={images[activeImage]} style={imageStyle} />
-      </Box>
+      </BattlePassListItemTeaserAnimation>
     </MaskedView>
   );
 };
