a884813cb258170d0b9a8a7616e69579c47025e0
Vitaly Trus <vitaly@yulife.com>
2025-02-04 10:00:16 +0000

    Task: use Box for the event screen
    See merge request yu-life/yulife-rn-client!5693

--- src/components/screens/member/events/event-dialog/event-dialog.styles.ts
+++ src/components/screens/member/events/event-dialog/event-dialog.styles.ts
@@ -1,8 +1,8 @@
-import { StyleSheet, ViewStyle, Platform } from "react-native";
+import { StyleSheet, Platform } from "react-native";
 import { Style, Colours } from "@styles";
 import { PADDING_TOP } from "@styles/top-bar.styles";
 
-const STATUS_BAR_COVER_HEIGHT = Platform.select({
+export const STATUS_BAR_COVER_HEIGHT = Platform.select({
   ios: PADDING_TOP + Style.adjust(12),
   android: 0,
 });
@@ -26,28 +26,6 @@ export const SMOOTH_GRADIENT_COLORS = [
 ];
 
 export default StyleSheet.create({
-  wrapper: { flexGrow: 1 },
-  eventEndedBadgeWrapper: {
-    display: "flex",
-    position: "absolute",
-    alignItems: "center",
-    justifyContent: "center",
-    width: Style.DEVICE_WIDTH,
-    transform: [{ translateY: -Style.adjust(12) }],
-  },
-  eventEndedBadge: {
-    borderRadius: Style.adjust(4),
-    paddingVertical: Style.adjust(4),
-    paddingHorizontal: Style.adjust(20),
-    backgroundColor: Colours.primary.p600,
-  },
-  eventEndedBadgeSpacer: {
-    height: Style.adjust(10),
-  },
-  statusBarCover: {
-    width: Style.DEVICE_WIDTH,
-    height: STATUS_BAR_COVER_HEIGHT,
-  } as ViewStyle,
   headerImageContainer: {
     position: "absolute",
     top: PADDING_TOP,
@@ -77,36 +55,17 @@ export default StyleSheet.create({
     minHeight: "100%",
     paddingHorizontal: Style.adjust(24),
   },
-  rewardsWrapper: {
-    marginHorizontal: Style.adjust(-24),
-  },
-  progressText: { flexDirection: "row", paddingVertical: Style.adjust(8) },
   progressTextIcon: { marginRight: Style.adjust(4) },
   about: { marginTop: Style.adjust(40), marginBottom: Style.adjust(24) },
   aboutForVersus: { marginTop: Style.adjust(12), marginBottom: Style.adjust(24) },
   bannerWrapper: { marginBottom: Style.adjust(24) },
   banner: { paddingVertical: 0 },
   hintContainer: { marginBottom: Style.adjust(20) },
-  ctaPadding: { height: Style.adjust(60) },
   ctaWrapper: {
     position: "absolute",
     bottom: 0,
     width: Style.DEVICE_WIDTH,
     padding: Style.adjust(32),
     paddingTop: Style.adjust(50),
-  },
-  taskContainer: {
-    marginTop: Style.adjust(24),
-  },
-  tasksWrapper: {
-    marginTop: Style.adjust(16),
-  },
-  task: {
-    alignItems: "center",
-    flexDirection: "row",
-    marginBottom: Style.adjust(16),
-  },
-  taskTitle: {
-    marginLeft: Style.adjust(16),
   },
 });
