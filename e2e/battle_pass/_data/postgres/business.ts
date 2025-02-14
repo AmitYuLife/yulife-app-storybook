b6279a5b97c0c93e71e122de4e155bda0f23e7d9
Michael Peterseil <michael.peterseil@yulife.com>
2025-02-14 13:15:06 +0000

    ISA-3212 ESG Battlepass Visible for Concurrent Employments
    See merge request yu-life/yulife-rn-client!5752

--- e2e/battle_pass/_data/postgres/business.ts
+++ e2e/battle_pass/_data/postgres/business.ts
@@ -4,20 +4,31 @@ const type = "postgres";
 const modelName = "business";
 
 export const BUSINESS_THE_BEAR = {
-    type,
-    modelName,
-    data: {
-        business_account_id: generateRandomMongoId(),
-        business_account_name: "The Bear",
-        coupon: null,
-        status: null,
-        hubspot_id: "3395752702",
-        created_at: "2024-08-01T09:00:00Z",
-        created_by_id: "0.49.0",
-        modified_at: "2024-08-02T09:00:00Z",
-        modified_by_id: "0.49.0",
-        stripe_account_id: null,
-        email_template: null,
-        archived: false,
-    },
+  type,
+  modelName,
+  data: {
+    business_account_id: generateRandomMongoId(),
+    business_account_name: "The Bear",
+    coupon: null,
+    status: null,
+    hubspot_id: "3395752702",
+    created_at: "2024-08-01T09:00:00Z",
+    created_by_id: "0.49.0",
+    modified_at: "2024-08-02T09:00:00Z",
+    modified_by_id: "0.49.0",
+    stripe_account_id: null,
+    email_template: null,
+    archived: false,
+  },
+} as IDatabaseItem;
+
+export const BUSINESS_ACCOUNT_2 = {
+  type,
+  modelName,
+  data: {
+    business_account_id: generateRandomMongoId(),
+    business_account_name: "Vegan Mobile",
+    hubspot_id: "3395752702",
+    archived: false,
+  },
 } as IDatabaseItem;
--- e2e/battle_pass/battle_pass/_resources/constants.ts
+++ e2e/battle_pass/battle_pass/_resources/constants.ts
@@ -1,11 +1,17 @@
 import { IMPACT_DONATION } from "./types";
 
-export const emptySeasonalRewardVisible = "It's quiet in here. Head over to your seasonal rewards path to earn exclusive vouchers and discounts!";
-export const emptyInventoryState = "Your inventory is empty. Head over to your seasonal rewards path to earn helpful items to aid you on your journey.";
+export const emptySeasonalRewardVisible =
+  "It's quiet in here. Head over to your seasonal rewards path to earn exclusive vouchers and discounts!";
+export const emptyInventoryState =
+  "Your inventory is empty. Head over to your seasonal rewards path to earn helpful items to aid you on your journey.";
 
 export const impactDonationImages: Record<IMPACT_DONATION, string> = {
-    tree: "https://yulife-develop.imgix.net/game/donations/tree_2024_08_05.svg?ixlib=js-3.2.1&w=88&h=88&fit=clip&fm=png&dpr=3&s=7206050e6b81edf91895a64b9313019c",
-    water: "https://yulife-develop.imgix.net/game/donations/water_2024_08_05.svg?ixlib=js-3.2.1&w=88&h=88&fit=clip&fm=png&dpr=3&s=2ae92fd7e197dd71cadf265da1d980fe",
-    meal: "https://yulife-develop.imgix.net/game/donations/meal_2024_08_05.svg?ixlib=js-3.2.1&w=88&h=88&fit=clip&fm=png&dpr=3&s=4e3d9f9cdfb662faa21bf6e71f2321eb",
-    ocean: "https://yulife-develop.imgix.net/game/donations/ocean_2024_08_05.svg?ixlib=js-3.2.1&w=88&h=88&fit=clip&fm=png&dpr=3&s=a5d486b2ba195b396d03eb2905d55fa1",
+  tree: "https://yulife-develop.imgix.net/game/donations/tree_2024_08_05.svg?ixlib=js-3.2.1&w=88&h=88&fit=clip&fm=png&dpr=3&s=7206050e6b81edf91895a64b9313019c",
+  water:
+    "https://yulife-develop.imgix.net/game/donations/water_2024_08_05.svg?ixlib=js-3.2.1&w=88&h=88&fit=clip&fm=png&dpr=3&s=2ae92fd7e197dd71cadf265da1d980fe",
+  meal: "https://yulife-develop.imgix.net/game/donations/meal_2024_08_05.svg?ixlib=js-3.2.1&w=88&h=88&fit=clip&fm=png&dpr=3&s=4e3d9f9cdfb662faa21bf6e71f2321eb",
+  ocean:
+    "https://yulife-develop.imgix.net/game/donations/ocean_2024_08_05.svg?ixlib=js-3.2.1&w=88&h=88&fit=clip&fm=png&dpr=3&s=a5d486b2ba195b396d03eb2905d55fa1",
 };
+
+export const locationModalTitle = "Choose your location!";
