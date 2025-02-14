aec10132b9f0b6e88c59eb9e3ef6cea2d09a28d0
Manolis Vrondakis <manolis.vrondakis@yulife.com>
2025-02-14 10:04:24 +0000

    Task: Fix entitlements randomly missing in some builds
    See merge request yu-life/yulife-rn-client!5757

--- plugins/yuwatch/with-yuwatch-framework.plugin.js
+++ plugins/yuwatch/with-yuwatch-framework.plugin.js
@@ -1,29 +1,20 @@
-const { withXcodeProject } = require("@expo/config-plugins");
+const plugins = require("@expo/config-plugins");
 const fs = require("fs");
 const xcode = require("xcode");
 
-module.exports = (config) => {
-  return withXcodeProject(config, async (newConfig) => {
-    try {
-      const projectName = newConfig.modRequest.projectName;
-      const bundleId = config.ios?.bundleIdentifier || "";
-      const widgetBundleId = `${bundleId}.widget`;
+module.exports = (data) => {
+  return plugins.withDangerousMod(data, [
+    "ios",
+    async (config) => {
+      const projectName = config.modRequest.projectName;
+      const projPath = `${config.modRequest.platformProjectRoot}/${projectName}.xcodeproj/project.pbxproj`;
 
-      const projPath = `${newConfig.modRequest.platformProjectRoot}/${projectName}.xcodeproj/project.pbxproj`;
-      await updateXCodeProj(projPath, widgetBundleId);
-      return newConfig;
-    } catch (e) {
-      console.error(e);
-      throw e;
-    }
-  });
+      const xcodeProject = xcode.project(projPath);
+      xcodeProject.parseSync();
+      xcodeProject.addFramework("WatchConnectivity.framework");
+      fs.writeFileSync(projPath, xcodeProject.writeSync());
+
+      return config;
+    },
+  ]);
 };
-
-async function updateXCodeProj(projPath) {
-  const xcodeProject = xcode.project(projPath);
-  xcodeProject.parse(() => {
-    xcodeProject.addFramework("WatchConnectivity.framework");
-
-    fs.writeFileSync(projPath, xcodeProject.writeSync());
-  });
-}
