b2c33e5b54aefd0674836bf83c5717c0e8473731
Manolis Vrondakis <manolis.vrondakis@yulife.com>
2025-02-19 16:51:29 +0000

    Task: Increase patch line limit
    See merge request yu-life/yulife-rn-client!5787

--- .githooks/pre-commit/02_large_patches.sh
+++ .githooks/pre-commit/02_large_patches.sh
@@ -6,7 +6,7 @@ NoColor='\033[0m'
 git diff --staged --name-only | while read -r file_path; do
     if [[ "$file_path" =~ ^patches/* ]]; then
         line_count=$(grep '^+' $file_path | wc -l | awk '{print $1}')
-        if [[ $line_count -gt 50 ]]; then
+        if [[ $line_count -gt 300 ]]; then
             echo -e "The file $Red$file_path$NoColor is too large."
             echo -e "Are you sure you want to commit this file? If so, try with$Yellow git commit --no-verify$NoColor"
             exit 1
