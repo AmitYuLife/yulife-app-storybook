314c39f88d36fe66800febde696a44054cf84e0f
Bret Cameron <bret@yulife.com>
2025-02-21 14:38:43 +0000

    Bugfix LCS-1112: Fix Gift Recipient Validation Failure During Send
    See merge request yu-life/yulife-rn-client!5802

--- e2e/activity/_common/given.ts
+++ e2e/activity/_common/given.ts
@@ -1,22 +1,19 @@
-import { navigation } from "@navigation"
-import { dataManager } from "@yu-life/yulife-bdd-framework"
+import { navigation } from "@navigation";
+import { dataManager } from "@yu-life/yulife-bdd-framework";
 
 export const {
-    logInAndGoToTab,
-    loginAsUser,
-    loginOnly,
-    loginAndCollectSignupBonus,
-    loginToYuScreen,
-    loginAsPLIUser,
-} = navigation.login
+  logInAndGoToTab,
+  loginAsUser,
+  loginOnly,
+  loginAndCollectSignupBonus,
+  loginToYuScreen,
+  loginAsPLIUser,
+} = navigation.login;
 
-export const {
-    navigateViaID,
-    navigateViaText,
-} = navigation.common
+export const { navigateViaID, navigateViaText } = navigation.common;
 
 export const giveEngagementSurveyAccess = (tags: string[]) => async () => {
-    await dataManager.triggerWorkerTask("GIVE_ENGAGEMENT_SURVEY_ACCESS", {
-        tags,
-    });
+  await dataManager.triggerWorkerTask("GIVE_ENGAGEMENT_SURVEY_ACCESS", {
+    tags,
+  });
 };
