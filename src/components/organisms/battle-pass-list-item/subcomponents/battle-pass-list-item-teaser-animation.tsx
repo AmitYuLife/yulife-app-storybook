db84175bcc000810603de6766150f82b042bd5ce
Manolis Vrondakis <manolis.vrondakis@yulife.com>
2025-02-25 15:17:54 +0000

    Task: Fix teaser animation
    See merge request yu-life/yulife-rn-client!5822

--- src/components/organisms/battle-pass-list-item/subcomponents/battle-pass-list-item-teaser-animation.tsx
+++ src/components/organisms/battle-pass-list-item/subcomponents/battle-pass-list-item-teaser-animation.tsx
@@ -2,8 +2,6 @@ import { Box } from "@atoms";
 import { ReactNode, memo } from "react";
 import { Easing, useAnimatedStyle, withSequence, withTiming } from "react-native-reanimated";
 
-const EXIT_DURATION = 500;
-const IMAGE_WAIT = 1000;
 const ENTER_DURATION = 300;
 
 interface IAnimatedTeaserImageProps {
@@ -17,9 +15,7 @@ const BattlePassListItemTeaserAnimation = ({ children }: IAnimatedTeaserImagePro
         {
           scale: withSequence(
             withTiming(0, { duration: 0 }),
-            withTiming(1, { duration: ENTER_DURATION, easing: Easing.elastic(1.1) }),
-            withTiming(1, { duration: IMAGE_WAIT }),
-            withTiming(0, { duration: EXIT_DURATION })
+            withTiming(1, { duration: ENTER_DURATION, easing: Easing.elastic(1.1) })
           ),
         },
       ],
