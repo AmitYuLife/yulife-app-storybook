1bef8bc84025825cbe709a74fe7e777ace40366a
Jeff Filho <jeff@yulife.com>
2025-02-26 10:21:08 +0000

    Bugfix GS-1317: Only sendTokenToIntercom if the user has granted push notification permission
    See merge request yu-life/yulife-rn-client!5826

--- src/redux/user/sagas/setLoggerIdentity.helper.ts
+++ src/redux/user/sagas/setLoggerIdentity.helper.ts
@@ -1,14 +1,11 @@
 import { getCurrentLocaleOptions } from "@locale";
-import { getDeviceToken } from "@redux/device/device.selectors";
 import Logger from "@services/logging/logger";
 import { UserSupportLevel } from "@services/logging/types";
-import { call, delay, select } from "redux-saga/effects";
+import { call, delay } from "redux-saga/effects";
 
 export default function* setLoggerIdentity(userId: string, intercomHash: string, supportLevel: UserSupportLevel) {
-  const deviceToken: ReturnType<typeof getDeviceToken> = yield select(getDeviceToken);
-
   yield call(Logger.init);
-  yield call(Logger.setUserId, userId, intercomHash, supportLevel, deviceToken);
+  yield call(Logger.setUserId, userId, intercomHash, supportLevel);
 
   const localeOptions = getCurrentLocaleOptions();
 
