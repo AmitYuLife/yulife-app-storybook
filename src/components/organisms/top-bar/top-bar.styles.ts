84b43a48c6430f4850fa6cc8ded646373b81134b
Cris Naranjo <cris@yulife.com>
2025-03-04 12:00:36 +0000

    Bugfix: Top bar height + hitslop
    See merge request yu-life/yulife-rn-client!5862

--- src/components/organisms/top-bar/top-bar.styles.ts
+++ src/components/organisms/top-bar/top-bar.styles.ts
@@ -1,8 +1,11 @@
-import { StyleSheet, TextStyle, ViewStyle } from "react-native";
+import { Platform, StyleSheet, TextStyle, ViewStyle } from "react-native";
 import { Style } from "../../../styles";
 import { TOP_BAR } from "@styles";
 
-export const TOP_BAR_HEIGHT = TOP_BAR.HEIGHT;
+export const TOP_BAR_HEIGHT = Platform.select({
+  ios: TOP_BAR.HEIGHT + 16, // Height + 8px padding top + 8px padding bottom
+  android: TOP_BAR.HEIGHT + TOP_BAR.PADDING_TOP + TOP_BAR.PADDING_BOTTOM,
+});
 
 export default StyleSheet.create({
   wrapper: {
--- src/styles/top-bar.styles.ts
+++ src/styles/top-bar.styles.ts
@@ -24,7 +24,7 @@ const HIT_SLOP_SIZE = Style.adjust(16);
 const HIT_SLOP = {
   left: HIT_SLOP_SIZE,
   right: HIT_SLOP_SIZE,
-  bottom: HIT_SLOP_SIZE,
+  bottom: Platform.select({ ios: HIT_SLOP_SIZE, android: 0 }),
   top: HIT_SLOP_SIZE,
 };
 
