e835ef9fc2149b2c1f6a9fc2000091fddb3a39f5
Bret Cameron <bret@yulife.com>
2025-03-05 17:11:09 +0000

    Bugfix: Remove margin around progress bar to fix Android obscured text bug
    See merge request yu-life/yulife-rn-client!5880

--- src/components/molecules/hero-card/subcomponents/hero-card-body.tsx
+++ src/components/molecules/hero-card/subcomponents/hero-card-body.tsx
@@ -1,6 +1,4 @@
-import { StyleSheet, View } from "react-native";
 import { memo } from "react";
-import { Style } from "@styles";
 import { ProgressBar } from "@atoms";
 import { HeroCardBody as HeroCardBodyProps, HeroCardProgressMilestoneState } from "@utils/heroCards";
 
@@ -10,26 +8,18 @@ const HeroCardBody = ({ progress, cardWidth, cardPadding }: HeroCardBodyProps) =
   }
 
   return (
-    <View style={styles.body}>
-      <ProgressBar
-        type="compact"
-        width={cardWidth - cardPadding * 2}
-        current={progress.currentProgress}
-        max={progress.maxProgress}
-        milestones={progress.milestones.map((milestone) => ({
-          value: milestone.progress,
-          rewardClaimed: milestone.state === HeroCardProgressMilestoneState.Active,
-          shouldAttractAttention: milestone.state === HeroCardProgressMilestoneState.Emphasized,
-        }))}
-      />
-    </View>
+    <ProgressBar
+      type="compact"
+      width={cardWidth - cardPadding * 2}
+      current={progress.currentProgress}
+      max={progress.maxProgress}
+      milestones={progress.milestones.map((milestone) => ({
+        value: milestone.progress,
+        rewardClaimed: milestone.state === HeroCardProgressMilestoneState.Active,
+        shouldAttractAttention: milestone.state === HeroCardProgressMilestoneState.Emphasized,
+      }))}
+    />
   );
 };
-
-const styles = StyleSheet.create({
-  body: {
-    marginVertical: Style.adjust(8),
-  },
-});
 
 export default memo(HeroCardBody);
