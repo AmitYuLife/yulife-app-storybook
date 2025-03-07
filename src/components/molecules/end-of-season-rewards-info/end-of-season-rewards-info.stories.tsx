6c9890e73851ae9cda54f466f5eabead02d51250
Jeff Filho <jeff@yulife.com>
2025-03-07 13:45:05 +0000

    Task GS-1339: Update EndOfSeasonRewardsInfo component
    See merge request yu-life/yulife-rn-client!5894

--- src/components/molecules/end-of-season-rewards-info/end-of-season-rewards-info.stories.tsx
+++ src/components/molecules/end-of-season-rewards-info/end-of-season-rewards-info.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import EndOfSeasonRewardsInfo from "./end-of-season-rewards-info";
-
 const meta: Meta<typeof EndOfSeasonRewardsInfo> = {
   component: EndOfSeasonRewardsInfo,
   title: "Design System/Molecules/EndOfSeasonRewardsInfo",
@@ -13,32 +12,38 @@ const meta: Meta<typeof EndOfSeasonRewardsInfo> = {
   },
   args: {},
 };
-
 export default meta;
 type Story = StoryObj<typeof EndOfSeasonRewardsInfo>;
-
 export const Default: Story = {
   args: {
     items: [
       {
-        icon: "https://yulife-develop.imgix.net/storybook/Trees.png?ixlib=js-3.2.1&s=35708cecab9e9d702d18b21887371f55",
+        icon: {
+          uri: "https://yulife-develop.imgix.net/storybook/Trees.png?ixlib=js-3.2.1&s=35708cecab9e9d702d18b21887371f55",
+        },
         title: "Trees planted",
-        value: "1,000",
+        score: "1,000",
       },
       {
-        icon: "https://yulife-develop.imgix.net/storybook/Mael.png?ixlib=js-3.2.1&s=8df6c8a3bc2dde1dbb7db2c3ea64b926",
+        icon: {
+          uri: "https://yulife-develop.imgix.net/storybook/Mael.png?ixlib=js-3.2.1&s=8df6c8a3bc2dde1dbb7db2c3ea64b926",
+        },
         title: "Meals donated",
-        value: "300",
+        score: "300",
       },
       {
-        icon: "https://yulife-develop.imgix.net/storybook/Water.png?ixlib=js-3.2.1&s=9e2637ec4d187b0b79f2c1d4d403a1bd",
+        icon: {
+          uri: "https://yulife-develop.imgix.net/storybook/Water.png?ixlib=js-3.2.1&s=9e2637ec4d187b0b79f2c1d4d403a1bd",
+        },
         title: "Water donated",
-        value: "300l",
+        score: "300l",
       },
       {
-        icon: "https://yulife-develop.imgix.net/storybook/Ocean.png?ixlib=js-3.2.1&s=95f2952dfe85daab28c0035e034246cd",
+        icon: {
+          uri: "https://yulife-develop.imgix.net/storybook/Ocean.png?ixlib=js-3.2.1&s=95f2952dfe85daab28c0035e034246cd",
+        },
         title: "Plastic removed",
-        value: "115kg",
+        score: "115kg",
       },
     ],
   },
