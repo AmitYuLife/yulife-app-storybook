547ceb85eb731a4b8b8f7250bcd154fe3afac69c
Jeff Filho <jeff@yulife.com>
2025-03-14 15:34:54 +0000

    Bugfix GS-1368: Screen size issues on battle pass on android 15
    See merge request yu-life/yulife-rn-client!5949

--- src/hooks/useSafeAreaViewOffset.ts
+++ src/hooks/useSafeAreaViewOffset.ts
@@ -2,6 +2,7 @@ import { safeAreaViewOffset } from "@styles/safeAreaViewOffset";
 import { useCallback } from "react";
 
 export const useSafeAreaViewOffset = () => {
+  // @TODO: change this to use useSafeAreaInsets();
   const setSafeAreaViewOffset = useCallback((newSafeAreaViewOffset: { y: number }) => {
     safeAreaViewOffset.y = newSafeAreaViewOffset.y;
   }, []);
