f003a0acbefd2a81081b83a254005f303fd51733
Julian Memai <julian.memai@yulife.com>
2025-04-15 10:19:19 +0000

    Detox ENG-4160: Validate Donation Flow After Challenge Completion
    Closes ENG-4160
    
    See merge request yu-life/yulife-rn-client!6068

--- e2e/battle_pass/_common/given.ts
+++ e2e/battle_pass/_common/given.ts
@@ -1,21 +1,20 @@
 import { navigation } from "@navigation";
 import { dataManager } from "@yu-life/yulife-bdd-framework";
-import { BATTLE_PASS_DONATIONS } from "../_data";
 
 export const { logInAndGoToTab, loginAsUser, loginOnly } = navigation.login;
 
-export const triggerGenerateBattlePassSeason = async () => {
-    await dataManager.triggerWorkerTask("GENERATE_DONATIONS_BATTLE_PASS_SEASON", {
-        battlePassId: BATTLE_PASS_DONATIONS.data._id,
-        seasonNumber: 1,
-        execute: 1,
-        purgeExisting: false,
-        targetMultiplier: 0.375,
-        limitMilestones: 3,
-        seasonName: "Season of Harmony",
-    });
+export const triggerGenerateBattlePassSeason = (businessIds: string[]) => async () => {
+  await dataManager.triggerWorkerTask("GENERATE_DONATIONS_BATTLE_PASS_SEASON", {
+    entityIds: businessIds,
+    seasonNumber: 1,
+    execute: 1,
+    purgeExisting: false,
+    targetMultiplier: 0.375,
+    limitMilestones: 3,
+    seasonName: "Season of Harmony",
+  });
 };
 
 export const triggerCreateRandomChestPool = async () => {
-    await dataManager.triggerWorkerTask("SCHEDULE_RANDOM_CHEST_POOL_REWARD_LINKING", {});
+  await dataManager.triggerWorkerTask("SCHEDULE_RANDOM_CHEST_POOL_REWARD_LINKING", {});
 };
--- e2e/battle_pass/battle_pass/_resources/constants.ts
+++ e2e/battle_pass/battle_pass/_resources/constants.ts
@@ -4,6 +4,8 @@ export const emptySeasonalRewardVisible =
   "It's quiet in here. Head over to your seasonal rewards path to earn exclusive vouchers and discounts!";
 export const emptyInventoryState =
   "Your inventory is empty. Head over to your seasonal rewards path to earn helpful items to aid you on your journey.";
+export const outOfCoinsMessage =
+  "You have insufficient balance to make this purchase. \n \n Try finishing some challenges to earn more coins.";
 
 export const impactDonationImages: Record<IMPACT_DONATION, string> = {
   tree: "https://yulife-develop.imgix.net/game/donations/tree_2024_08_05.svg?ixlib=js-3.2.1&w=88&h=88&fit=clip&fm=png&dpr=3&s=7206050e6b81edf91895a64b9313019c",
