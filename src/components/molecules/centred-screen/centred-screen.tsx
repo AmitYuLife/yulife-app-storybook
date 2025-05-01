9f37f701b59c1639790e1dbde5df40a734dd05d1
Giles Williams <giles.williams@yulife.com>
2025-05-01 15:05:44 +0100

    Fix LCS-1143: login confirm error key + background bottom alignment
    Closes LCS-1143
    
    See merge request yu-life/yulife-rn-client!6141

--- src/components/molecules/centred-screen/centred-screen.tsx
+++ src/components/molecules/centred-screen/centred-screen.tsx
@@ -39,7 +39,6 @@ function CentredScreen({
           height={Style.DEVICE_HEIGHT}
           left={0}
           right={0}
-          bottom={0}
           justifyContent="flex-end"
         >
           {backgroundImage}
