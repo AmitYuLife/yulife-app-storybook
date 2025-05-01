801d109a919f4a30b1029802272eee5f00f251d6
Jeff Filho <jeff@yulife.com>
2025-05-01 13:36:04 +0000

    Bugfix GS-1507: Name and level displaying wrong when inspecting user on leaderboard
    See merge request yu-life/yulife-rn-client!6140

--- src/components/organisms/name-level-mini-avatar/name-level-mini-avatar.tsx
+++ src/components/organisms/name-level-mini-avatar/name-level-mini-avatar.tsx
@@ -2,7 +2,7 @@ import React, { FC, memo, useEffect, useRef } from "react";
 import { Animated, Easing, Image, StyleSheet, View } from "react-native";
 import { Style } from "@styles";
 import { TextTemplate } from "@atoms";
-import { getCurrentWorld, getCurrentWorldText } from "@utils";
+import { getCurrentWorld, getCurrentWorldImage, getCurrentWorldText } from "@utils";
 import { useSelector } from "react-redux";
 import { getUserAvatar, getUserName } from "@redux/user/user.selectors";
 import { getCurrentLevel, getYuniversalProgress } from "@redux/levels/levels.selectors";
@@ -12,68 +12,97 @@ import { Avatar } from "@components/molecules";
 import { DETOX_ENABLED } from "@services/socket";
 
 interface IProps {
+  name?: string;
+  level?: number;
+  yuniversalMap?: number;
   showYumoji?: boolean;
   animateYumoji?: boolean;
   textColour?: string;
   centerContent?: boolean;
+  hideDisplayedLevel?: boolean;
 }
 
 const ANIMATION_DURATION = DETOX_ENABLED ? 0 : 200;
 const HIDDEN_YUMOJI_POSITION = Style.adjust(-76);
 
-const NameLevelMiniAvatar: FC<IProps> = memo(({ showYumoji, animateYumoji, textColour, centerContent }) => {
-  const userName = useSelector(getUserName);
-  const currentLevel = useSelector(getCurrentLevel);
-  const { yuniversalMap, yuniversalLevel } = useSelector(getYuniversalProgress);
-  const avatar = useSelector(getUserAvatar);
+const NameLevelMiniAvatar: FC<IProps> = memo(
+  ({ name, level, yuniversalMap, showYumoji, animateYumoji, textColour, centerContent, hideDisplayedLevel }) => {
+    const storedUserName = useSelector(getUserName);
+    const storedCurrentLevel = useSelector(getCurrentLevel);
+    const { yuniversalMap: storedYuniversalMap, yuniversalLevel } = useSelector(getYuniversalProgress);
 
-  const isYuniversal = !!yuniversalMap;
-  const currentWorld = getCurrentWorld(currentLevel);
-  const worldIcon = getCurrentWorldIcon(currentWorld, isYuniversal);
-  const worldName = getCurrentWorldText(currentWorld, isYuniversal);
-  const displayedLevel = isYuniversal ? ROMAN_NUMERALS[yuniversalLevel] : currentLevel;
+    const userName = name || storedUserName;
+    const currentLevel = level || storedCurrentLevel;
+    const isYuniversal = yuniversalMap === undefined ? !!storedYuniversalMap : !!yuniversalMap;
 
-  const translateX = useRef(new Animated.Value(HIDDEN_YUMOJI_POSITION)).current;
+    const avatar = useSelector(getUserAvatar);
 
-  useEffect(() => {
-    if (DETOX_ENABLED) {
-      return;
-    }
+    const currentWorld = getCurrentWorld(currentLevel);
+    const worldIcon = name
+      ? getCurrentWorldImage(currentWorld, isYuniversal)
+      : getCurrentWorldIcon(currentWorld, isYuniversal);
+    const worldName = getCurrentWorldText(currentWorld, isYuniversal);
+    const displayedLevel = getDisplayedLevel(currentLevel, yuniversalLevel, isYuniversal, hideDisplayedLevel);
 
-    const animation = Animated.timing(translateX, {
-      duration: ANIMATION_DURATION,
-      toValue: animateYumoji ? 0 : HIDDEN_YUMOJI_POSITION,
-      useNativeDriver: true,
-      easing: Easing.inOut(Easing.ease),
-    });
+    const translateX = useRef(new Animated.Value(HIDDEN_YUMOJI_POSITION)).current;
 
-    animation.start();
-    return () => animation.stop();
-  }, [animateYumoji]);
+    useEffect(() => {
+      if (DETOX_ENABLED) {
+        return;
+      }
 
-  return (
-    <Animated.View style={{ transform: [{ translateX }] }}>
-      {!showYumoji ? null : (
-        <View style={styles.avatarWrapper}>
-          <Avatar uri={avatar?.avatarRemoteFiles?.pngMini} showEmpty={true} size={52} />
+      const animation = Animated.timing(translateX, {
+        duration: ANIMATION_DURATION,
+        toValue: animateYumoji ? 0 : HIDDEN_YUMOJI_POSITION,
+        useNativeDriver: true,
+        easing: Easing.inOut(Easing.ease),
+      });
+
+      animation.start();
+      return () => animation.stop();
+    }, [animateYumoji]);
+
+    return (
+      <Animated.View style={{ transform: [{ translateX }] }}>
+        {!showYumoji ? null : (
+          <View style={styles.avatarWrapper}>
+            <Avatar uri={avatar?.avatarRemoteFiles?.pngMini} showEmpty={true} size={52} />
+          </View>
+        )}
+        <View style={[styles.wrapper, centerContent ? { alignItems: "center" } : {}]}>
+          <TextTemplate type="b1b" numberOfLines={1} color={textColour} testID={YUSCREEN_V5_USERNAME(userName)}>
+            {userName}
+          </TextTemplate>
+          <View style={styles.worldContainer}>
+            <Image style={styles.image} source={worldIcon} />
+            <TextTemplate
+              type="l1"
+              color={textColour}
+              testID={YUSCREEN_V5_WORLD_AND_LEVEL(worldName, displayedLevel)}
+            >{`${worldName} ${displayedLevel}`}</TextTemplate>
+          </View>
         </View>
-      )}
-      <View style={[styles.wrapper, centerContent ? { alignItems: "center" } : {}]}>
-        <TextTemplate type="b1b" numberOfLines={1} color={textColour} testID={YUSCREEN_V5_USERNAME(userName)}>
-          {userName}
-        </TextTemplate>
-        <View style={styles.worldContainer}>
-          <Image style={styles.image} source={worldIcon} />
-          <TextTemplate
-            type="l1"
-            color={textColour}
-            testID={YUSCREEN_V5_WORLD_AND_LEVEL(worldName, displayedLevel)}
-          >{`${worldName} ${displayedLevel}`}</TextTemplate>
-        </View>
-      </View>
-    </Animated.View>
-  );
-});
+      </Animated.View>
+    );
+  }
+);
+
+const getDisplayedLevel = (
+  currentLevel: number,
+  yuniversalLevel: number,
+  isYuniversal: boolean,
+  hideDisplayedLevel: boolean
+) => {
+  if (hideDisplayedLevel) {
+    return "";
+  }
+
+  if (isYuniversal && yuniversalLevel) {
+    return ROMAN_NUMERALS[yuniversalLevel];
+  }
+
+  return currentLevel;
+};
 
 export default NameLevelMiniAvatar;
 
