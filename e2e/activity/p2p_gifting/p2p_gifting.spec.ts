74889886fadb8592c1cf019ef16c3f90ab52129c
Ulas Temel <ulas.temel@yulife.com>
2025-05-16 08:42:00 +0000

    Detox: updated stickers for p2p gifting test
    See merge request yu-life/yulife-rn-client!6212

--- e2e/activity/p2p_gifting/p2p_gifting.spec.ts
+++ e2e/activity/p2p_gifting/p2p_gifting.spec.ts
@@ -109,7 +109,7 @@ Feature("P2P gifting", async () => {
     });
   });
 
-  Scenario("I should be restricted from sending a gift to the same user after reaching the gifting limit, and I can see 18 selectable messages and 24 selectable stickers", scenario.start, async () => {
+  Scenario("I should be restricted from sending a gift to the same user after reaching the gifting limit, and I can see 18 selectable messages and 33 selectable stickers", scenario.start, async () => {
     Given("I login", given.loginAsUser(data.CUSTOMER_18, data.AUTH_18), async () => {
       When("I trigger the search token worker", when.triggerSearchTokens(55), async () => {
         When("I go to my YuScreen", when.tapID(ids.NAV_BAR("yu")), async () => {
@@ -145,7 +145,7 @@ Feature("P2P gifting", async () => {
     });
     When("I tap to add a sticker", when.tapID(ids.P2P_STICKER, 2000), async () => {
       Then("I should see the stickers modal appear", then.idVisible(ids.P2P_STICKER_MODAL));
-      Then("I can see and select through all 24 stickers", then.cycleThroughStickers);
+      Then("I can see and select through all 33 stickers", then.cycleThroughStickers);
     });
   });
 
