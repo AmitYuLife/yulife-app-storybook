491983c2fb5a414f077fca6d8a2d20d77a576e14
Ulas Temel <ulas.temel@yulife.com>
2025-06-30 08:02:17 +0000

    Detox NVS-715: Dai-ichi product cards showing correct images
    See merge request yu-life/yulife-rn-client!6414

--- e2e/japan/_resources/smoking_fixtures.ts
+++ e2e/japan/_resources/smoking_fixtures.ts
@@ -1,12 +1,21 @@
 export const CUSTOMER_2_SMOKING_TIPS = [
-    "health-benefit-tip-day-1",
-    "drinking-alcohol-trigger-tip-1",
-    "default-trigger-tip-1",
-    "default-trigger-tip-2"
-  ]
-  
-  export const CUSTOMER_2_MOMENTS_AND_REASONS = [
-    "お酒を飲んでいるとき",
-    "家族のため",
-    "お金を節約するため"
-  ]
+  "health-benefit-tip-day-1",
+  "drinking-alcohol-trigger-tip-1",
+  "default-trigger-tip-1",
+  "default-trigger-tip-2",
+];
+
+export const CUSTOMER_2_MOMENTS_AND_REASONS = [
+  "お酒を飲んでいるとき",
+  "家族のため",
+  "お金を節約するため",
+];
+
+export const PRODUCT_CARD_ILLUSTRATIONS = {
+  COMPASS:
+    "https://yulife-develop.imgix.net/yuscreen/product-card-carousel/illustrations/group-medical-insurance-square-2024-10-22.png?ixlib=js-3.2.1&fit=clip&fm=png&dpr=1&s=52c437a7fa0402c0dacc6c85316ad091",
+  CANDLE:
+    "https://yulife-develop.imgix.net/yuscreen/product-card-carousel/illustrations/3-diseases-square-2025-06-20.png?ixlib=js-3.2.1&fit=clip&fm=png&dpr=1&s=9b60624b384a24a8ae7bf668e4af33b1",
+  LANTERN:
+    "https://yulife-develop.imgix.net/yuscreen/product-card-carousel/illustrations/death-benefit-insurance-square-2025-06-20.png?ixlib=js-3.2.1&fit=clip&fm=png&dpr=1&s=0d05ff18498ec14ea6cfb45ff0473f89",
+};
