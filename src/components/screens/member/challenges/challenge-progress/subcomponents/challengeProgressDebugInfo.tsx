67c331b8507a8fa5487bd71b2b36dc09804a6285
Boris <boris@yulife.com>
2025-07-31 14:31:58 +0000

    Task GS-1796: Get initial steps before subscribe
    See merge request yu-life/yulife-rn-client!6533

--- src/components/screens/member/challenges/challenge-progress/subcomponents/challengeProgressDebugInfo.tsx
+++ src/components/screens/member/challenges/challenge-progress/subcomponents/challengeProgressDebugInfo.tsx
@@ -6,20 +6,21 @@ import { FlashList } from "@shopify/flash-list";
 import { useDispatch } from "react-redux";
 import { logMixpanelEventActionCreator } from "@redux/logging/logging.actions";
 import { t } from "@locale";
-import { Alert } from "react-native";
+import { Alert, Platform } from "react-native";
+import { IPedometerHistoryEntry } from "@redux/debug/debug.types";
 
 type Props = {
   initialDailySteps: number;
   currentPedometerSteps: number;
   progressCalculation: number;
   healthProvider: string;
-  historySteps: number[];
+  historySteps: IPedometerHistoryEntry[];
 };
 
-const keyExtractor = (item: number, index: number) => `${index}-${item.toString()}`;
+const keyExtractor = (item: IPedometerHistoryEntry, index: number) => `${index}-${item.steps.toString()}`;
 
-const renderItem = ({ item }: { item: number }) => (
-  <TextTemplate type="l1b">{t("smart_count.steps", { smart_count: item })}</TextTemplate>
+const renderItem = ({ item }: { item: IPedometerHistoryEntry }) => (
+  <TextTemplate type="l1b">{t("smart_count.steps", { smart_count: item.steps })}</TextTemplate>
 );
 
 const ChallengeProgressDebugInfo = ({
@@ -29,17 +30,27 @@ const ChallengeProgressDebugInfo = ({
   healthProvider,
   historySteps,
 }: Props) => {
-  const flashListRef = useRef<FlashList<number>>();
+  const flashListRef = useRef<FlashList<IPedometerHistoryEntry>>();
   const dispatch = useDispatch();
 
   const onExportDataPress = useCallback(() => {
+    const reducedHistorySteps = (historySteps || []).reverse().reduce(
+      (acc, item) => {
+        acc.historySteps.push(item.steps);
+        acc.stepsBeforeSubscribe.push(item.stepsBeforeSubscribe);
+        return acc;
+      },
+      { historySteps: [], stepsBeforeSubscribe: [] }
+    );
+
     dispatch(
       logMixpanelEventActionCreator("challenge_progress_debug_tools", {
         initialDailySteps: initialDailySteps || 0,
         currentPedometerSteps: currentPedometerSteps || 0,
         progressCalculation: progressCalculation || 0,
         healthProvider,
-        historySteps,
+        historySteps: reducedHistorySteps.historySteps,
+        stepsBeforeSubscribe: Platform.select({ ios: [], android: reducedHistorySteps.stepsBeforeSubscribe }),
       })
     );
 
--- src/components/screens/member/challenges/challenge-progress/subcomponents/challengeProgressDebugInfoWrapper.tsx
+++ src/components/screens/member/challenges/challenge-progress/subcomponents/challengeProgressDebugInfoWrapper.tsx
@@ -46,14 +46,15 @@ const ChallengeProgressDebugInfoWrapper = () => {
 
   const { initialPedometerResult } = useSelector(getActiveLevel);
   const historySteps = useSelector(getDebugPedometerHistorySteps);
+  const firstHistoryStep = first(historySteps)?.steps || 0;
 
   return (
     <ChallengeProgressDebugInfo
       historySteps={historySteps}
       healthProvider={healthProvider}
       initialDailySteps={initialPedometerResult}
-      currentPedometerSteps={first(historySteps)}
-      progressCalculation={first(historySteps) - initialPedometerResult}
+      currentPedometerSteps={firstHistoryStep}
+      progressCalculation={firstHistoryStep - initialPedometerResult}
     />
   );
 };
