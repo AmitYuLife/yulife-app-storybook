d033753b82d661802acc93ccc1e39d64733014bf
Jeff Filho <jeff@yulife.com>
2025-08-05 17:35:33 +0000

    Task GS-1845: Update StarEmptySlotIcon with the new design
    See merge request yu-life/yulife-rn-client!6580

--- src/components/atoms/icon/star-empty-slot-icon.tsx
+++ src/components/atoms/icon/star-empty-slot-icon.tsx
@@ -1,5 +1,5 @@
 import React, { memo } from "react";
-import Svg, { G, Path, Defs, ClipPath } from "react-native-svg";
+import Svg, { Path } from "react-native-svg";
 import { Style } from "@styles";
 
 interface IProps {
@@ -7,17 +7,10 @@ interface IProps {
 }
 
 export const StarEmptySlotIcon = memo(({ size = 17 }: IProps) => (
-  <Svg width={Style.adjust(size)} height={Style.adjust(size)} fill="none" viewBox="0 0 17 17">
-    <G clipPath="url(#a)">
-      <Path
-        fill="#E3E3E1"
-        d="M8 .175s1.138 3.594 2.772 5.228S16 8.175 16 8.175s-3.594 1.138-5.228 2.772S8 16.175 8 16.175s-1.138-3.594-2.772-5.228S0 8.175 0 8.175s3.594-1.138 5.228-2.772S8 .175 8 .175Z"
-      />
-    </G>
-    <Defs>
-      <ClipPath id="a">
-        <Path fill="#fff" d="M0 .175h16v16H0z" />
-      </ClipPath>
-    </Defs>
+  <Svg width={Style.adjust(size)} height={Style.adjust(size)} fill="none" viewBox="0 0 36 36">
+    <Path
+      fill="#E3E3E1"
+      d="M18 0s1.138 13.594 2.772 15.228C22.406 16.862 36 18 36 18s-13.594 1.138-15.228 2.772C19.138 22.406 18 36 18 36s-1.138-13.594-2.772-15.228C13.594 19.138 0 18 0 18s13.594-1.138 15.228-2.772C16.862 13.594 18 0 18 0Z"
+    />
   </Svg>
 ));
