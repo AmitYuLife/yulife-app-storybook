53d354f95468919b66d84f539801226f3b6f848b
Vitaly Trus <vitaly@yulife.com>
2025-08-29 15:28:39 +0000

    Bugfix: double badge render on weeklies
    See merge request yu-life/yulife-rn-client!6667

--- src/components/organisms/game-icon-button/weeklies.button.tsx
+++ src/components/organisms/game-icon-button/weeklies.button.tsx
@@ -2,7 +2,6 @@ import React, { memo } from "react";
 import { WeekliesIcon } from "@atoms/icon/weeklies-icon";
 import { GameButton } from "./_base.button";
 import { useRemainingTime } from "./helpers/useRemainingTime";
-import { Colours } from "@styles";
 import { WEEKLY_GOAL_ICON } from "@ids";
 import { t } from "@locale";
 
@@ -17,15 +16,11 @@ type Props = {
 const _Weeklies = ({ endDateTime, onPress, claimableRewards, hasJoined, isClaimed }: Props) => {
   const time = useRemainingTime(endDateTime);
   const hasBadge = (claimableRewards && claimableRewards > 0) || !hasJoined;
-  const badge = hasBadge ? getBadge(hasJoined, claimableRewards) : undefined;
   const label = isClaimed ? t("labels.cta.done") : time.shortFormat;
 
   return (
     <GameButton
       onPress={onPress}
-      badge={badge}
-      badgeColor={Colours.status.er300}
-      badgeStyle={badgeStyle}
       label={label}
       Icon={<WeekliesIcon hasBadge={hasBadge} active={!isClaimed} animated={hasBadge} />}
       accessibilityLabel={""}
@@ -34,14 +29,4 @@ const _Weeklies = ({ endDateTime, onPress, claimableRewards, hasJoined, isClaime
   );
 };
 
-const getBadge = (hasJoined: boolean, claimableRewards: number) => {
-  if (!hasJoined) {
-    return "1";
-  }
-
-  return String(claimableRewards);
-};
-
 export const Weeklies = memo(_Weeklies);
-
-const badgeStyle = { right: 1, top: 1 };
