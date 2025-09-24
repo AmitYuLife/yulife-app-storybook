ff75bd1584b4010ef15f387a0ed66667f19c700c
Julian Memai <julian.memai@yulife.com>
2025-09-24 12:37:18 +0000

    Detox: New Achievement Unlocked
    See merge request yu-life/yulife-rn-client!6796

--- e2e/battle_pass/battle_pass/battle_pass.spec.ts
+++ e2e/battle_pass/battle_pass/battle_pass.spec.ts
@@ -250,70 +250,70 @@ Feature("I can view and use all battle pass features", async () => {
       Then("I should be able to see the battle pass", then.idVisible(ids.BATTLE_PASS_SCREEN, 2000));
     });
   });
-});
 
-Scenario("I can view, adjust and validate coupon values before completing checkout", scenario.start, () => {
-  Given("I trigger the battle pass season worker", given.triggerGenerateBattlePassSeason([BUSINESS_THE_BEAR.data.business_account_id]), async () => {
-    Given("I trigger the random chest pool worker", given.triggerCreateRandomChestPool, async () => {
-      Given("I login and navigate to the rewards store", given.logInAndGoToTab("rewards", data.CUSTOMER_CARMY, data.AUTH_CARMY), async () => {
-        Then("I should be on the location modal", then.idVisible(ids.REWARDS_LOCATION_CONFIRM, 4000));
+  Scenario("I can view, adjust and validate coupon values before completing checkout", scenario.start, () => {
+    Given("I trigger the battle pass season worker", given.triggerGenerateBattlePassSeason([BUSINESS_THE_BEAR.data.business_account_id]), async () => {
+      Given("I trigger the random chest pool worker", given.triggerCreateRandomChestPool, async () => {
+        Given("I login and navigate to the rewards store", given.logInAndGoToTab("rewards", data.CUSTOMER_CARMY, data.AUTH_CARMY), async () => {
+          Then("I should be on the location modal", then.idVisible(ids.REWARDS_LOCATION_CONFIRM, 4000));
+        });
       });
     });
-  });
-  When("I confirm my store location", when.tapID(ids.REWARDS_LOCATION_CONFIRM, 4000), async () => {
-    Then("I should see my coin balance at the top right", then.idVisible(ids.VIEW_TOP_RIGHT_COIN_COUNTER(85200)));
-  });
-  When("I tap to open Wallet", when.tapID(ids.SHINE_BUTTON("Wallet"), 2500), async () => {
-    Then("I should see the 'M&S' coupon available for purchase", then.idVisible(ids.WALLET_CARD_TITLE("M&S"), 2000));
-  });
-  When("I tap the 'M&S' Coupon card", when.tapID(ids.WALLET_CARD_TITLE("M&S"), 2000), async () => {
-    Then("I should see the correct discount info", then.textVisible("10%", 1500));
-    Then("I should see the correct Coupon name", then.textVisible(data.CORE_REWARDS_MARKS_AND_SPENCER.data.name["en-GB"]));
-    Then("I should see the Coupon description", then.idVisible(ids.WALLET_COUPON_ITEM_DESCRIPTION("One time use"), 2000));
-  });
-  When("I tap the voucher card to adjust its values", when.tapID(ids.WALLET_COUPON_ITEM_DESCRIPTION("One time use"), 2000), async () => {
-    Then("I should see multiple available denominations", then.multipleIDVisible([ids.TEXT_TEMPLATE("£100", "b2b"), ids.TEXT_TEMPLATE("£5", "b2b")]));
-    Then("I should see the coupon input field", then.idVisible(ids.CONTENT_ITEM_INPUT("coupon-input"), 2000));
-    Then("I should see the currency info warning", then.idVisible(ids.TEXT_TEMPLATE("You can only redeem it in GBP (£).", "b2"), 2000));
-    Then("I should see the reward details info button", then.idVisible(ids.TERTIARY_BUTTON("How can my voucher be used"), 2000));
-  });
-  When("I tap the reward details info button", when.tapID(ids.TERTIARY_BUTTON("How can my voucher be used"), 2000), async () => {
-    Then("I should be on the reward details screen", then.textVisible(data.CORE_REWARDS_MARKS_AND_SPENCER.data.redemptionSteps.info["en-GB"]));
-  });
-  When("I tap to go back to the edit screen", when.tapID(ids.BACK_BUTTON, 2000), async () => {
-    When("I select the '£20' value pill", when.tapID(ids.TEXT_TEMPLATE("£20", "b2b"), 2000), async () => {
-      Then("I should see the minimum denomination error message", then.textVisible("Must be at least £50.", 2000));
-      Then("I should see that the checkout cta button is disabled", then.idVisible(ids.BUTTON_BASE("Checkout", true), 2000));
+    When("I confirm my store location", when.tapID(ids.REWARDS_LOCATION_CONFIRM, 4000), async () => {
+      Then("I should see my coin balance at the top right", then.idVisible(ids.VIEW_TOP_RIGHT_COIN_COUNTER(85200)));
     });
-  });
-  When("I select the '£100' value pill", when.tapID(ids.TEXT_TEMPLATE("£100", "b2b"), 2000), async () => {
-    Then("I should see the correct discount amount", then.idVisible(ids.TEXT_TEMPLATE("-£10.00", "b2b"), 2000));
-    Then("I should see the correct remaining payable amount", then.idVisible(ids.TEXT_TEMPLATE("£90.00", "b1b"), 2000));
-    Then("I should see that the 'Checkout' cta button is now enabled", then.idVisible(ids.BUTTON_BASE("Checkout", false), 2000));
-  });
-  When("I tap to checkout", when.tapID(ids.BUTTON_BASE("Checkout", false), 2000), async () => {
-    Then("I should see the terms and conditions checkbox", then.idVisible(ids.CHECK_BOX_STATE(couponsTermsAndConditions, false), 2000));
-    Then("I should see that the total payable amount is still '£90'", then.idVisible(ids.TEXT_TEMPLATE("£90", "b1b"), 2000));
-    Then("I should see the 'Accept terms and conditions' title", then.idVisible(ids.TEXT_TEMPLATE("Accept terms and conditions", "b1b"), 2000));
-    Then("I should see that the 'Continue to Payment' cta button is disabled", then.idVisible(ids.BUTTON_BASE("Continue to Payment", true), 2000));
-  });
-  When("I tap to edit the Coupon values", when.tapID(ids.TEXT_TEMPLATE("Edit", "b2b"), 2000), async () => {
-    Then("I should see the multiple available denominations", then.multipleIDVisible([ids.TEXT_TEMPLATE("£75", "b2b"), ids.TEXT_TEMPLATE("£2.50", "b2b")]));
-  });
-  When("I select the '£75' value pill", when.tapID(ids.TEXT_TEMPLATE("£75", "b2b"), 2000), async () => {
-    Then("I should see the updated discount amount", then.idVisible(ids.TEXT_TEMPLATE("-£7.50", "b2b"), 2000));
-    Then("I should see the correct remaining payable amount", then.idVisible(ids.TEXT_TEMPLATE("£67.50", "b1b"), 2000));
-  });
-  When("I tap to checkout", when.tapID(ids.BUTTON_BASE("Checkout", false), 2000), async () => {
-    When("I mark the terms and conditions checkbox", when.tapID(ids.CHECK_BOX_STATE(couponsTermsAndConditions, false), 2000), async () => {
-      Then("I should see that the 'Continue to Payment' cta button is now enabled", then.idVisible(ids.BUTTON_BASE("Continue to Payment", false), 2000));
-      Then("I should see that checkbox is now selected", then.idVisible(ids.CHECK_BOX_STATE(couponsTermsAndConditions, true), 2000));
+    When("I tap to open Wallet", when.tapID(ids.SHINE_BUTTON("Wallet"), 2500), async () => {
+      Then("I should see the 'M&S' coupon available for purchase", then.idVisible(ids.WALLET_CARD_TITLE("M&S"), 2000));
     });
-  });
-  When("I tap to continue to payment", when.tapID(ids.BUTTON_BASE("Continue to Payment", false), 2500), async () => {
-    When("I fill in the required card details on the Stripe payment modal", when.completeStripePayment(cardDetails, "67.50"), async () => {
-      Then("I should see that payment was successful", then.idVisible(ids.VOUCHER_CODE_TITLE(`£75 ${data.CORE_REWARDS_MARKS_AND_SPENCER.data.name["en-GB"]} voucher`), 5000));
-      Then("I should see the coupon email in my inbox", then.assertCouponEmailReceived(data.CUSTOMER_CARMY.data.email, data.CORE_REWARDS_MARKS_AND_SPENCER.data.name["en-GB"]));
+    When("I tap the 'M&S' Coupon card", when.tapID(ids.WALLET_CARD_TITLE("M&S"), 2000), async () => {
+      Then("I should see the correct discount info", then.textVisible("10%", 1500));
+      Then("I should see the correct Coupon name", then.textVisible(data.CORE_REWARDS_MARKS_AND_SPENCER.data.name["en-GB"]));
+      Then("I should see the Coupon description", then.idVisible(ids.WALLET_COUPON_ITEM_DESCRIPTION("One time use"), 2000));
+    });
+    When("I tap the voucher card to adjust its values", when.tapID(ids.WALLET_COUPON_ITEM_DESCRIPTION("One time use"), 2000), async () => {
+      Then("I should see multiple available denominations", then.multipleIDVisible([ids.TEXT_TEMPLATE("£100", "b2b"), ids.TEXT_TEMPLATE("£5", "b2b")]));
+      Then("I should see the coupon input field", then.idVisible(ids.CONTENT_ITEM_INPUT("coupon-input"), 2000));
+      Then("I should see the currency info warning", then.idVisible(ids.TEXT_TEMPLATE("You can only redeem it in GBP (£).", "b2"), 2000));
+      Then("I should see the reward details info button", then.idVisible(ids.TERTIARY_BUTTON("How can my voucher be used"), 2000));
+    });
+    When("I tap the reward details info button", when.tapID(ids.TERTIARY_BUTTON("How can my voucher be used"), 2000), async () => {
+      Then("I should be on the reward details screen", then.textVisible(data.CORE_REWARDS_MARKS_AND_SPENCER.data.redemptionSteps.info["en-GB"]));
+    });
+    When("I tap to go back to the edit screen", when.tapID(ids.BACK_BUTTON, 2000), async () => {
+      When("I select the '£20' value pill", when.tapID(ids.TEXT_TEMPLATE("£20", "b2b"), 2000), async () => {
+        Then("I should see the minimum denomination error message", then.textVisible("Must be at least £50.", 2000));
+        Then("I should see that the checkout cta button is disabled", then.idVisible(ids.BUTTON_BASE("Checkout", true), 2000));
+      });
+    });
+    When("I select the '£100' value pill", when.tapID(ids.TEXT_TEMPLATE("£100", "b2b"), 2000), async () => {
+      Then("I should see the correct discount amount", then.idVisible(ids.TEXT_TEMPLATE("£10.00", "b2b"), 2000));
+      Then("I should see the correct remaining payable amount", then.idVisible(ids.TEXT_TEMPLATE("£90.00", "b1b"), 2000));
+      Then("I should see that the 'Checkout' cta button is now enabled", then.idVisible(ids.BUTTON_BASE("Checkout", false), 2000));
+    });
+    When("I tap to checkout", when.tapID(ids.BUTTON_BASE("Checkout", false), 2000), async () => {
+      Then("I should see the terms and conditions checkbox", then.idVisible(ids.CHECK_BOX_STATE(couponsTermsAndConditions, false), 2000));
+      Then("I should see that the total payable amount is still '£90'", then.idVisible(ids.TEXT_TEMPLATE("£90", "b1b"), 2000));
+      Then("I should see the 'Accept terms and conditions' title", then.idVisible(ids.TEXT_TEMPLATE("Accept terms and conditions", "b1b"), 2000));
+      Then("I should see that the 'Continue to Payment' cta button is disabled", then.idVisible(ids.BUTTON_BASE("Continue to Payment", true), 2000));
+    });
+    When("I tap to edit the Coupon values", when.tapID(ids.TEXT_TEMPLATE("Edit", "b2b"), 2000), async () => {
+      Then("I should see the multiple available denominations", then.multipleIDVisible([ids.TEXT_TEMPLATE("£75", "b2b"), ids.TEXT_TEMPLATE("£2.50", "b2b")]));
+    });
+    When("I select the '£75' value pill", when.tapID(ids.TEXT_TEMPLATE("£75", "b2b"), 2000), async () => {
+      Then("I should see the updated discount amount", then.idVisible(ids.TEXT_TEMPLATE("£7.50", "b2b"), 2000));
+      Then("I should see the correct remaining payable amount", then.idVisible(ids.TEXT_TEMPLATE("£67.50", "b1b"), 2000));
+    });
+    When("I tap to checkout", when.tapID(ids.BUTTON_BASE("Checkout", false), 2000), async () => {
+      When("I mark the terms and conditions checkbox", when.tapID(ids.CHECK_BOX_STATE(couponsTermsAndConditions, false), 2000), async () => {
+        Then("I should see that the 'Continue to Payment' cta button is now enabled", then.idVisible(ids.BUTTON_BASE("Continue to Payment", false), 2000));
+        Then("I should see that checkbox is now selected", then.idVisible(ids.CHECK_BOX_STATE(couponsTermsAndConditions, true), 2000));
+      });
+    });
+    When("I tap to continue to payment", when.tapID(ids.BUTTON_BASE("Continue to Payment", false), 2500), async () => {
+      When("I fill in the required card details on the Stripe payment modal", when.completeStripePayment(cardDetails, "67.50"), async () => {
+        Then("I should see that payment was successful", then.idVisible(ids.VOUCHER_CODE_TITLE(`£75 ${data.CORE_REWARDS_MARKS_AND_SPENCER.data.name["en-GB"]} voucher`), 5000));
+        Then("I should see the coupon email in my inbox", then.assertCouponEmailReceived(data.CUSTOMER_CARMY.data.email, data.CORE_REWARDS_MARKS_AND_SPENCER.data.name["en-GB"]));
+      });
     });
   });
 });
