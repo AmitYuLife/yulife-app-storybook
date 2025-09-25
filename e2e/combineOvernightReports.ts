65cb9f6c5eca07f279f418bd226569a781818b37
Cris Naranjo <cris@yulife.com>
2025-09-25 13:49:28 +0000

    Task DEVOPS-4300: Gitlab CI Detox overnight tests
    See merge request yu-life/yulife-rn-client!6784

--- e2e/combineOvernightReports.ts
+++ e2e/combineOvernightReports.ts
@@ -7,6 +7,12 @@ const ignoreKeys = ["healthcheck", "all", "screenshots"];
 
 const outputDir = "./allure-reports-all";
 
+// TODO: While we're a migrating to the Gitlab CI, we need to support both the old and new S3 bucket names
+// The new bucket is a generic reporting bucket that uses folder structure to separate reports (cypress/detox)
+const S3_BUCKET_NAME =
+  process.env.REPORT_S3_BUCKET_NAME || "yu-eu-west-2-develop-detox-static-origin";
+const S3_PREFIX = process.env.REPORT_S3_BUCKET_NAME ? "detox/" : "";
+
 // 1. fetch the last develop reports
 for (const key of keys) {
   if (ignoreKeys.includes(key)) {
@@ -14,7 +20,7 @@ for (const key of keys) {
   }
 
   console.log(`Syncing ${key}...`);
-  const cmd = `aws s3 sync s3://yu-eu-west-2-develop-detox-static-origin/reports/${key}/develop/allure-results ${outputDir}/${key}`;
+  const cmd = `aws s3 sync s3://${S3_BUCKET_NAME}/${S3_PREFIX}reports/${key}/develop/allure-results ${outputDir}/${key}`;
   console.log(cmd);
   execSync(cmd, { stdio: "inherit" });
 }
@@ -26,7 +32,7 @@ execSync(cmd, { stdio: "inherit" });
 
 // 3. Send it back up to S3
 execSync(
-  `aws s3 sync ${outputDir}/_all s3://yu-eu-west-2-develop-detox-static-origin/reports/develop-summary`,
+  `aws s3 sync ${outputDir}/_all s3://${S3_BUCKET_NAME}/${S3_PREFIX}reports/develop-summary`,
   { stdio: "inherit" }
 );
 
