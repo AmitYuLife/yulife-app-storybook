0768551fc824ee593c67784b32748ab96bb73fd4
Boris <boris@yulife.com>
2025-10-06 15:11:36 +0000

    Task GS-1976: Mood calendar week days
    See merge request yu-life/yulife-rn-client!6844

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -109,6 +109,7 @@ export { default as WalletDiscountItem } from "./reward-wallet/walletDiscountIem
 export { default as ShineButton } from "./shine-button/shine-button";
 export { default as AchievementPoints } from "./achievement-points/achievement-points";
 export { default as AchievementSlot } from "./achievement-slot/achievement-slot";
+export { default as WeekDays } from "./week-days/week-days";
 export { default as RewardSectionHeader } from "./reward-section-header/reward-section-header";
 export {
   default as HorizontalNumberDisplay,
---
+++ src/components/molecules/week-days/week-days.tsx
@@ -0,0 +1,39 @@
+import React, { memo, useMemo } from "react";
+import moment from "moment";
+import { Box } from "@atoms";
+import { TextTemplate } from "@atoms/text/text-template";
+import { Colours } from "@styles";
+
+export const WeekDays = () => {
+  const weekDays = useMemo(() => {
+    const days: { label: string; isToday: boolean }[] = [];
+    const startOfWeek = moment().startOf("week");
+    const today = moment();
+
+    for (let i = 0; i < 7; i++) {
+      const day = startOfWeek.clone().add(i, "days");
+      days.push({
+        label: day.format("dd"),
+        isToday: day.isSame(today, "day"),
+      });
+    }
+
+    return days;
+  }, []);
+
+  return (
+    <Box flexDirection="row" mb={24} px={16}>
+      {weekDays.map((day) => {
+        return (
+          <Box key={day.label} flex={1} center={true}>
+            <TextTemplate type="b2b" color={day.isToday ? Colours.primary.p500 : Colours.neutral.n600}>
+              {day.label}
+            </TextTemplate>
+          </Box>
+        );
+      })}
+    </Box>
+  );
+};
+
+export default memo(WeekDays);
