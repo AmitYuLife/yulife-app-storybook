dd48c7988ec3e2f5ff92b50e863178689da8408e
Manolis Vrondakis <manolis.vrondakis@yulife.com>
2026-01-12 17:17:02 +0000

    Task GS-2191: Pathways claimed screen top bar issues
    See merge request yu-life/yulife-rn-client!7183

--- src/modules/pathways/screens/pathways-reflected/pathways-reflected.screen.tsx
+++ src/modules/pathways/screens/pathways-reflected/pathways-reflected.screen.tsx
@@ -1,18 +1,18 @@
 import { memo } from "react";
 import { Box, TextTemplate } from "@atoms";
-import { DETOX_ENABLED } from "@services/socket";
-import ChestAnimatedRaysBackground from "@components/modals/open-random-chest/subcomponents/chest-animated-rays-background";
 import { ImageBackground } from "expo-image";
-import { ScrollView } from "react-native";
+import { ScrollView, useWindowDimensions } from "react-native";
 import { Colours, StyleSheet } from "@styles";
 import colours from "@styles/colours";
 import PathwayStreaks from "../../components/pathway-streaks/pathway-streaks";
 import { AnimatedPlusPoints, Button } from "@components/molecules";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { t } from "@locale";
 import { FadeIn } from "react-native-reanimated";
 import { GetUserPathwaysQuery } from "@graphql/__generated";
 import { ItemDetailsReward } from "@organisms";
+import ChestAnimatedRaysBackground from "@components/modals/open-random-chest/subcomponents/chest-animated-rays-background";
+import { DETOX_ENABLED } from "@services/socket";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 interface IPathwaysReflectedScreenProps {
   onClose: () => void;
@@ -39,55 +39,59 @@ const PathwaysReflectedScreen = ({
   reflectionProgress,
 }: IPathwaysReflectedScreenProps) => {
   const { maxProgress = 5, reflectedToday = false } = reflectionProgress ?? {};
-  const { bottom } = useSafeAreaInsets();
+  const { bottom, top } = useSafeAreaInsets();
+  const { width } = useWindowDimensions();
 
   const rewardSize = isStreakComplete ? REWARD_SIZE_MYSTERY_BOX : REWARD_SIZE_YUCOIN;
 
   return (
     <PathwaysReflectedScreenWrapper isLoading={isLoading}>
       <Box flex={1} w="100%" entering={FadeIn.duration(300)}>
-        <ScrollView>
-          <Box mt={90} justifyContent="center" alignItems="center">
-            <Box justifyContent="center" alignItems="center" gap={50}>
-              <Box>
-                <ItemDetailsReward
-                  size={rewardSize}
-                  imageSize={isStreakComplete ? REWARD_IMAGE_SIZE_MYSTERY_BOX : REWARD_IMAGE_SIZE_YUCOIN}
-                  source={isStreakComplete ? MYSTERY_BOX_ICON : YUCOIN_SHADOW_ICON}
-                />
-                {todayReward > 0 ? (
-                  <Box alignItems="center" position="absolute" justifyContent="center" w={rewardSize} top={-30}>
-                    <AnimatedPlusPoints type="challenge-success" coins={todayReward} textType="h3" />
-                  </Box>
-                ) : null}
-              </Box>
-              <Box gap={50} px={20}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          {DETOX_ENABLED ? null : <ChestAnimatedRaysBackground top={top - width * 0.2} />}
+          <Box mt={20 + top} disableAutoAdjust={true}>
+            <Box justifyContent="center" alignItems="center" pb={90}>
+              <Box justifyContent="center" alignItems="center" gap={50}>
                 <Box>
-                  <TextTemplate type="big64" textAlign="center" color={colours.neutral.white}>
-                    {currentProgress}
-                  </TextTemplate>
-                  <Box gap={5}>
-                    <TextTemplate type="h3" textAlign="center" color={colours.neutral.white}>
-                      {t("screens.pathways.reflected.title")}
-                    </TextTemplate>
-                    <TextTemplate type="b1" textAlign="center" color={colours.neutral.white}>
-                      {t("screens.pathways.reflected.subtitle")}
-                    </TextTemplate>
-                  </Box>
-                </Box>
-
-                <Box bg={Colours.neutral.white} br={10} px={20} py={20}>
-                  <PathwayStreaks
-                    currentStreak={currentProgress}
-                    reflectedToday={reflectedToday}
-                    maxProgress={maxProgress}
-                    textColor={colours.neutral.n900}
-                    completedBorderColor={null}
-                    notCompletedBorderColor={colours.pathways.reflectionCompletedStreakBorder}
-                    notCompletedColor={colours.pathways.reflectionCompletedStreakBackground}
-                    notCompletedChestForegroundColor={colours.pathways.reflectionCompletedStreakBackground}
-                    notCompletedChestBackgroundColor={colours.pathways.reflectionCompletedStreakBorder}
+                  <ItemDetailsReward
+                    size={rewardSize}
+                    imageSize={isStreakComplete ? REWARD_IMAGE_SIZE_MYSTERY_BOX : REWARD_IMAGE_SIZE_YUCOIN}
+                    source={isStreakComplete ? MYSTERY_BOX_ICON : YUCOIN_SHADOW_ICON}
                   />
+                  {todayReward > 0 ? (
+                    <Box alignItems="center" position="absolute" justifyContent="center" w={rewardSize} top={-30}>
+                      <AnimatedPlusPoints type="challenge-success" coins={todayReward} textType="h3" />
+                    </Box>
+                  ) : null}
+                </Box>
+                <Box gap={50} px={20}>
+                  <Box>
+                    <TextTemplate type="big64" textAlign="center" color={colours.neutral.white}>
+                      {currentProgress}
+                    </TextTemplate>
+                    <Box gap={5}>
+                      <TextTemplate type="h3" textAlign="center" color={colours.neutral.white}>
+                        {t("screens.pathways.reflected.title")}
+                      </TextTemplate>
+                      <TextTemplate type="b1" textAlign="center" color={colours.neutral.white}>
+                        {t("screens.pathways.reflected.subtitle")}
+                      </TextTemplate>
+                    </Box>
+                  </Box>
+
+                  <Box bg={Colours.neutral.white} br={10} px={20} py={20}>
+                    <PathwayStreaks
+                      currentStreak={currentProgress}
+                      reflectedToday={reflectedToday}
+                      maxProgress={maxProgress}
+                      textColor={colours.neutral.n900}
+                      completedBorderColor={null}
+                      notCompletedBorderColor={colours.pathways.reflectionCompletedStreakBorder}
+                      notCompletedColor={colours.pathways.reflectionCompletedStreakBackground}
+                      notCompletedChestForegroundColor={colours.pathways.reflectionCompletedStreakBackground}
+                      notCompletedChestBackgroundColor={colours.pathways.reflectionCompletedStreakBorder}
+                    />
+                  </Box>
                 </Box>
               </Box>
             </Box>
@@ -105,7 +109,6 @@ const PathwaysReflectedScreenWrapper = ({ children, isLoading }: { isLoading: bo
   return (
     <Box w="100%" h="100%" justifyContent="space-between" alignItems="center">
       <ImageBackground source={require("./assets/pathways-reflected-bg.webp")} style={styles.imageBackground} />
-      {DETOX_ENABLED ? null : <ChestAnimatedRaysBackground />}
       {!isLoading ? children : null}
     </Box>
   );
