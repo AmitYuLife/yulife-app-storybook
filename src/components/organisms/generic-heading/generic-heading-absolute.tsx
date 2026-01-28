186c3bf7e4a09a242ecd6acaed8c465a174fcb51
Manolis Vrondakis <manolis.vrondakis@yulife.com>
2026-01-28 13:46:43 +0000

    Task GS-2279: Fix GenericHeadingAbsolute issues
    See merge request yu-life/yulife-rn-client!7262

--- src/components/organisms/generic-heading/generic-heading-absolute.tsx
+++ src/components/organisms/generic-heading/generic-heading-absolute.tsx
@@ -1,17 +1,18 @@
-import React, { ComponentProps } from "react";
+import { ComponentProps, ReactNode } from "react";
 import GenericHeading from "./generic-heading";
-import { View, ViewStyle, Platform } from "react-native";
+import { Platform } from "react-native";
 import { SharedValue } from "react-native-reanimated";
 import { TOP_BAR, Colours, StyleSheet } from "@styles";
 import { BUTTON_CLOSE, CONNECTION_SETUP_TITLE } from "@ids";
 import { useGenericHeadingShadowOpacity } from "./generic-heading.shadow";
 import { Box } from "@atoms";
+import { TOP_BAR_WITH_PAD } from "@styles/top-bar.styles";
 
 interface OwnProps {
   backgroundColor?: string;
   hasShadow?: boolean;
   scrollValue?: SharedValue<number>;
-  children?: React.ReactNode;
+  children?: ReactNode;
 }
 
 type Props = ComponentProps<typeof GenericHeading> & OwnProps;
@@ -22,58 +23,57 @@ const GenericHeadingAbsolute = (props: Props) => {
   const shadowOpacityStyle = useGenericHeadingShadowOpacity(hasShadow, scrollValue);
 
   return (
-    <View pointerEvents="box-none" style={[styles.wrapper, { backgroundColor }]} testID={CONNECTION_SETUP_TITLE}>
-      <GenericHeading {...props} />
+    <>
+      <Box
+        left={0}
+        right={0}
+        position="absolute"
+        alignItems="center"
+        bg={backgroundColor}
+        justifyContent="center"
+        pointerEvents="box-none"
+        disableAutoAdjust={true}
+        pt={TOP_BAR.PADDING_TOP}
+        testID={CONNECTION_SETUP_TITLE}
+        height={!children ? TOP_BAR_WITH_PAD : undefined}
+        minHeight={children ? TOP_BAR_WITH_PAD : undefined}
+      >
+        <GenericHeading {...props} />
 
-      {children}
+        {children}
 
-      {hideBorder ? null : <View style={styles.bottomBorder} testID={BUTTON_CLOSE} />}
+        {hideBorder ? null : (
+          <Box
+            position="absolute"
+            bottom={0}
+            left={0}
+            right={0}
+            width="100%"
+            height={Platform.select({ ios: StyleSheet.hairlineWidth, android: 1 })}
+            bg={Colours.overlay.black20}
+            testID={BUTTON_CLOSE}
+          />
+        )}
 
-      {hasShadow ? (
-        <Box
-          position="absolute"
-          bottom={-4}
-          left={0}
-          right={0}
-          width="100%"
-          height={4}
-          bg="black"
-          pointerEvents="none"
-          forceAnimated={true}
-          style={shadowOpacityStyle}
-        />
-      ) : null}
-    </View>
+        {hasShadow ? (
+          <Box
+            position="absolute"
+            bottom={-4}
+            left={0}
+            right={0}
+            width="100%"
+            height={4}
+            bg="black"
+            pointerEvents="none"
+            forceAnimated={true}
+            style={shadowOpacityStyle}
+          />
+        ) : null}
+      </Box>
+    </>
   );
 };
 
-export const GenericHeadingPad = () => <View style={styles.pad} />;
+export const GenericHeadingPad = () => <Box h={TOP_BAR.TOP_BAR_WITH_PAD} disableAutoAdjust={true} />;
 
 export default GenericHeadingAbsolute;
-
-const styles = StyleSheet.create({
-  wrapper: {
-    position: "absolute",
-    start: 0,
-    end: 0,
-    minHeight: TOP_BAR.TOP_BAR_WITH_PAD,
-    paddingTop: TOP_BAR.PADDING_TOP,
-    justifyContent: "center",
-    alignItems: "center",
-  } as ViewStyle,
-  whiteBackground: {
-    backgroundColor: "white",
-  } as ViewStyle,
-  bottomBorder: {
-    position: "absolute",
-    bottom: 0,
-    start: 0,
-    end: 0,
-    width: "100%",
-    height: Platform.select({ ios: StyleSheet.hairlineWidth, android: 1 }),
-    backgroundColor: Colours.overlay.black20,
-  } as ViewStyle,
-  pad: {
-    height: TOP_BAR.TOP_BAR_WITH_PAD,
-  } as ViewStyle,
-});
