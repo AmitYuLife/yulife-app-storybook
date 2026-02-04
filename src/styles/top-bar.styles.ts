48764955f7312bdeff6372f33542ada8c6938ef4
Leomar Alejar <leomar@yulife.com>
2026-02-04 17:17:40 +0000

    Bugfix GS-2307: Generic heading absolute issue & getDeviceName() func
    See merge request yu-life/yulife-rn-client!7310

--- src/styles/top-bar.styles.ts
+++ src/styles/top-bar.styles.ts
@@ -1,6 +1,6 @@
 import { initialWindowMetrics } from "react-native-safe-area-context";
-import { deviceName } from "expo-device";
 import Style from "./style";
+import { getDeviceName } from "@utils";
 
 // TODO: Update this for the rest of time
 const NOTCHED_IPHONES = [
@@ -37,7 +37,7 @@ const NOTCHED_IPHONES = [
   "iPhone 17 Pro Max",
 ];
 
-const hasNotch = NOTCHED_IPHONES.includes(deviceName ?? "");
+const hasNotch = NOTCHED_IPHONES.includes(getDeviceName() ?? "");
 
 export const HEIGHT = Style.adjust(36);
 const HIT_SLOP_SIZE = Style.adjust(16);
@@ -52,7 +52,7 @@ export const PADDING_TOP = initialWindowMetrics.insets.top + (hasNotch ? Style.a
 export const PADDING_BOTTOM = Style.adjust(5);
 export const LEFT_PADDING_TOP = Style.adjust(3);
 export const LOGO_PADDING_TOP = Style.adjust(12);
-export const TOP_BAR_WITH_PAD = HEIGHT + PADDING_TOP + PADDING_BOTTOM;
+export const TOP_BAR_WITH_PAD = HEIGHT + PADDING_TOP + PADDING_BOTTOM + Style.adjust(10);
 export default {
   HEIGHT,
   PADDING_TOP,
