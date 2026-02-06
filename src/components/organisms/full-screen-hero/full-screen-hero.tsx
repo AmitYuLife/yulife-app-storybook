a95d9a4120ebda49b9f532138a246aa7c1579f88
Manolis Vrondakis <manolis.vrondakis@yulife.com>
2026-02-06 13:19:44 +0000

    Fix GS-2317 - Fix login screen layout issue
    Closes GS-2317
    
    See merge request yu-life/yulife-rn-client!7326

--- src/components/organisms/full-screen-hero/full-screen-hero.tsx
+++ src/components/organisms/full-screen-hero/full-screen-hero.tsx
@@ -2,7 +2,7 @@ import { Box, Logo, TextTemplate } from "@atoms";
 import { Button, Markdown, TouchableOpacityWithDelay } from "@components/molecules";
 import { Colours, Style } from "@styles";
 import { FullScreenHeroProps } from "./types";
-import { KeyboardAvoidingView, LayoutChangeEvent, Platform, SafeAreaView } from "react-native";
+import { LayoutChangeEvent, SafeAreaView } from "react-native";
 import { markdownStyles, styles } from "./styles";
 import { memo, useCallback, useState } from "react";
 import LoopingCarousel from "./components/looping-carousel";
@@ -22,69 +22,61 @@ const FullScreenHero = ({ primaryCta, secondaryCta, disclaimerMarkdown, slides }
   );
 
   return (
-    <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : null} style={styles.flex}>
-      <Box
-        forceAnimated={true}
-        entering={FadeIn.duration(1000)}
-        justifyContent="space-between"
-        alignContent="space-between"
-        flex={1}
-      >
-        <LoopingCarousel data={slides} setCurrentSlide={(index: number) => setCurrentSlide(index)} />
-        <Box position="absolute" top={0} left={0} right={0}>
-          <SafeAreaView>
-            <Logo type="full" width={Style.adjust(76)} style={styles.logo} colour={Colours.neutral.white} />
-          </SafeAreaView>
-        </Box>
-        <Box position="absolute" bottom={0} left={0} right={0} entering={SlideInDown.duration(1000)}>
-          <Box position="relative" left={0} right={0} bottom={0} entering={FadeIn.duration(1000)}>
-            {slides[currentSlide].foregroundComponent}
-          </Box>
-          <Box
-            forceAnimated={true}
-            bg={Colours.neutral.white}
-            pv={40}
-            ph={32}
-            borderTopLeftRadius={16}
-            borderTopRightRadius={16}
-            shadowColor={Colours.neutral.black}
-            shadowOffset={{ width: 0, height: -8 }}
-            shadowOpacity={0.1}
-            shadowRadius={16}
-            entering={FadeIn.duration(1000)}
-            onLayout={handleLayout}
-          >
-            <Button
-              size="Large"
-              onPress={primaryCta.onPress}
-              translatedLabel={primaryCta.label}
-              translationKey=""
-              testID={LOGIN_HERO_CLAIM_ACCOUNT_BUTTON}
-            />
-            {secondaryCta ? (
-              <TouchableOpacityWithDelay onPress={secondaryCta.onPress} delay={1000}>
-                <Box pv={16}>
-                  <TextTemplate
-                    type="l1b"
-                    textAlign="center"
-                    color={Colours.primary.p600}
-                    decoration="underline"
-                    testID={LOGIN_HERO_LOGIN_BUTTON}
-                  >
-                    {secondaryCta.label}
-                  </TextTemplate>
+    <Box h="100%" alignContent="space-between" justifyContent="space-between" entering={FadeIn.duration(1000)}>
+      <LoopingCarousel data={slides} setCurrentSlide={(index: number) => setCurrentSlide(index)} />
+      <Box position="absolute" top={0} left={0} right={0}>
+        <SafeAreaView>
+          <Logo type="full" width={Style.adjust(76)} style={styles.logo} colour={Colours.neutral.white} />
+        </SafeAreaView>
+      </Box>
+      <Box position="absolute" bottom={0} left={0} right={0} h="100%" justifyContent="flex-end">
+        <Box pb={0} entering={FadeIn.duration(1000)} justifyContent="flex-end" h="100%">
+          <Box entering={FadeIn.duration(1000)}>{slides[currentSlide].foregroundComponent}</Box>
+          <Box entering={SlideInDown.duration(1000)}>
+            <Box
+              pv={40}
+              ph={32}
+              shadowRadius={16}
+              shadowOpacity={0.1}
+              onLayout={handleLayout}
+              borderTopLeftRadius={16}
+              borderTopRightRadius={16}
+              bg={Colours.neutral.white}
+              shadowColor={Colours.neutral.black}
+              shadowOffset={{ width: 0, height: -8 }}
+            >
+              <Button
+                size="Large"
+                onPress={primaryCta.onPress}
+                translatedLabel={primaryCta.label}
+                translationKey=""
+                testID={LOGIN_HERO_CLAIM_ACCOUNT_BUTTON}
+              />
+              {secondaryCta ? (
+                <TouchableOpacityWithDelay onPress={secondaryCta.onPress} delay={1000}>
+                  <Box pv={16}>
+                    <TextTemplate
+                      type="l1b"
+                      textAlign="center"
+                      color={Colours.primary.p600}
+                      decoration="underline"
+                      testID={LOGIN_HERO_LOGIN_BUTTON}
+                    >
+                      {secondaryCta.label}
+                    </TextTemplate>
+                  </Box>
+                </TouchableOpacityWithDelay>
+              ) : null}
+              {disclaimerMarkdown ? (
+                <Box pt={12}>
+                  <Markdown text={disclaimerMarkdown} markdownStyles={markdownStyles} />
                 </Box>
-              </TouchableOpacityWithDelay>
-            ) : null}
-            {disclaimerMarkdown ? (
-              <Box pt={12}>
-                <Markdown text={disclaimerMarkdown} markdownStyles={markdownStyles} />
-              </Box>
-            ) : null}
+              ) : null}
+            </Box>
           </Box>
         </Box>
       </Box>
-    </KeyboardAvoidingView>
+    </Box>
   );
 };
 
