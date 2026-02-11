79a08d8d35ce493c09bfeb97df69cc1215bc0c04
Leomar Alejar <leomar@yulife.com>
2026-02-11 19:02:07 +0000

    Bugfix GS-2356: Mystery box padding top
    See merge request yu-life/yulife-rn-client!7362

--- src/components/modals/open-random-chest/subcomponents/stages/redeemed-stages/default-redeemed-stage.tsx
+++ src/components/modals/open-random-chest/subcomponents/stages/redeemed-stages/default-redeemed-stage.tsx
@@ -1,49 +1,36 @@
-import { Style, StyleSheet } from "@styles";
 import { memo } from "react";
-import { View } from "react-native";
 import { GetMobileGameBattlePassChestDetailsQuery } from "@graphql/__generated";
 import RadioBattlePassRewardItem from "@components/molecules/radio-battle-pass-reward-item/radio-battle-pass-reward-item";
 import { FadeOutDown } from "react-native-reanimated";
 import { Button } from "@components/molecules";
 import ChestHeaderText from "../../chest-header-text";
 import { Box } from "@atoms";
+import { t } from "@locale";
 
 interface IChestRedeemedStageProps {
   redeemedItems: GetMobileGameBattlePassChestDetailsQuery["details"]["redeemedRewards"];
   onClose?: () => void;
 }
 
-const ChestRedeemedStage = ({ redeemedItems, onClose }: IChestRedeemedStageProps) => (
-  <View style={styles.container}>
-    {/* TODO: Temporary text */}
-    <ChestHeaderText label="You already redeemed your prize!" />
-    <View style={styles.listSelectPicker}>
-      {redeemedItems.map((item) => (
-        <RadioBattlePassRewardItem
-          key={item.id}
-          checked={true}
-          onPress={onClose}
-          reward={{ id: item.id, title: item.title }}
-        />
-      ))}
-    </View>
-    <Box exiting={FadeOutDown.duration(800)} mt={150}>
-      <Button translationKey="labels.cta.close" onPress={onClose} />
+const ChestRedeemedStage = ({ redeemedItems, onClose }: IChestRedeemedStageProps) => {
+  return (
+    <Box w="100%" alignItems="center" justifyContent="center" pt={48}>
+      <ChestHeaderText label={t("modals.open_random_chest.default_redeemed_stage_title")} />
+      <Box w="100%" gap={15} ph={30}>
+        {redeemedItems.map((item) => (
+          <RadioBattlePassRewardItem
+            key={item.id}
+            checked={true}
+            onPress={onClose}
+            reward={{ id: item.id, title: item.title }}
+          />
+        ))}
+      </Box>
+      <Box exiting={FadeOutDown.duration(800)} mt={36}>
+        <Button translationKey="labels.cta.close" onPress={onClose} />
+      </Box>
     </Box>
-  </View>
-);
+  );
+};
 
-const styles = StyleSheet.create({
-  container: {
-    width: "100%",
-    alignItems: "center",
-    justifyContent: "center",
-    paddingTop: Style.adjust(48),
-  },
-  listSelectPicker: {
-    width: "100%",
-    gap: Style.adjust(15),
-    paddingHorizontal: Style.adjust(30),
-  },
-});
 export default memo(ChestRedeemedStage);
