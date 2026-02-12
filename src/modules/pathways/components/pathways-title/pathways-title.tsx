89ebf76ea630e3ceb6541e5feb183acda342b40d
Artiom Fotescu <artiom@yulife.com>
2026-02-12 14:43:12 +0000

    Bugfix GS-2366: Bring drunk yunity back home
    See merge request yu-life/yulife-rn-client!7375

--- src/modules/pathways/components/pathways-title/pathways-title.tsx
+++ src/modules/pathways/components/pathways-title/pathways-title.tsx
@@ -1,43 +1,27 @@
-import { Box } from "@atoms";
-import { Markdown } from "@components/molecules";
+import { Box, TextTemplate } from "@atoms";
 import { t } from "@locale";
-import { Colours, Style, StyleSheet, templateTextMarkdownStyles } from "@styles";
+import { Colours, Style } from "@styles";
 import { memo } from "react";
-import { Platform } from "react-native";
+import { Image } from "react-native";
 
 const PathwaysTitle = () => {
   return (
-    <Box>
-      <Markdown
-        markdownStyles={markdownStyles}
-        text={t("screens.pathways.powered_by_unity", { yunityUrl: require("../../assets/yunity.png") })}
-      />
+    <Box flexDirection="row" alignItems="center">
+      <TextTemplate type="b2" color={Colours.neutral.white}>
+        {t("screens.pathways.powered_by_yunity")}
+      </TextTemplate>
+      <Box mt={-1}>
+        <Image source={require("../../assets/yunity.png")} style={styles.image} />
+      </Box>
     </Box>
   );
 };
 
 export default memo(PathwaysTitle);
 
-const markdownStyles = StyleSheet.create({
-  paragraph: {
-    paddingVertical: 0,
-  },
-  text: {
-    ...templateTextMarkdownStyles.b2,
-    color: Colours.neutral.white,
-  },
-  imageWrapper: {
-    width: Style.adjust(43),
-    height: Style.adjust(17),
-  },
+const styles = {
   image: {
     width: Style.adjust(43),
     height: Style.adjust(17),
-    bottom: Style.adjust(
-      Platform.select({
-        ios: 2,
-        android: -2,
-      })
-    ),
   },
-});
+};
