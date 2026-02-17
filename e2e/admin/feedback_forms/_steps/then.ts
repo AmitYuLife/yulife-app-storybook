3e02ea45a0a944edc75f858e9a5b8da81133ab3b
Julian Memai <julian.memai@yulife.com>
2026-02-17 14:50:51 +0000

    Detox GS-2398: Fix Feedback Forms
    See merge request yu-life/yulife-rn-client!7402

--- e2e/admin/feedback_forms/_steps/then.ts
+++ e2e/admin/feedback_forms/_steps/then.ts
@@ -1,12 +1,17 @@
-import { navigation } from "@utils"
+import { navigation } from "@utils";
 
-export const {
-    idVisible,
-    textVisible,
-    textNotVisible,
-    multipleTextVisible,
-} = navigation.common
+export const { idVisible, textVisible, textNotVisible, multipleTextVisible } = navigation.common;
 
-export const feedbackFormVisible =(title:string)=> async()=>{
-    await waitFor(element(by.text(title))).toBeVisible().withTimeout(6000)
-}
+export const feedbackFormVisible = (title: string) => async () => {
+  await new Promise((res) => setTimeout(res, 3000));
+
+  await device.disableSynchronization();
+  try {
+    await waitFor(element(by.text(title)))
+      .toBeVisible()
+      .withTimeout(15000);
+  } finally {
+    await device.enableSynchronization();
+  }
+  await new Promise((res) => setTimeout(res, 1000));
+};
