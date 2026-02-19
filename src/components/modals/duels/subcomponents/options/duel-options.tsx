823b6ad156f45fa9cd9b440fd644e12655fa114c
Leomar Alejar <leomar@yulife.com>
2026-02-19 10:43:11 +0000

    Bugfix GS-2418: Remove BlurProvider component
    See merge request yu-life/yulife-rn-client!7421

--- src/components/modals/duels/subcomponents/options/duel-options.tsx
+++ src/components/modals/duels/subcomponents/options/duel-options.tsx
@@ -1,6 +1,6 @@
-import { useState, useMemo } from "react";
-import { View } from "react-native";
-import { Loading, BlurProvider } from "@atoms";
+import { useState, useMemo, useCallback } from "react";
+import { Platform, View } from "react-native";
+import { Loading, BlurredWrapper } from "@atoms";
 import { Button, Markdown } from "@molecules";
 import styles from "./duel-options.styles";
 import { DuelStepProps } from "../../duels.types";
@@ -31,6 +31,7 @@ export default function DuelOptions({
   isLoading,
 }: DuelStepProps) {
   const [pickerAmountLabel, setPickerAmountLabel] = useState(null);
+  const [isPickerVisible, setIsPickerVisible] = useState(false);
   const { data, loading } = useQuery(gql("GetDuelTemplatesDocument"), {
     fetchPolicy: "network-only",
   });
@@ -60,71 +61,71 @@ export default function DuelOptions({
 
   const onPressLeftIcon = () => Navigation.dismissModal(componentId);
 
+  const showPicker = useCallback(() => setIsPickerVisible(true), []);
+  const hidePicker = useCallback(() => setIsPickerVisible(false), []);
+
+  const pickerItems = wagerOptions.map((option) => ({
+    ...option,
+    onPress() {
+      setPickerAmountLabel(option.label);
+      setYucoin(option.value);
+      hidePicker();
+    },
+  }));
+
   return (
-    <BlurProvider
-      backgroundColor="dark"
-      style={styles.blurStyle}
-      render={({ toggleOverlay }) => (
-        <View style={styles.safeAreaView}>
-          <TopBarAbsolute leftIcon={LeftIcon.CLOSE} onPressLeftIcon={onPressLeftIcon} rightIcon="Coins" />
-          <View style={styles.wrapper} testID={DUEL_OPTIONS_SCREEN}>
-            <View style={styles.description}>
-              <Markdown
-                markdownStyles={markdownStyles}
-                text={t("modals.duels.duel_options.header", {
-                  name: opponent?.fullName || t("modals.duels.duel_options.colleague"),
-                })}
-              />
-            </View>
-            <View style={styles.description}>
-              <Markdown markdownStyles={markdownStyles} text={t("modals.duels.duel_options.subheader_1")} />
-            </View>
-            <View style={styles.flexRow}>
-              <Markdown markdownStyles={markdownStyles} text={t("modals.duels.duel_options.subheader_2")} />
-            </View>
-            <View>
-              <WagerDropdown
-                yucoin={yucoin}
-                pickerAmountLabel={pickerAmountLabel || t("modals.duels.duel_options.select_wager")}
-                onPress={toggleOverlay}
-              />
-            </View>
-          </View>
-          <View style={styles.buttonWrapper}>
-            <Button
-              disabled={yucoin === null || isLoading}
-              isLoading={isLoading}
-              onPress={submitDuel}
-              translationKey="modals.duels.duel_options.button_label"
+    <>
+      <View style={styles.safeAreaView}>
+        <TopBarAbsolute leftIcon={LeftIcon.CLOSE} onPressLeftIcon={onPressLeftIcon} rightIcon="Coins" />
+        <View style={styles.wrapper} testID={DUEL_OPTIONS_SCREEN}>
+          <View style={styles.description}>
+            <Markdown
+              markdownStyles={markdownStyles}
+              text={t("modals.duels.duel_options.header", {
+                name: opponent?.fullName || t("modals.duels.duel_options.colleague"),
+              })}
             />
           </View>
-          {loading ? (
-            <View style={[styles.safeAreaView, styles.loadingOverlay]}>
-              <Loading />
-            </View>
-          ) : null}
+          <View style={styles.description}>
+            <Markdown markdownStyles={markdownStyles} text={t("modals.duels.duel_options.subheader_1")} />
+          </View>
+          <View style={styles.flexRow}>
+            <Markdown markdownStyles={markdownStyles} text={t("modals.duels.duel_options.subheader_2")} />
+          </View>
+          <View>
+            <WagerDropdown
+              yucoin={yucoin}
+              pickerAmountLabel={pickerAmountLabel || t("modals.duels.duel_options.select_wager")}
+              onPress={showPicker}
+            />
+          </View>
         </View>
-      )}
-      renderOverlay={({ toggleOverlay }) => {
-        const items = wagerOptions.map((option) => ({
-          ...option,
-          onPress() {
-            setPickerAmountLabel(option.label);
-            setYucoin(option.value);
-            toggleOverlay();
-          },
-        }));
-        return (
-          <ListPicker
-            onPressCancel={toggleOverlay}
-            instruction={t("modals.duels.duel_options.instruction")}
-            items={items}
+        <View style={styles.buttonWrapper}>
+          <Button
+            disabled={yucoin === null || isLoading}
+            isLoading={isLoading}
+            onPress={submitDuel}
+            translationKey="modals.duels.duel_options.button_label"
           />
-        );
-      }}
-    />
+        </View>
+        {loading ? (
+          <View style={[styles.safeAreaView, styles.loadingOverlay]}>
+            <Loading />
+          </View>
+        ) : null}
+      </View>
+      <BlurredWrapper tint="light" isVisible={isPickerVisible} backgroundColor={backgroundColor}>
+        <ListPicker
+          onPressCancel={hidePicker}
+          instruction={t("modals.duels.duel_options.instruction")}
+          items={pickerItems}
+        />
+      </BlurredWrapper>
+    </>
   );
 }
+
+const backgroundColor = Platform.select({ ios: "rgba(0,0,0,0.5)", android: "rgba(0,0,0,0.3)" });
 
 export const markdownStyles = StyleSheet.create({
   text: templateTextMarkdownStyles.b1,
