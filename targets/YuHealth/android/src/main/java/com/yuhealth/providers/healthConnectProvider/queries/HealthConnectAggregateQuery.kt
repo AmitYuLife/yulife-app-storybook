7e1e79c59e6dc17542c5b9cbe5ab21d23e6b0156
Teodor Kurtev <teodor.kurtev@yulife.com>
2026-04-07 14:42:25 +0300

    Task GS-2514: Add meditation back to Health Connect
    See merge request yu-life/yulife-rn-client!7664

--- targets/YuHealth/android/src/main/java/com/yuhealth/providers/healthConnectProvider/queries/HealthConnectAggregateQuery.kt
+++ targets/YuHealth/android/src/main/java/com/yuhealth/providers/healthConnectProvider/queries/HealthConnectAggregateQuery.kt
@@ -179,7 +179,7 @@ class HealthConnectAggregateQuery(
     val activityQuery = ActivityQueryParams(
       request.startTime, request.endTime, QueryOptions(
         disableUserEntries = request.queryOptions.disableUserEntries,
-        whitelistActivityTypes = request.queryOptions.whitelistActivityTypes,
+        whitelistActivityTypes = activityFilters ?: request.queryOptions.whitelistActivityTypes,
         whitelistApps = request.queryOptions.whitelistApps,
       )
     )
