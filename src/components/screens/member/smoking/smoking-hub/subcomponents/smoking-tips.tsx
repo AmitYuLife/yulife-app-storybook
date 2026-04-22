bfef424915fb70e695e4e7e1ce1bf0402197002e
Julian Memai <julian.memai@yulife.com>
2026-04-22 12:40:04 +0000

    Bugfix GS-2696: Fix invalid hook call in smoking tips
    See merge request yu-life/yulife-rn-client!7790

--- src/components/screens/member/smoking/smoking-hub/subcomponents/smoking-tips.tsx
+++ src/components/screens/member/smoking/smoking-hub/subcomponents/smoking-tips.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { FC, memo } from "react";
+import { FC, memo, useCallback } from "react";
 import { FlatList, View } from "react-native";
 import { SMOKING_INFO_PANEL } from "@ids";
 import { HealthSmokingStateTip } from "@redux/health-smoking/health-smoking.types";
@@ -11,21 +11,27 @@ interface Props {
   tips: HealthSmokingStateTip[];
 }
 
-export const SmokingTips: FC<Props> = memo(({ tips }) => (
-  <View testID={SMOKING_INFO_PANEL}>
-    <FlatList
-      renderItem={TipCardItem}
-      snapToInterval={SNAP_TO_INTERVAL}
-      data={tips}
-      showsHorizontalScrollIndicator={false}
-      horizontal={true}
-      decelerationRate={DECELERATION_RATE}
-      keyExtractor={keyExtractor}
-      viewabilityConfig={VIEWABILITY_CONFIG}
-      ItemSeparatorComponent={TipCardSeparator}
-      contentContainerStyle={tipCardStyles.contentContainer}
-    />
-  </View>
-));
+export const SmokingTips: FC<Props> = memo(({ tips }) => {
+  const renderItem = useCallback(({ item }: { item: HealthSmokingStateTip }) => <TipCardItem item={item} />, []);
+
+  const renderSeparator = useCallback(() => <TipCardSeparator />, []);
+
+  return (
+    <View testID={SMOKING_INFO_PANEL}>
+      <FlatList
+        renderItem={renderItem}
+        snapToInterval={SNAP_TO_INTERVAL}
+        data={tips}
+        showsHorizontalScrollIndicator={false}
+        horizontal={true}
+        decelerationRate={DECELERATION_RATE}
+        keyExtractor={keyExtractor}
+        viewabilityConfig={VIEWABILITY_CONFIG}
+        ItemSeparatorComponent={renderSeparator}
+        contentContainerStyle={tipCardStyles.contentContainer}
+      />
+    </View>
+  );
+});
 
 export default SmokingTips;
